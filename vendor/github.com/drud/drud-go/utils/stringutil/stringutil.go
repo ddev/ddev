2434eabee8116e19c5d47311a97765d746b86ab7
tannerjfco <tannerjfco@gmail.com>
2017-02-13 19:35:16 -0700

    add drud-go/utils sub pkgs

---
+++ vendor/github.com/drud/drud-go/utils/stringutil/stringutil.go
@@ -0,0 +1,36 @@
+package stringutil
+
+import (
+	"math/rand"
+	"time"
+)
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*(){}[]<>?*")
+
+// FormatPlural is a simple wrapper which returns different strings based on the count value.
+func FormatPlural(count int, single string, plural string) string {
+	if count == 1 {
+		return single
+	}
+	return plural
+}
+
+// RandStringRunes returns a random string of length n
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
+// RandomString returns a random string of len strlen
+func RandomString(strlen int) string {
+	rand.Seed(time.Now().UTC().UnixNano())
+	const chars = "abcdefghijklmnopqrstuvwxyz0123456789"
+	result := make([]byte, strlen)
+	for i := 0; i < strlen; i++ {
+		result[i] = chars[rand.Intn(len(chars))]
+	}
+	return string(result)
+}
---
+++ vendor/github.com/drud/drud-go/utils/system/system.go
@@ -0,0 +1,89 @@
+package system
+
+import (
+	"errors"
+	"io"
+	"net/http"
+	"os"
+	"os/exec"
+	"strings"
+
+	log "github.com/Sirupsen/logrus"
+)
+
+// GetHomeDir uses the $HOME var to return the user's home directory
+func GetHomeDir() (string, error) {
+	homedir := os.Getenv("HOME")
+	if homedir == "" {
+		return "", errors.New("Standard methods to locate user's home firectory failed. Please set $HOME and try again.")
+	}
+
+	return homedir, nil
+}
+
+// RunCommand runs a command on the host system.
+func RunCommand(command string, args []string) (string, error) {
+	log.WithFields(log.Fields{
+		"Command": command + " " + strings.Join(args[:], " "),
+	}).Info("Running Command")
+
+	out, err := exec.Command(
+		command, args...,
+	).CombinedOutput()
+
+	log.WithFields(log.Fields{
+		"Result": string(out),
+	}).Debug("Command Result")
+
+	return string(out), err
+}
+
+// RunCommandPipe runs a command on the host system while piping output to stderr and stdout.
+func RunCommandPipe(command string, args []string) error {
+	log.WithFields(log.Fields{
+		"Command": command + " " + strings.Join(args[:], " "),
+	}).Info("Running Command")
+
+	proc := exec.Command(command, args...)
+	proc.Stdout = os.Stdout
+	proc.Stdin = os.Stdin
+	proc.Stderr = os.Stderr
+
+	return proc.Run()
+}
+
+// FileExists checks a file's existence
+func FileExists(name string) bool {
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
+
+// DownloadFile retreives a file.
+func DownloadFile(filepath string, url string) (err error) {
+
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Writer the body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
