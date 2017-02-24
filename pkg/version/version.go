c19f9e514c8b5bcb491aa0b279f9194395cbba98
Randy Fay <randy@randyfay.com>
2017-02-24 11:20:02 -0700

    Put the basic default versions back in so ddev can be used by devs outside make context

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -6,22 +6,22 @@ var VERSION = ""
 // IMPORTANT: These versions are overridden by version ldflags specifications VERSION_VARIABLES in the Makefile
 
 // Current version of the ddev tool, by default the git committish (should be current git tag)
-var DdevVersion = ""  // 0.4.0
+var DdevVersion = "0.4.0"  // Note that this is overridden by make
 
 // WebImg defines the default web image for drud dev
-var WebImg = ""  // drud/nginx-php-fpm7-local
+var WebImg = "drud/nginx-php-fpm7-local"  // Note that this is overridden by make
 
 // WebTag defines the default web image tag for drud dev
-var WebTag = "" // 0.2.0
+var WebTag = "0.2.0" // Note that this is overridden by make
 
 // DBImg defines the default db image for drud dev
-var DBImg = ""  // drud/mysql-docker-local-57
+var DBImg = "drud/mysql-docker-local-57"  // Note that this is overridden by make
 
 // DBTag defines the default db image tag for drud dev
-var DBTag = "" // 0.1.0
+var DBTag = "0.1.0" // Note that this is overridden by make
 
 // RouterImage defines the image used for the drud dev router.
-var RouterImage = "" // drud/nginx-proxy
+var RouterImage = "drud/nginx-proxy" // Note that this is overridden by make
 
 // RouterTag defines the tag used for the drud dev router.
-var RouterTag = ""  // 0.1.0
+var RouterTag = "0.1.0"  // Note that this is overridden by make
