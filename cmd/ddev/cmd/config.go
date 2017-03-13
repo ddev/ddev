2564f69f091cdac3611de60b8e2ea3a05fcece69
Brad Bowman <beeradb@gmail.com>
2017-03-13 15:21:16 -0600

    [57-ddev-config] Add approot and configfile as arguments to appconfig constructor

--- cmd/ddev/cmd/config.go
+++ cmd/ddev/cmd/config.go
@@ -21,14 +21,13 @@ var ConfigCommand = &cobra.Command{
 	Use:   "config",
 	Short: "Create or modify a ddev application config in the current directory",
 	Run: func(cmd *cobra.Command, args []string) {
-		dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+		appRoot, err := filepath.Abs(filepath.Dir(os.Args[0]))
 		if err != nil {
 			log.Fatalf("Could not determine current working directory: %v\n", err)
 		}
+		configFile := path.Join(appRoot, ".ddev", "config.yaml")
 
-		configFile := path.Join(dir, ".ddev", "config.yaml")
-
-		c, err := appconfig.NewAppConfig(configFile)
+		c, err := appconfig.NewAppConfig(appRoot, configFile)
 		err = c.Config()
 		if err != nil {
 			log.Fatalf("There was a problem configuring your application: %v\n", err)
