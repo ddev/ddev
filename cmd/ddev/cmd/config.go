a7475112c83bf43846d58608efab60f190f6d9f5
Brad Bowman <beeradb@gmail.com>
2017-03-13 13:10:41 -0600

    [57-ddev-config] set a default config.yaml location

--- cmd/ddev/cmd/config.go
+++ cmd/ddev/cmd/config.go
@@ -2,6 +2,9 @@ package cmd
 
 import (
 	"log"
+	"os"
+	"path"
+	"path/filepath"
 
 	"github.com/drud/ddev/pkg/appconfig"
 	"github.com/spf13/cobra"
@@ -18,11 +21,22 @@ var ConfigCommand = &cobra.Command{
 	Use:   "config",
 	Short: "Create or modify a ddev application config in the current directory",
 	Run: func(cmd *cobra.Command, args []string) {
-		c := appconfig.NewAppConfig()
-
-		err := c.Config()
+		dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 		if err != nil {
-			log.Fatalf("There was a problem configuring your application: %s", err)
+			log.Fatalf("Could not determine current working directory: %v\n", err)
+		}
+
+		configFile := path.Join(dir, ".ddev", "config.yaml")
+
+		c, err := appconfig.NewAppConfig(configFile)
+		err = c.Config()
+		if err != nil {
+			log.Fatalf("There was a problem configuring your application: %v\n", err)
+		}
+
+		err = c.Write()
+		if err != nil {
+			log.Fatalf("Could not write ddev config file: %v\n", err)
 		}
 	},
 }
