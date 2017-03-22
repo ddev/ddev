cf78677a6b15e2b9599c7df312a0bc7770fd80fa
Brad Bowman <beeradb@gmail.com>
2017-03-22 12:20:59 -0600

    [ddevapp-tests] Add some simple string checking for the output of the docker-compose file

--- pkg/ddevapp/config_test.go
+++ pkg/ddevapp/config_test.go
@@ -222,6 +222,7 @@ func TestWriteDockerComposeYaml(t *testing.T) {
 	assert.Error(err)
 	config.Name = testcommon.RandString(32)
 	config.AppType = allowedAppTypes[0]
+	config.Docroot = testcommon.RandString(16)
 
 	err = config.WriteDockerComposeConfig()
 	// We should get an error here since no config or directory path exists.
@@ -240,4 +241,12 @@ func TestWriteDockerComposeYaml(t *testing.T) {
 	assert.NoError(err)
 	assert.False(fileinfo.IsDir())
 	assert.Equal(fileinfo.Name(), filepath.Base(config.DockerComposeYAMLPath()))
+
+	composeBytes, err := ioutil.ReadFile(config.DockerComposeYAMLPath())
+	assert.NoError(err)
+	contentString := string(composeBytes)
+	assert.Contains(contentString, config.Docroot)
+	assert.Contains(contentString, config.Name)
+	assert.Contains(contentString, config.Platform)
+	assert.Contains(contentString, config.AppType)
 }
