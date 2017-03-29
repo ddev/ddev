2edc6d5d9b95d8b5769d1f328eb5bafa41cc0219
tannerjfco <tannerjfco@gmail.com>
2017-03-28 18:08:56 -0600

    remove WP_HOME/WP_SITEURL, update db charset

--- pkg/cms/config/wordpress.go
+++ pkg/cms/config/wordpress.go
@@ -40,7 +40,7 @@ if (file_exists(__DIR__ . '/wp-config.local.php')) {
 }
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
+define('DB_CHARSET', 'utf8mb4');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
@@ -67,27 +67,11 @@ define( 'SECURE_AUTH_SALT', '{{ $config.SecureAuthSalt }}' );
 define( 'LOGGED_IN_SALT',   '{{ $config.LoggedInSalt }}' );
 define( 'NONCE_SALT',       '{{ $config.NonceSalt }}' );
 
-/**
- * WP_SITEURL allows the WordPress address (URL) to be defined. The value defined is the
- * address where your WordPress core files reside.
- */
-if ( !defined('WP_SITEURL') ) {
-  define( 'WP_SITEURL', '{{ $config.DeployURL }}' );
-}
-
-/**
- * WP_HOME overrides the wp_options table value for home but does not change it in the
- * database. home is the address you want people to type in their browser to reach your WordPress site.
- */
-if ( !defined('WP_HOME') ) {
-  define( 'WP_HOME', '{{ $config.DeployURL }}' );
-}
-
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__));
+	define('ABSPATH', dirname(__FILE__) . '/');
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . '/wp-settings.php');
