f8a3a193ac33f0ddcee0ef2f1a1f138cbd4be3e8
Brad Bowman <beeradb@gmail.com>
2017-03-31 12:45:36 -0600

    [35-describe-command] Fix test failures

--- cmd/ddev/cmd/restart_test.go
+++ cmd/ddev/cmd/restart_test.go
@@ -2,33 +2,34 @@ package cmd
 
 import (
 	"fmt"
-	"strings"
 	"testing"
 
-	"github.com/drud/ddev/pkg/plugins/platform"
 	"github.com/drud/drud-go/utils/system"
 	"github.com/stretchr/testify/assert"
 )
 
 // TestDevRestart runs `drud legacy restart` on the test apps
 func TestDevRestart(t *testing.T) {
+	assert := assert.New(t)
 	for _, site := range DevTestSites {
 		cleanup := site.Chdir()
 
 		args := []string{"restart"}
 		out, err := system.RunCommand(DdevBin, args)
-		assert.NoError(t, err)
+		assert.NoError(err)
 
-		app := platform.PluginMap[strings.ToLower(plugin)]
-		app.Init()
+		app, err := getActiveApp()
+		if err != nil {
+			assert.Fail("Could not find an active ddev configuration: %v", err)
+		}
 
 		format := fmt.Sprintf
-		assert.Contains(t, string(out), format("Stopping %s-web", app.ContainerName()))
-		assert.Contains(t, string(out), format("Stopping %s-db", app.ContainerName()))
-		assert.Contains(t, string(out), format("Starting %s-web", app.ContainerName()))
-		assert.Contains(t, string(out), format("Starting %s-db", app.ContainerName()))
-		assert.Contains(t, string(out), "Your application can be reached at")
-		assert.Contains(t, string(out), app.URL())
+		assert.Contains(string(out), format("Stopping %s-web", app.ContainerName()))
+		assert.Contains(string(out), format("Stopping %s-db", app.ContainerName()))
+		assert.Contains(string(out), format("Starting %s-web", app.ContainerName()))
+		assert.Contains(string(out), format("Starting %s-db", app.ContainerName()))
+		assert.Contains(string(out), "Your application can be reached at")
+		assert.Contains(string(out), app.URL())
 
 		cleanup()
 	}
