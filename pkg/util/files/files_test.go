fae741bc3fd57d04cdb00bbd011a2073559c513d
tannerjfco <tannerjfco@gmail.com>
2017-04-05 19:37:54 -0600

    adds test for untar

--- pkg/util/files/files_test.go
+++ pkg/util/files/files_test.go
@@ -1,7 +1,6 @@
 package files
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"path"
@@ -12,17 +11,17 @@ import (
 )
 
 var (
-	temp     = os.TempDir()
-	cwd      string
-	testFile = path.Join(temp, "testfile")
+	temp            = os.TempDir()
+	cwd             string
+	testArchiveURL  = "https://github.com/drud/wordpress/releases/download/v0.1.0/files.tar.gz"
+	testArchivePath = path.Join(os.TempDir(), "files.tar.gz")
 )
 
 func TestMain(m *testing.M) {
-	tf, err := os.Create(testFile)
+	err := system.DownloadFile(testArchivePath, testArchiveURL)
 	if err != nil {
-		log.Fatalf("failed to create test file: %s", err)
+		log.Fatalf("archive download failed: %s", err)
 	}
-	tf.Close()
 
 	cwd, err = os.Getwd()
 	if err != nil {
@@ -34,15 +33,29 @@ func TestMain(m *testing.M) {
 	os.Exit(testRun)
 }
 
+func TestUntargz(t *testing.T) {
+	assert := assert.New(t)
+	exDir := path.Join(temp, "extract")
+	err := os.Mkdir(exDir, 0755)
+	assert.NoError(err)
+
+	err = Untargz(testArchivePath, exDir)
+	assert.NoError(err)
+	assert.True(system.FileExists(path.Join(exDir, "2017", "04", "pexels-photo-265186-100x100.jpeg")))
+	assert.True(system.FileExists(path.Join(exDir, "2017", "04", "pexels-photo-265186-2000x1200.jpeg")))
+
+	os.RemoveAll(exDir)
+}
+
 // TestCopyFile tests copying a file.
 func TestCopyFile(t *testing.T) {
 	assert := assert.New(t)
 	dest := path.Join(temp, "testfile2")
 
-	err := os.Chmod(testFile, 0644)
+	err := os.Chmod(testArchivePath, 0644)
 	assert.NoError(err)
 
-	err = CopyFile(testFile, dest)
+	err = CopyFile(testArchivePath, dest)
 	assert.NoError(err)
 
 	file, err := os.Stat(dest)
@@ -59,16 +72,14 @@ func TestCopyDir(t *testing.T) {
 	os.Mkdir(dest, 0755)
 
 	// test source not a directory
-	err := CopyDir(testFile, temp)
+	err := CopyDir(testArchivePath, temp)
 	assert.Error(err)
-	msg := fmt.Sprintf("%v", err)
-	assert.Contains(msg, "source is not a directory")
+	assert.Contains(err.Error(), "source is not a directory")
 
 	// test destination exists
 	err = CopyDir(temp, cwd)
 	assert.Error(err)
-	msg = fmt.Sprintf("%v", err)
-	assert.Contains(msg, "destination already exists")
+	assert.Contains(err.Error(), "destination already exists")
 	os.RemoveAll(dest)
 
 	// copy a directory.
