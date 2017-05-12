248182539dac3e3c23b1a4ade5c6f219e9947384
Brad Bowman <beeradb@gmail.com>
2017-05-12 10:49:24 -0600

    Inform users when a `ddev` update is available (#217)
    * [update-notifications] Add required vendor updates
    
    * [update-notifications] Add commenting for updatecheck functions
    
    * [update-notifications] Improved commenting and messaging to users

---
+++ pkg/updatecheck/updatecheck.go
@@ -0,0 +1,79 @@
+package updatecheck
+
+import (
+	"context"
+	"strings"
+
+	"time"
+
+	"os"
+
+	"github.com/Masterminds/semver"
+	"github.com/google/go-github/github"
+)
+
+// AvailableUpdates returns true (along with a release URL) if there is an update available in the specified repo which is newer than the currentVersion string.
+func AvailableUpdates(repoOrg string, repoName string, currentVersion string) (bool, string, error) {
+	client := github.NewClient(nil)
+	opt := &github.ListOptions{Page: 1}
+	releases, _, err := client.Repositories.ListReleases(context.Background(), repoOrg, repoName, opt)
+	if err != nil {
+		return false, "", err
+	}
+
+	if isReleaseVersion(currentVersion) {
+		cv, err := semver.NewVersion(currentVersion)
+		if err != nil {
+			return false, "", err
+		}
+		for _, release := range releases {
+			releaseVersion, err := semver.NewVersion(*release.TagName)
+			if err != nil {
+				return false, "", err
+			}
+
+			if cv.Compare(releaseVersion) == -1 {
+				return true, *release.HTMLURL, nil
+			}
+		}
+	}
+
+	return false, "", nil
+}
+
+// IsUpdateNeeded returns true if the modification date on filepath is older than the duration specified.
+func IsUpdateNeeded(filepath string, updateInterval time.Duration) (bool, error) {
+	info, err := os.Stat(filepath)
+	if os.IsNotExist(err) {
+		return true, ResetUpdateTime(filepath)
+	} else if err != nil {
+		return false, err
+	}
+
+	timeSinceMod := time.Since(info.ModTime())
+
+	if timeSinceMod >= updateInterval {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+// ResetUpdateTime resets the file modification date on filepath by removing and re-creating the file.
+func ResetUpdateTime(filepath string) error {
+	err := os.Remove(filepath)
+	_ = err // We don't actually care if remove failed. All we care about is that the create succeeds.
+	_, err = os.Create(filepath)
+	return err
+}
+
+// isReleaseVersion does a (very naive) check on whether a version string consistutes a release version or a dev build.
+func isReleaseVersion(version string) bool {
+	parts := strings.Split(version, "-")
+
+	if len(parts) > 1 {
+		return false
+	}
+
+	return true
+}
