965efca889f406cc893f8753c67ec680a06cdbb8
Randy Fay <randy@randyfay.com>
2017-05-15 08:44:57 -0600

    Add confirmation step to ddev rm

--- cmd/ddev/cmd/remove.go
+++ cmd/ddev/cmd/remove.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/drud/ddev/pkg/util"
 	"github.com/spf13/cobra"
 )
@@ -14,13 +16,18 @@ var LocalDevRMCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		app, err := getActiveApp()
 		if err != nil {
-			util.Failed("Failed to remove: %v", err)
+			util.Failed("Failed to get active app: %v", err)
 		}
 
 		if app.SiteStatus() == "not found" {
 			util.Failed("App not running locally. Try `ddev start`.")
 		}
 
+		fmt.Printf("Is it ok to remove the site %s with all of its containers? All data will be lost. (y/N): ", app.GetName())
+		if !util.AskForConfirmation() {
+			util.Failed("Not removing site.")
+			return
+		}
 		err = app.Down()
 		if err != nil {
 			util.Failed("Failed to remove %s: %s", app.GetName(), err)
