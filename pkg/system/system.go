8807f25d36fc2905fa881fef24ef43e250d2e22d
Brad Bowman <beeradb@gmail.com>
2017-06-02 11:06:53 -0600

    [downloadfile-unvendor] commit system package

---
+++ pkg/system/system.go
@@ -0,0 +1,50 @@
+package system
+
+import (
+	"os"
+	"os/exec"
+	"strings"
+
+	log "github.com/Sirupsen/logrus"
+)
+
+// RunCommand runs a command on the host system.
+func RunCommand(command string, args []string) (string, error) {
+	log.WithFields(log.Fields{
+		"Command": command + " " + strings.Join(args[:], " "),
+	}).Info("Running Command")
+
+	out, err := exec.Command(
+		command, args...,
+	).CombinedOutput()
+
+	log.WithFields(log.Fields{
+		"Result": string(out),
+	}).Debug("Command Result")
+
+	return string(out), err
+}
+
+// RunCommandPipe runs a command on the host system while piping output to stderr and stdout.
+func RunCommandPipe(command string, args []string) error {
+	log.WithFields(log.Fields{
+		"Command": command + " " + strings.Join(args[:], " "),
+	}).Info("Running Command")
+
+	proc := exec.Command(command, args...)
+	proc.Stdout = os.Stdout
+	proc.Stdin = os.Stdin
+	proc.Stderr = os.Stderr
+
+	return proc.Run()
+}
+
+// FileExists checks a file's existence
+func FileExists(name string) bool {
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
