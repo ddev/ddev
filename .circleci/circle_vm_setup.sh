3be941270cc02c65d15658b4b8ef281e9232fc23
Randy Fay <randy@randyfay.com>
2017-06-05 14:28:35 -0600

    Bump golang (for tests on ddev) to 1.8.3 to match golang container

--- .circleci/circle_vm_setup.sh
+++ .circleci/circle_vm_setup.sh
@@ -8,7 +8,7 @@ sudo apt-get install -qq mysql-client realpath zip
 
 # golang of the version we want
 sudo apt-get remove -qq golang &&
-wget -q -O /tmp/golang.tgz https://storage.googleapis.com/golang/go1.8.1.linux-amd64.tar.gz &&
+wget -q -O /tmp/golang.tgz https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz &&
 cd /tmp && tar -xf golang.tgz &&
 sudo rm -rf /usr/local/go && sudo mv go /usr/local
 
@@ -26,4 +26,3 @@ sudo apt-get install -qq docker-ce=17.05.0~ce-0~ubuntu-$(lsb_release -cs)
 # docker-compose
 sudo curl -s -L "https://github.com/docker/compose/releases/download/1.12.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-
