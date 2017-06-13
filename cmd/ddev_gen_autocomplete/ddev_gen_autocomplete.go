67268ed487964ebe2130a625a866458b4fad0ad9
Randy Fay <randy@randyfay.com>
2017-06-13 15:01:29 -0600

    Bash autocompletion for ddev, fixes #125 (#309)
    * Add autocomplete command build
    
    * Add bash completion to install_ddev.sh
    
    * Add minor doc for using autocomplete
    
    * Generate bash completion in circle and add to tarballs
    
    * Satisfy the static analysis gods
    
    * Consolidate artifacts-generation in a script

---
+++ cmd/ddev_gen_autocomplete/ddev_gen_autocomplete.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/drud/ddev/cmd/ddev/cmd"
+	"github.com/drud/ddev/pkg/util"
+)
+
+var targetDir = "bin"
+
+func main() {
+	if _, err := os.Stat(targetDir); os.IsNotExist(err) {
+		err = os.MkdirAll(targetDir, 0755)
+		util.CheckErr(err)
+	}
+	err := cmd.RootCmd.GenBashCompletionFile(filepath.Join(targetDir, "ddev_bash_completion.sh"))
+	util.CheckErr(err)
+}
