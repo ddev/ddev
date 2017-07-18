4e12b170359fac6a1f3dfd2844dda4a09c1f3625
Randy Fay <randy@randyfay.com>
2017-07-18 12:43:13 -0600

    Fix failed rebase to put the code in the right place

--- pkg/plugins/platform/local.go
+++ pkg/plugins/platform/local.go
@@ -872,13 +872,6 @@ func (l *LocalApp) AddHostsEntry() error {
 		}
 		dockerIP = dockerHostURL.Hostname()
 	}
-
-	_, err := osexec.Command("sudo", "-h").Output()
-	if (os.Getenv("DRUD_NONINTERACTIVE") != "") || err != nil {
-		util.Warning("You must manually add the following entry to your hosts file:\n%s %s", dockerIP, l.HostName())
-		return nil
-	}
-
 	hosts, err := goodhosts.NewHosts()
 	if err != nil {
 		log.Fatalf("could not open hostfile. %s", err)
@@ -889,7 +882,7 @@ func (l *LocalApp) AddHostsEntry() error {
 
 	_, err = osexec.Command("sudo", "-h").Output()
 	if (os.Getenv("DRUD_NONINTERACTIVE") != "") || err != nil {
-		fmt.Printf("You must manually add the following entry to your host file:\n%s %s\n", dockerIP, l.HostName())
+		util.Warning("You must manually add the following entry to your hosts file:\n%s %s", dockerIP, l.HostName())
 		return nil
 	}
 
