88cd5ec99e95097733ff8a9c20b0f51c6d369bc4
Randy Fay <randy@randyfay.com>
2017-07-26 14:23:07 -0600

    Improve retest instructions in build.sh, rm ~/.ddev/Test*

--- build.sh
+++ build.sh
@@ -12,17 +12,17 @@ if [ ! -z "$SURF_REF" ]; then
 	ln -s $PWD $DRUDSRC/ddev
 	cd $DRUDSRC/ddev
 	echo "Surf building $SURF_REF ($SURF_SHA1) on $(hostname) for OS=$(go env GOOS) in $DRUDSRC/ddev"
-	echo "To retry the build, use
-		export GITHUB_TOKEN=<token>
-		surf-build -s $SURF_SHA1 -r https://github.com/drud/ddev -n surf-$(go env GOOS)"
+	echo "To retry the build, export GITHUB_TOKEN, DDEV_PANTHEON_API_TOKEN, DRUD_DEBUG and...
+	surf-build -s $SURF_SHA1 -r https://github.com/drud/ddev -n surf-$(go env GOOS)"
 fi
 
 export GOTEST_SHORT=1
 
-echo "Warning: deleting all docker containers"
+echo "Warning: deleting all docker containers and deleting ~/.ddev/*"
 if [ "$(docker ps -aq | wc -l)" -gt 0 ] ; then
 	docker rm -f $(docker ps -aq)
 fi
+rm -rf ~/.ddev/Test*
 
 echo "Running tests..."
 time make test
