fe68c96b7ce36a7f621ae59e122c324d51d06e56
Randy Fay <randy@randyfay.com>
2017-08-07 11:55:02 -0600

    Add docs on test machine setup, minor improvement to build.sh (#427)
    * Add docs on test machine setup, minor improvement to build.sh
    
    * Add info about windows install fail
    
    * Correct output about what gets deleted in build.sh
    
    * Clarified Mac OS instructions around configuring Nosleep.
    
    * Added NPM install instructions for surf-build.

--- build.sh
+++ build.sh
@@ -11,14 +11,14 @@ if [ ! -z "$SURF_REF" ]; then
 	mkdir -p $DRUDSRC
 	ln -s $PWD $DRUDSRC/ddev
 	cd $DRUDSRC/ddev
-	echo "Surf building $SURF_REF ($SURF_SHA1) on $(hostname) for OS=$(go env GOOS) in $DRUDSRC/ddev"
+	echo "Surf building $SURF_REF ($SURF_SHA1) at $(date) on $(hostname) for OS=$(go env GOOS) in $DRUDSRC/ddev"
 	echo "To retry the build, export GITHUB_TOKEN, DDEV_PANTHEON_API_TOKEN, DRUD_DEBUG and...
 	surf-build -s $SURF_SHA1 -r https://github.com/drud/ddev -n surf-$(go env GOOS)"
 fi
 
 export GOTEST_SHORT=1
 
-echo "Warning: deleting all docker containers and deleting ~/.ddev/*"
+echo "Warning: deleting all docker containers and deleting ~/.ddev/Test*"
 if [ "$(docker ps -aq | wc -l)" -gt 0 ] ; then
 	docker rm -f $(docker ps -aq)
 fi
