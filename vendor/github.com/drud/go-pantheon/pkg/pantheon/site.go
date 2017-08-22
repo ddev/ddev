dee2f12b479e1e0cdbf6f098c6c7ef5c7575067f
Nick McCrory <n.mccrory@icloud.com>
2017-08-22 11:03:47 -0700

    Updated drud/go-pantheon distribution

--- vendor/github.com/drud/go-pantheon/pkg/pantheon/site.go
+++ vendor/github.com/drud/go-pantheon/pkg/pantheon/site.go
@@ -14,58 +14,15 @@ type Site struct {
 	Key         string `json:"key"`
 	Role        string `json:"role"`
 	Site        struct {
-		Attributes struct {
-			Label string `json:"label"`
-		} `json:"attributes"`
 		Created         jsonInt64 `json:"created"`
 		CreatedByUserID string    `json:"created_by_user_id"`
 		Framework       string    `json:"framework"`
 		Frozen          bool      `json:"frozen"`
-		Holder          struct {
-			Email   string `json:"email"`
-			ID      string `json:"id"`
-			Profile struct {
-				ActivityLevel   string `json:"activity_level"`
-				Code            string `json:"code"`
-				DashboardVisits []struct {
-					Date string `json:"date"`
-					Site string `json:"site"`
-				} `json:"dashboard_visits"`
-				Experiments                 struct{}    `json:"experiments"`
-				Firstname                   string      `json:"firstname"`
-				FullName                    string      `json:"full_name"`
-				GoogleAdwordsPushedCodeSent jsonInt64   `json:"google_adwords_pushed_code_sent"`
-				GuiltyOfAbuse               interface{} `json:"guilty_of_abuse"`
-				InitialIdentityName         interface{} `json:"initial_identity_name"`
-				InitialIdentityStrategy     interface{} `json:"initial_identity_strategy"`
-				InvitesSent                 jsonInt64   `json:"invites_sent"`
-				InvitesToSite               jsonInt64   `json:"invites_to_site"`
-				InvitesToUser               jsonInt64   `json:"invites_to_user"`
-				LastOrgSpinup               string      `json:"last-org-spinup"`
-				Lastname                    string      `json:"lastname"`
-				Maxdevsites                 jsonInt64   `json:"maxdevsites"`
-				MinimizeJitDocs             bool        `json:"minimize_jit_docs"`
-				Modified                    jsonInt64   `json:"modified"`
-				Organization                string      `json:"organization"`
-				Seens                       struct {
-					NewSiteSupportInterface bool `json:"new-site-support-interface"`
-				} `json:"seens"`
-				TrackingFirstCodePush       jsonInt64 `json:"tracking_first_code_push"`
-				TrackingFirstSiteCreate     jsonInt64 `json:"tracking_first_site_create"`
-				TrackingFirstTeamInvite     jsonInt64 `json:"tracking_first_team_invite"`
-				TrackingFirstWorkflowInLive jsonInt64 `json:"tracking_first_workflow_in_live"`
-				Verify                      jsonInt64 `json:"verify"`
-				WebServicesBusiness         bool      `json:"web_services_business"`
-			} `json:"profile"`
-		} `json:"holder"`
-		HolderID     string `json:"holder_id"`
-		HolderType   string `json:"holder_type"`
-		ID           string `json:"id"`
-		LastCodePush struct {
+		ID              string    `json:"id"`
+		LastCodePush    struct {
 			Timestamp string      `json:"timestamp"`
 			UserUUID  interface{} `json:"user_uuid"`
 		} `json:"last_code_push"`
-		LastFrozenAt  jsonInt64 `json:"last_frozen_at"`
 		Name          string    `json:"name"`
 		Owner         string    `json:"owner"`
 		PhpVersion    jsonInt64 `json:"php_version"`
@@ -81,47 +38,6 @@ type Site struct {
 			ProductID string `json:"product_id"`
 			URL       string `json:"url"`
 		} `json:"upstream"`
-		UpstreamUpdatesByEnvironment struct {
-			HasCode bool `json:"has_code"`
-		} `json:"upstream_updates_by_environment"`
-		UserInCharge struct {
-			Email   string `json:"email"`
-			ID      string `json:"id"`
-			Profile struct {
-				ActivityLevel   string `json:"activity_level"`
-				Code            string `json:"code"`
-				DashboardVisits []struct {
-					Date string `json:"date"`
-					Site string `json:"site"`
-				} `json:"dashboard_visits"`
-				Experiments                 struct{}    `json:"experiments"`
-				Firstname                   string      `json:"firstname"`
-				FullName                    string      `json:"full_name"`
-				GoogleAdwordsPushedCodeSent jsonInt64   `json:"google_adwords_pushed_code_sent"`
-				GuiltyOfAbuse               interface{} `json:"guilty_of_abuse"`
-				InitialIdentityName         interface{} `json:"initial_identity_name"`
-				InitialIdentityStrategy     interface{} `json:"initial_identity_strategy"`
-				InvitesSent                 jsonInt64   `json:"invites_sent"`
-				InvitesToSite               jsonInt64   `json:"invites_to_site"`
-				InvitesToUser               jsonInt64   `json:"invites_to_user"`
-				LastOrgSpinup               string      `json:"last-org-spinup"`
-				Lastname                    string      `json:"lastname"`
-				Maxdevsites                 jsonInt64   `json:"maxdevsites"`
-				MinimizeJitDocs             bool        `json:"minimize_jit_docs"`
-				Modified                    jsonInt64   `json:"modified"`
-				Organization                string      `json:"organization"`
-				Seens                       struct {
-					NewSiteSupportInterface bool `json:"new-site-support-interface"`
-				} `json:"seens"`
-				TrackingFirstCodePush       jsonInt64 `json:"tracking_first_code_push"`
-				TrackingFirstSiteCreate     jsonInt64 `json:"tracking_first_site_create"`
-				TrackingFirstTeamInvite     jsonInt64 `json:"tracking_first_team_invite"`
-				TrackingFirstWorkflowInLive jsonInt64 `json:"tracking_first_workflow_in_live"`
-				Verify                      jsonInt64 `json:"verify"`
-				WebServicesBusiness         bool      `json:"web_services_business"`
-			} `json:"profile"`
-		} `json:"user_in_charge"`
-		UserInChargeID string `json:"user_in_charge_id"`
 	} `json:"site"`
 	SiteID string `json:"site_id"`
 	UserID string `json:"user_id"`
