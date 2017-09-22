fe1a8c6a0ba28321e03c0a6a20c6a6890bfb073f
Randy Fay <randy@randyfay.com>
2017-09-22 10:23:39 -0600

    Revert to using Circleci's docker version, upgrade docker-compose, fixes #471 (#473)
    * Revert to using Circleci's docker version, upgrade docker-compose, fixes #471
    
    * Stop getting the new mysql signing key
    
    * See what go version they're using

--- .circleci/circle_vm_setup.sh
+++ .circleci/circle_vm_setup.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 set -o errexit
+set -x
 
 # Basic tools
 
-# mysql - old key is expired, so get the new one
-sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 5072E1F5 >/dev/null 2>&1
 sudo apt-get update -qq
 sudo apt-get install -qq mysql-client realpath zip
+
+go version
 
 # golang of the version we want
 sudo apt-get remove -qq golang &&
@@ -15,16 +16,6 @@ wget -q -O /tmp/golang.tgz https://storage.googleapis.com/golang/go1.9.linux-amd
 cd /tmp && tar -xf golang.tgz &&
 sudo rm -rf /usr/local/go && sudo mv go /usr/local
 
-# Docker setup
-sudo apt-get remove -qq docker docker-engine
-sudo apt-get update -qq
-sudo apt-get install -qq apt-transport-https ca-certificates  curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-# Per docker docs, you always need the stable repository, even if you want to install edge builds as well.
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update -qq
-sudo apt-get install -qq docker-ce=17.06.0~ce-0~ubuntu
-
 # docker-compose
-sudo curl -s -L "https://github.com/docker/compose/releases/download/1.14.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -s -L "https://github.com/docker/compose/releases/download/1.16.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
