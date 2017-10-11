4ee73a2a40224b4eb563ce16d47fe545007073c8
Randy Fay <randy@randyfay.com>
2017-10-10 21:19:27 -0600

    Try more explicit docker start

--- .circleci/macos_circle_vm_setup.sh
+++ .circleci/macos_circle_vm_setup.sh
@@ -3,9 +3,26 @@
 set -o errexit
 set -x
 
-# Basic tools
+docker version || echo "No docker yet"
 
+# Basic tools
 brew update && brew install mariadb coreutils golang && brew cask install docker
 
 # macOS version
 sw_vers
+
+
+# Start docker, wait for daemon
+# From https://stackoverflow.com/a/35979292/215713
+open -g -a /Applications/Docker.app || exit 2
+
+# Wait for the server to start up, if applicable.
+i=0
+while ! docker system info &>/dev/null; do
+  (( i++ == 0 )) && printf %s '-- Waiting for Docker to finish starting up...' || printf '.'
+  sleep 1
+done
+(( i )) && printf '\n'
+
+echo "-- Docker is ready."
+docker version
