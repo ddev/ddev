fad9c085516c1e29580ae9345f89ec27d4e324aa
tannerjfco <tannerjfco@gmail.com>
2017-10-31 14:29:31 -0600

    begs forgiveness of metalinter for shadow declaration and offers praises

--- pkg/plugins/platform/router.go
+++ pkg/plugins/platform/router.go
@@ -56,7 +56,7 @@ func StartDdevRouter() error {
 	}
 
 	certDir := filepath.Join(util.GetGlobalDdevDir(), "certs")
-	if _, err := os.Stat(certDir); os.IsNotExist(err) {
+	if _, err = os.Stat(certDir); os.IsNotExist(err) {
 		err = os.MkdirAll(certDir, 0755)
 		if err != nil {
 			return fmt.Errorf("unable to create directory for ddev router: %v", err)
