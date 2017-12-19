f3bc7559e6bb0953314133bfb91a5a08ba3de323
Cameron Eagans <me@cweagans.net>
2017-12-19 13:01:15 -0700

    Use the new v1.0.0 tag of the mariadb-local container.

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -26,7 +26,7 @@ var WebTag = "v0.8.2" // Note that this is overridden by make
 var DBImg = "drud/mariadb-local" // Note that this is overridden by make
 
 // DBTag defines the default db image tag for drud dev
-var DBTag = "mariadb-switchover" // Note that this is overridden by make
+var DBTag = "v1.0.0" // Note that this is overridden by make
 
 // DBAImg defines the default phpmyadmin image tag used for applications.
 var DBAImg = "drud/phpmyadmin"
