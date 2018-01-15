613697af58d1eacfc003b6a78e10c259e0abd1fb
Randy Fay <randy@randyfay.com>
2018-01-15 16:04:07 -0700

    Minor improvement to an error message to refer to settings file instead of config

--- pkg/ddevapp/ddevapp.go
+++ pkg/ddevapp/ddevapp.go
@@ -292,7 +292,7 @@ func (app *DdevApp) ImportDB(imPath string, extPath string) error {
 	if err != nil {
 		// @todo: Use a typed error instead of relying on the text of the message.
 		if strings.Contains(err.Error(), "settings files already exist and are being managed") {
-			return fmt.Errorf("failed to write configuration file for %s: %v", app.GetName(), err)
+			return fmt.Errorf("failed to write settings file for %s: %v", app.GetName(), err)
 		}
 		util.Warning("A custom settings file exists for your application, so ddev did not generate one.")
 		util.Warning("Run 'ddev describe' to find the database credentials for this application.")
