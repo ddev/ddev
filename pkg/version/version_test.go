a147a5d64f6e728e6c65be6201e18314bfccf3ab
Cameron Eagans <me@cweagans.net>
2018-01-17 13:40:04 -0700

    Remove redundant assertions

--- pkg/version/version_test.go
+++ pkg/version/version_test.go
@@ -10,15 +10,6 @@ func TestGetVersionInfo(t *testing.T) {
 	assert := asrt.New(t)
 	v := GetVersionInfo()
 
-	assert.Contains(v, "cli")
-	assert.Contains(v, "web")
-	assert.Contains(v, "db")
-	assert.Contains(v, "dba")
-	assert.Contains(v, "router")
-	assert.Contains(v, "commit")
-	assert.Contains(v, "domain")
-	assert.Contains(v, "build info")
-
 	assert.Equal(DdevVersion, v["cli"])
 	assert.Contains(v["web"], WebImg)
 	assert.Contains(v["web"], WebTag)
