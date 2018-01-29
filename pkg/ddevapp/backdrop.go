4f69b13f4f94202caec489ae6bf767d8a6863140
Cameron Eagans <me@cweagans.net>
2018-01-28 19:54:07 -0700

    Config paths for backdrop are now hardcoded.

--- pkg/ddevapp/backdrop.go
+++ pkg/ddevapp/backdrop.go
@@ -52,8 +52,8 @@ const backdropTemplate = `<?php
 $database = 'mysql://{{ $config.DatabaseUsername }}:{{ $config.DatabasePassword }}@{{ $config.DatabaseHost }}/{{ $config.DatabaseName }}';
 $database_prefix = '{{ $config.DatabasePrefix }}';
 
-$config_directories['active'] = 'files/config_' . md5($database) . '/active';
-$config_directories['staging'] = 'files/config_' . md5($database) . '/staging';
+$config_directories['active'] = 'files/ddev_config/active';
+$config_directories['staging'] = 'files/ddev_config/staging';
 
 $settings['update_free_access'] = FALSE;
 $settings['hash_salt'] = '{{ $config.HashSalt }}';
