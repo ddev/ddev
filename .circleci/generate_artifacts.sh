78af73e812fc5a2546d6508d80ed28341b39e18b
Randy Fay <randy@randyfay.com>
2018-02-19 15:06:35 -0700

    Move the images out of the tarball, as people may not want it, and it's big

--- .circleci/generate_artifacts.sh
+++ .circleci/generate_artifacts.sh
@@ -13,32 +13,31 @@ $BASE_DIR/bin/linux/ddev version | awk '/drud\// {print $2;}' >/tmp/images.txt
 for item in $(cat /tmp/images.txt); do
   docker pull $item
 done
-docker save -o /tmp/docker_images.tar $(cat /tmp/images.txt)
+docker save -o $ARTIFACTS/ddev_docker_images.$VERSION.tar $(cat /tmp/images.txt)
 
-# Generate and place the extra items like images and autocomplete
+# Generate and place extra items like autocomplete
 bin/linux/ddev_gen_autocomplete
 for dir in bin/darwin/darwin_amd64 bin/linux bin/windows/windows_amd64; do
   cp bin/ddev_bash_completion.sh $dir
-  cp /tmp/docker_images.tar $dir
 done
 
 # Generate macOS tarball/zipball
 cd $BASE_DIR/bin/darwin/darwin_amd64
-tar -czf $ARTIFACTS/ddev_macos.$VERSION.tar.gz ddev ddev_bash_completion.sh docker_images.tar
-zip $ARTIFACTS/ddev_macos.$VERSION.zip ddev ddev_bash_completion.sh docker_images.tar
+tar -czf $ARTIFACTS/ddev_macos.$VERSION.tar.gz ddev ddev_bash_completion.sh
+zip $ARTIFACTS/ddev_macos.$VERSION.zip ddev ddev_bash_completion.sh
 
 # Generate linux tarball/zipball
 cd $BASE_DIR/bin/linux
-tar -czf $ARTIFACTS/ddev_linux.$VERSION.tar.gz ddev ddev_bash_completion.sh docker_images.tar
-zip $ARTIFACTS/ddev_linux.$VERSION.zip ddev ddev_bash_completion.sh docker_images.tar
+tar -czf $ARTIFACTS/ddev_linux.$VERSION.tar.gz ddev ddev_bash_completion.sh
+zip $ARTIFACTS/ddev_linux.$VERSION.zip ddev ddev_bash_completion.sh
 
 # generate windows tarball/zipball
 cd $BASE_DIR/bin/windows/windows_amd64
-tar -czf $ARTIFACTS/ddev_windows.$VERSION.tar.gz ddev.exe ddev_bash_completion.sh docker_images.tar
-zip $ARTIFACTS/ddev_windows.$VERSION.zip ddev.exe ddev_bash_completion.sh docker_images.tar
+tar -czf $ARTIFACTS/ddev_windows.$VERSION.tar.gz ddev.exe ddev_bash_completion.sh
+zip $ARTIFACTS/ddev_windows.$VERSION.zip ddev.exe ddev_bash_completion.sh
 
 # Create the sha256 files
 cd $ARTIFACTS
-for item in *.tar.gz *.zip; do
+for item in *.tar.gz *.zip ddev_docker_images.$VERSION.tar; do
   sha256sum $item > $item.sha256.txt
 done
