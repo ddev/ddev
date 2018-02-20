0c8416bc6a4235d1846b0c7d51b77336679ded0f
Randy Fay <randy@randyfay.com>
2018-02-20 15:18:12 -0700

    Upgrade golang to 1.10 for testing, docker-compose 1.19 (#665)
    * Upgrade golang to 1.10 for testing, docker-compose 1.19
    
    * Fix t.Skip problem that go 1.10 pointed out

--- .circleci/circle_vm_setup.sh
+++ .circleci/circle_vm_setup.sh
@@ -8,14 +8,12 @@ set -x
 sudo apt-get update -qq
 sudo apt-get install -qq mysql-client realpath zip
 
-go version
-
 # golang of the version we want
-sudo apt-get remove -qq golang &&
-wget -q -O /tmp/golang.tgz https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz &&
-cd /tmp && tar -xf golang.tgz &&
-sudo rm -rf /usr/local/go && sudo mv go /usr/local
+sudo apt-get remove -qq golang && sudo rm -rf /usr/local/go &&
+wget -q -O /tmp/golang.tgz https://dl.google.com/go/go1.10.linux-amd64.tar.gz &&
+sudo tar -C /usr/local -xzf /tmp/golang.tgz
+
 
 # docker-compose
-sudo curl -s -L "https://github.com/docker/compose/releases/download/1.16.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -s -L "https://github.com/docker/compose/releases/download/1.19.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
