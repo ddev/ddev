183af00c50f19752320c9f64c7ae493a87bb0dfd
Randy Fay <randy@randyfay.com>
2018-02-21 13:26:33 -0700

    gzip images after the tar is created by docker save

--- .circleci/generate_artifacts.sh
+++ .circleci/generate_artifacts.sh
@@ -14,6 +14,7 @@ for item in $(cat /tmp/images.txt); do
   docker pull $item
 done
 docker save -o $ARTIFACTS/ddev_docker_images.$VERSION.tar $(cat /tmp/images.txt)
+gzip $ARTIFACTS/ddev_docker_images.$VERSION.tar
 
 # Generate and place extra items like autocomplete
 bin/linux/ddev_gen_autocomplete
@@ -38,6 +39,6 @@ zip $ARTIFACTS/ddev_windows.$VERSION.zip ddev.exe ddev_bash_completion.sh
 
 # Create the sha256 files
 cd $ARTIFACTS
-for item in *.tar.gz *.zip ddev_docker_images.$VERSION.tar; do
+for item in *.tar.gz *.zip ddev_docker_images.$VERSION.tar.gz; do
   sha256sum $item > $item.sha256.txt
 done
