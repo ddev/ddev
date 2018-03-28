49885e3a0fe06bc2da207e1cb883256797c1c77c
Randy Fay <randy@randyfay.com>
2018-03-28 09:06:22 -0600

    Quiet the install a bit, improve circle output

--- .circleci/circle_vm_setup.sh
+++ .circleci/circle_vm_setup.sh
@@ -26,9 +26,9 @@ sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get update -qq
+sudo apt-get install -qq docker-ce
 
 # docker-compose
-sudo curl -s -L "https://github.com/docker/compose/releases/download/1.19.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -s -L "https://github.com/docker/compose/releases/download/1.20.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
