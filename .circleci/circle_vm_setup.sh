2aff9f2dc338ef8ec8ff7a26e9eef84ecc263e22
Randy Fay <randy@randyfay.com>
2018-03-28 10:16:39 -0600

    Make circleci install current stable Docker-ce (#761)

--- .circleci/circle_vm_setup.sh
+++ .circleci/circle_vm_setup.sh
@@ -14,6 +14,21 @@ wget -q -O /tmp/golang.tgz https://dl.google.com/go/go1.10.linux-amd64.tar.gz &&
 sudo tar -C /usr/local -xzf /tmp/golang.tgz
 
 
+# Remove existing docker
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update -qq
+sudo apt-get install -qq docker-ce
+
 # docker-compose
-sudo curl -s -L "https://github.com/docker/compose/releases/download/1.19.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -s -L "https://github.com/docker/compose/releases/download/1.20.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
