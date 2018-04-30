996b8c4d62a12fd4acc7f2828ea7a8b5f654aa39
Boris Schauer <me@bschauer.de>
2018-04-30 21:10:50 +0200

    [BUGFIX] Trusted hosts pattern (#1) (#808)
    Problem:
    ```
    The current host header value does not match the configured trusted hosts pattern! Check the pattern defined in $GLOBALS['TYPO3_CONF_VARS']['SYS']['trustedHostsPattern'] and adapt it, if you want to allow the current host header 'website.ddev.local:8080' for your installation.
    ```

--- pkg/ddevapp/typo3.go
+++ pkg/ddevapp/typo3.go
@@ -15,6 +15,8 @@ const typo3AdditionalConfigTemplate = `<?php
 /** ` + DdevFileSignature + `: Automatically generated TYPO3 AdditionalConfiguration.php file.
  ddev manages this file and may delete or overwrite the file unless this comment is removed.
  */
+ 
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['trustedHostsPattern'] = '.*';
 
 $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default'] = array_merge($GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default'], [
                     'dbname' => 'db',
