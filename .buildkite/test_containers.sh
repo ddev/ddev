6975fa025ae1f4f1863563142285b99985f6b566
Randy Fay <randy@randyfay.com>
2018-06-07 21:56:12 -0600

    Fix broken containers resulting from CRs in scripts (#913)
    Mostly this was fixed by changing the agent configuration,
    but this PR does adjust the container build script to delete
    existing images before beginning.

--- .buildkite/test_containers.sh
+++ .buildkite/test_containers.sh
@@ -7,6 +7,14 @@ set -o pipefail
 set -o nounset
 set -x
 
+# Make sure we don't have any existing containers on the testbot that might
+# result in this container not being built from scratch.
+VERSION=$(make version | sed 's/^VERSION://')
+IMAGES=$(docker images | awk "/$VERSION/ { print \$3 }")
+if [ ! -z "$IMAGES" ] ; then
+  docker rmi --force $IMAGES
+fi
+
 for dir in containers/*
     do pushd $dir
     time make test
