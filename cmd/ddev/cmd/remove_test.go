9962f9e3c7081ff216c961e1e9dced8ece144937
Andrew French <andrew@afren.ch>
2018-07-03 17:22:23 -0600

    Add test case to ensure a user can't combine --all and --remove-data.

--- cmd/ddev/cmd/remove_test.go
+++ cmd/ddev/cmd/remove_test.go
@@ -33,11 +33,21 @@ func TestDevRemove(t *testing.T) {
 
 	// Re-create running sites.
 	addSites()
-	out, err := exec.RunCommand(DdevBin, []string{"remove", "--all"})
+
+	// Ensure a user can't accidentally wipe out everything.
+	appsBefore := len(ddevapp.GetApps())
+	out, err := exec.RunCommand(DdevBin, []string{"remove", "--remove-data", "--all"})
+	assert.Error(err, "ddev remove --all --remove-data should error, but succeeded")
+	assert.Contains(out, "Illegal option")
+	assert.EqualValues(appsBefore, len(ddevapp.GetApps()), "No apps should be removed or added after ddev remove --all --remove-data")
+
+	// Ensure the --all option can remove all active apps
+	out, err = exec.RunCommand(DdevBin, []string{"remove", "--all"})
 	assert.NoError(err, "ddev remove --all should succeed but failed, err: %v, output: %s", err, out)
 	out, err = exec.RunCommand(DdevBin, []string{"list"})
 	assert.NoError(err)
 	assert.Contains(out, "no running ddev projects")
+	assert.Equal(0, len(ddevapp.GetApps()), "Not all apps were removed after ddev remove --all")
 
 	// Now put the sites back together so other tests can use them.
 	addSites()
