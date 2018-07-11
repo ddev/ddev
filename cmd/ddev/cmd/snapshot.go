177b32ef8170f88fb64e44b4773a1b51b7e20a68
Randy Fay <randy@randyfay.com>
2018-07-11 16:42:49 -0600

    Add snapshot command

---
+++ cmd/ddev/cmd/snapshot.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"github.com/drud/ddev/pkg/util"
+	"github.com/spf13/cobra"
+)
+
+var snapshotAll bool
+
+// DdevSnapshotCommand provides the snapshot command
+var DdevSnapshotCommand = &cobra.Command{
+	Use:   "snapshot [projectname]",
+	Short: "Create a database snapshot for one or more projects.",
+	Long:  `Uses mariabackup command to create a database snapshot in the .ddev/db_snapshots folder.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		apps, err := getRequestedApps(args, stopAll)
+		if err != nil {
+			util.Failed("Unable to get project(s): %v", err)
+		}
+
+		for _, app := range apps {
+			if err := app.SnapshotDatabase(); err != nil {
+				util.Failed("Failed to snapshot %s: %v", app.GetName(), err)
+			}
+		}
+	},
+}
+
+func init() {
+	DdevSnapshotCommand.Flags().BoolVarP(&stopAll, "all", "a", false, "Snapshot all running sites")
+	RootCmd.AddCommand(DdevSnapshotCommand)
+}
