709b6d3f5971c67fe97c07c6b58eeb0fa50bf483
Dominik Hahn <developer@dominikhahn.com>
2018-08-10 21:53:21 +0200

    Change the AdditionalConfiguration.php template to extend the MAAIL array instead of overwriting it

--- pkg/ddevapp/typo3.go
+++ pkg/ddevapp/typo3.go
@@ -16,7 +16,7 @@ const typo3AdditionalConfigTemplate = `<?php
 /** ` + DdevFileSignature + `: Automatically generated TYPO3 AdditionalConfiguration.php file.
  ddev manages this file and may delete or overwrite the file unless this comment is removed.
  */
- 
+
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['trustedHostsPattern'] = '.*';
 
 $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default'] = array_merge($GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default'], [
@@ -28,10 +28,8 @@ $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default'] = array_merge($GLOBA
 ]);
 
 // This mail configuration sends all emails to mailhog
-$GLOBALS['TYPO3_CONF_VARS']['MAIL'] = [
-    'transport' => 'smtp',
-    'transport_smtp_server' => 'localhost:1025',
-];
+$GLOBALS['TYPO3_CONF_VARS']['MAIL']['transport'] = 'smtp';
+$GLOBALS['TYPO3_CONF_VARS']['MAIL']['transport_smtp_server'] = 'localhost:1025';
 
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['devIPmask'] = '*';
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['displayErrors'] = 1;
