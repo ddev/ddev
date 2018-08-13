ada3f9abf8aeefa2bfa9202c8dfe518d3c7cdf23
Randy Fay <randy@randyfay.com>
2018-08-13 10:16:39 -0600

    Fix assert.Error() chokes on err == nil

--- pkg/testcommon/testcommon_test.go
+++ pkg/testcommon/testcommon_test.go
@@ -40,8 +40,10 @@ func TestTmpDir(t *testing.T) {
 	// Clean up tempoary directory and ensure it no longer exists.
 	CleanupDir(testDir)
 	_, err = os.Stat(testDir)
-	assert.Error(err, "Could not stat temporary directory")
-	assert.True(os.IsNotExist(err), "Error is of type IsNotExists")
+	assert.True(err != nil, "Could not stat temporary directory")
+	if err != nil {
+		assert.True(os.IsNotExist(err), "Error is of type IsNotExists")
+	}
 }
 
 // TestChdir tests the Chdir function and ensures it will change to a temporary directory and then properly return
@@ -139,7 +141,6 @@ func TestValidTestSite(t *testing.T) {
 	site.Cleanup()
 	_, err = os.Stat(site.Dir)
 	assert.Error(err, "Could not stat temporary directory after cleanup")
-
 }
 
 // TestGetLocalHTTPResponse() brings up a project and hits a URL to get the response
@@ -190,7 +191,7 @@ func TestGetCachedArchive(t *testing.T) {
 
 	sourceURL := "https://raw.githubusercontent.com/drud/ddev/master/.gitignore"
 	exPath, archPath, err := GetCachedArchive("TestInvalidArchive", "test", "", sourceURL)
-	assert.Error(err)
+	assert.True(err != nil)
 	assert.Contains(err.Error(), fmt.Sprintf("archive extraction of %s failed", archPath))
 
 	err = os.RemoveAll(filepath.Dir(exPath))
@@ -198,8 +199,10 @@ func TestGetCachedArchive(t *testing.T) {
 
 	sourceURL = "http://invalid_domain/somefilethatdoesnotexists"
 	exPath, archPath, err = GetCachedArchive("TestInvalidDownloadURL", "test", "", sourceURL)
-	assert.Error(err)
-	assert.Contains(err.Error(), fmt.Sprintf("Failed to download url=%s into %s", sourceURL, archPath))
+	assert.True(err != nil)
+	if err != nil {
+		assert.Contains(err.Error(), fmt.Sprintf("Failed to download url=%s into %s", sourceURL, archPath))
+	}
 
 	err = os.RemoveAll(filepath.Dir(exPath))
 	assert.NoError(err)
