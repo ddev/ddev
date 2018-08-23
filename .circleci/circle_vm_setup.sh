6e40fc161d64b77218c0004b3f9d682b6afdae6b
Randy Fay <randy@randyfay.com>
2018-08-23 17:28:50 -0600

    Have to remove the old docker-compose first

--- .circleci/circle_vm_setup.sh
+++ .circleci/circle_vm_setup.sh
@@ -15,6 +15,7 @@ sudo tar -C /usr/local -xzf /tmp/golang.tgz
 
 
 # docker-compose
+sudo rm -f /usr/local/bin/docker-compose
 sudo curl -s -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
