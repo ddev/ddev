d7de762bd31c796b14acd3aa135c3e4a114914e5
Randy Fay <randy@randyfay.com>
2018-08-23 19:07:26 -0600

    Circleci: Go back to older image and install docker ourselves (#1080)

--- .circleci/circle_vm_setup.sh
+++ .circleci/circle_vm_setup.sh
@@ -15,22 +15,22 @@ sudo tar -C /usr/local -xzf /tmp/golang.tgz
 
 
 # docker-compose
+sudo rm -f /usr/local/bin/docker-compose
 sudo curl -s -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-# Remove existing docker. This section should not be required after switch to
-# circleci/classic:201808-01 image
-#sudo apt-get remove docker docker-engine docker.io
-#sudo apt-get install \
-#    apt-transport-https \
-#    ca-certificates \
-#    curl \
-#    software-properties-common
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-#sudo add-apt-repository \
-#   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-#   $(lsb_release -cs) \
-#   stable"
-#sudo apt-get update -qq
-#sudo apt-get install -qq docker-ce
-#
+# Remove existing docker and install from their apt package
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update -qq
+sudo apt-get install -qq docker-ce
+
