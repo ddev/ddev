233e6c5155e5dc33976a20563cf098828f8336c4
Randy Fay <randy@randyfay.com>
2018-09-15 21:40:35 -0600

    Bump web container version

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -23,7 +23,7 @@ var DockerComposeFileFormatVersion = "3.6"
 var WebImg = "drud/ddev-webserver"
 
 // WebTag defines the default web image tag for drud dev
-var WebTag = "20180913_locales" // Note that this can be overridden by make
+var WebTag = "20180915_php_imagick" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "drud/ddev-dbserver"
