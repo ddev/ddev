336e67db38737dc710cf52826a69ecaad99cacff
Randy Fay <randy@randyfay.com>
2018-10-05 14:40:56 -0600

    Update dbserver to provide slow log info to stdout

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -31,7 +31,7 @@ var WebTag = "20181004_check_cms_content" // Note that this can be overridden by
 var DBImg = "drud/ddev-dbserver"
 
 // DBTag defines the default db image tag for drud dev
-var DBTag = "20180929_mariadb_stderr" // Note that this may be overridden by make
+var DBTag = "20181004_check_cms_content" // Note that this may be overridden by make
 
 // DBAImg defines the default phpmyadmin image tag used for applications.
 var DBAImg = "drud/phpmyadmin"
