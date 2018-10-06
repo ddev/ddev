cf3d7766317ca65c4e38a5b2015f62d2205b0eea
Randy Fay <randy@randyfay.com>
2018-10-06 11:29:00 -0600

    Scale back web container healthcheck to not query db container

--- containers/ddev-webserver/files/healthcheck.sh
+++ containers/ddev-webserver/files/healthcheck.sh
@@ -5,6 +5,5 @@
 set -eo pipefail
 
 curl --fail -s localhost/phpstatus >/dev/null && printf "phpstatus: OK, " || (printf "phpstatus FAILED" && exit 1)
-mysql db -e "SHOW DATABASES LIKE 'db';" >/dev/null && printf "mariadb: OK, " || (printf "mariadb FAILED" && exit 2)
-ls /var/www/html >/dev/null && printf "/var/www/html: OK, " || (printf "/var/www/html access FAILED" && exit 3)
-curl --fail -s localhost:8025 >/dev/null && printf "mailhog: OK" || (printf "mailhog FAILED" && exit 4)
+ls /var/www/html >/dev/null && printf "/var/www/html: OK, " || (printf "/var/www/html access FAILED" && exit 2)
+curl --fail -s localhost:8025 >/dev/null && printf "mailhog: OK" || (printf "mailhog FAILED" && exit 3)
