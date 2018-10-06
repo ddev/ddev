1b0e46dcb5beab822f178e0aeb75867988531b84
Randy Fay <randy@randyfay.com>
2018-10-06 13:50:39 -0600

    Add php*-sqlite3 to web container (#1150)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -25,7 +25,7 @@ var DockerComposeFileFormatVersion = "3.6"
 var WebImg = "drud/ddev-webserver"
 
 // WebTag defines the default web image tag for drud dev
-var WebTag = "20180925_php_ini_disable_functions" // Note that this can be overridden by make
+var WebTag = "20181002_sqlite" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "drud/ddev-dbserver"
