7dd7358442e4ef93142fa746282ea96510b75b5e
Randy Fay <randy@randyfay.com>
2018-10-06 20:44:59 -0600

    Update the base db to be from 10.2 AND have db_mariadb_version.txt in it

--- containers/ddev-dbserver/files/create_base_db.sh
+++ containers/ddev-dbserver/files/create_base_db.sh
@@ -71,7 +71,7 @@ mariabackup --backup --target-dir=$OUTDIR --user root --password root --socket=$
 
 # Initialize with current mariadb_version
 my_mariadb_version=$(mysql -V | awk '{sub( /\.[0-9]+-MariaDB,/, ""); print $5 }')
-echo $my_mariadb_version >/var/lib/mysql/db_mariadb_version.txt
+echo $my_mariadb_version >$OUTDIR/db_mariadb_version.txt
 
 if ! kill -s TERM "$pid" || ! wait "$pid"; then
 	echo >&2 'Mariadb initialization process failed.'
