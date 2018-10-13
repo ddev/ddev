490f263060623aae019fe3f4ca987374bac705a2
Andrew French <andrew@afren.ch>
2018-10-13 15:10:33 -0600

    Add command to output final config given to docker compose

---
+++ cmd/ddev/cmd/compose-config.go
@@ -0,0 +1,50 @@
+package cmd
+
+import (
+	"strings"
+
+	"github.com/drud/ddev/pkg/ddevapp"
+	"github.com/drud/ddev/pkg/dockerutil"
+	"github.com/drud/ddev/pkg/output"
+	"github.com/drud/ddev/pkg/util"
+	"github.com/spf13/cobra"
+)
+
+var ComposeConfigCmd = &cobra.Command{
+	Use:   "compose-config [project]",
+	Short: "Prints the docker-compose configuration of the current project",
+	Run: func(cmd *cobra.Command, args []string) {
+		projectName := ""
+
+		if len(args) > 1 {
+			util.Failed("This command only takes one optional argument: project name")
+		}
+
+		if len(args) == 1 {
+			projectName = args[0]
+		}
+
+		app, err := ddevapp.GetActiveApp(projectName)
+		if err != nil {
+			util.Failed("Failed to get compose-config: %v", err)
+		}
+
+		app.DockerEnv()
+		files, err := app.ComposeFiles()
+		if err != nil {
+			util.Failed("Failed to get compose-config: %v", err)
+		}
+
+		out, _, err := dockerutil.ComposeCmd(files, "config")
+		if err != nil {
+			util.Failed("Failed to get compose-config: %v", err)
+		}
+
+		output.UserOut.Printf(strings.TrimSpace(out))
+	},
+}
+
+func init() {
+	ComposeConfigCmd.Hidden = true
+	RootCmd.AddCommand(ComposeConfigCmd)
+}
