07b479859e90979e9c612f2073f2d63df7d74031
Andrew French <andrew@afren.ch>
2018-10-22 09:49:13 -0600

    Linteroni

--- cmd/ddev/cmd/debug.go
+++ cmd/ddev/cmd/debug.go
@@ -1,12 +1,16 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/drud/ddev/pkg/util"
+	"github.com/spf13/cobra"
+)
 
 var DebugCmd = &cobra.Command{
 	Use:   "debug [command]",
 	Short: "A collection of debugging commands",
 	Run: func(cmd *cobra.Command, args []string) {
-		cmd.Usage()
+		err := cmd.Usage()
+		util.CheckErr(err)
 	},
 }
 
