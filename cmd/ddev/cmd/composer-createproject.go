ecfa38dfa5920d7f9608f5cb51efee042ae6f1e3
Andrew French <andrew@afren.ch>
2018-11-07 11:01:36 -0900

    Improve create-project

--- cmd/ddev/cmd/composer-createproject.go
+++ cmd/ddev/cmd/composer-createproject.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	"github.com/drud/ddev/pkg/ddevapp"
+	"github.com/drud/ddev/pkg/output"
 	"github.com/drud/ddev/pkg/util"
 	"github.com/spf13/cobra"
 )
@@ -53,26 +54,44 @@ var ComposerCreateProjectCmd = &cobra.Command{
 		}
 
 		// Make the user confirm that existing contents will be deleted
-		util.Warning("Warning: Existing contents of the project root will be overwritten")
-		for {
-			r := strings.ToLower(util.Prompt("Would you like to continue? [Y/n]", "yes"))
-			if r == "no" || r == "n" {
+		// If the user hasn't passed any positional arguments, we're not actually executing an
+		// install and nothing will be deleted -- for example, 'ddev composer create-project -h'
+		removeData := false
+		if len(positionals) > 0 {
+			util.Warning("Warning: Any existing contents of the project root will be overwritten")
+			removeData = true
+			if !util.Confirm("Would you like to continue?") {
 				util.Failed("create-project cancelled")
-			}
-
-			if r == "yes" || r == "y" {
-				break
 			}
 		}
 
+		// Only remove data if we expect an install
+		composerTmpDir := "/tmp/composer"
+		if removeData {
+			// The install directory may be populated if a previous create-project was
+			// executed using the same container.
+			output.UserOut.Printf("Ensuring composer install directory is empty")
+			_, _, _ = app.Exec(&ddevapp.ExecOpts{
+				Service: "web",
+				Cmd:     []string{"sh", "-c", fmt.Sprintf("rm -rf %s", composerTmpDir)},
+			})
+
+			// Remove any contents of project root
+			util.Warning("Removing any existing files in project root")
+			_, _, _ = app.Exec(&ddevapp.ExecOpts{
+				Service: "web",
+				Cmd:     []string{"sh", "-c", "rm -rf /var/www/html/*"},
+			})
+		}
+
 		// Build container composer command
-		installDir := path.Join("/tmp/composer", dir)
+		installDir := path.Join(composerTmpDir, dir)
 		composerCmd := append([]string{}, "composer", "create-project")
 		composerCmd = append(composerCmd, options...)
 		composerCmd = append(composerCmd, pack, installDir, ver)
 
-		fmt.Println(composerCmd)
-		_, _, err = app.Exec(&ddevapp.ExecOpts{
+		output.UserOut.Printf("Executing composer command: %s\n", strings.Join(composerCmd, " "))
+		stdout, _, err := app.Exec(&ddevapp.ExecOpts{
 			Service: "web",
 			Cmd:     composerCmd,
 		})
@@ -80,16 +99,22 @@ var ComposerCreateProjectCmd = &cobra.Command{
 			util.Failed("Failed to execute create-project command")
 		}
 
-		// Move project to app root
-		_, _, _ = app.Exec(&ddevapp.ExecOpts{
-			Service: "web",
-			Cmd:     []string{"sh", "-c", "if [ -d /tmp/composer ]; then mv /tmp/composer/* /var/www/html/; fi"},
-		})
+		if len(stdout) > 0 {
+			fmt.Println(stdout)
+		}
+
+		// Move project to app root if an install is underway
+		if removeData {
+			output.UserOut.Printf("Moving installation to project root")
+			_, _, _ = app.Exec(&ddevapp.ExecOpts{
+				Service: "web",
+				Cmd:     []string{"sh", "-c", fmt.Sprintf("if [ -d %s ]; then mv %s /var/www/html/; fi", composerTmpDir, path.Join(composerTmpDir, "*"))},
+			})
+		}
 	},
 }
 
 func init() {
 	ComposerCmd.AddCommand(ComposerCreateProjectCmd)
 	ComposerCreateProjectCmd.DisableFlagParsing = true
-	ComposerCreateProjectCmd.Flags().SetInterspersed(false)
 }
