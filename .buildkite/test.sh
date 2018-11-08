b85eb73986b4efca7c16e25c2143c07d972e4711
Randy Fay <randy@randyfay.com>
2018-11-08 10:10:13 -0700

    Make sure buildkite agent startup fails if removal of ~/.ddev/Test* fails

--- .buildkite/test.sh
+++ .buildkite/test.sh
@@ -20,16 +20,16 @@ if [ "$(docker ps -aq | wc -l)" -gt 0 ] ; then
 	docker rm -f $(docker ps -aq)
 fi
 docker system prune --volumes --force
-rm -rf ~/.ddev/TestPkg*
 
 # Update all images that could have changed
 docker images | awk '/drud/ {print $1":"$2 }' | xargs -L1 docker pull
-rm -rf ~/.ddev/Test*
 
 set -o errexit
 set -o pipefail
 set -o nounset
 set -x
+
+rm -rf ~/.ddev/Test*
 
 # Our testbot should now be sane, run the testbot checker to make sure.
 ./.buildkite/sanetestbot.sh
