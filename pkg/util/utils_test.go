73b8b443ca3c0bd5ae6d96998f46794903a5bd97
Andrew French <andrew@afren.ch>
2018-11-13 15:13:47 -0700

    Unset/reset env var for test.

--- pkg/util/utils_test.go
+++ pkg/util/utils_test.go
@@ -3,6 +3,7 @@ package util_test
 import (
 	"bufio"
 	"fmt"
+	"os"
 	"strings"
 	"testing"
 
@@ -84,6 +85,13 @@ func TestCaptureStdOut(t *testing.T) {
 // TestConfirm ensures that the confirmation prompt works as expected.
 func TestConfirm(t *testing.T) {
 	assert := asrt.New(t)
+
+	noninteractiveEnv := "DRUD_NONINTERACTIVE"
+	defer os.Setenv(noninteractiveEnv, os.Getenv(noninteractiveEnv))
+	err := os.Unsetenv(noninteractiveEnv)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
 
 	yesses := []string{"YES", "Yes", "yes", "Y", "y"}
 	for _, y := range yesses {
