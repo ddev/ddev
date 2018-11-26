17e1f4e56d265e4417c55b9a91a4ddf98514e3a7
Randy Fay <randy@randyfay.com>
2018-11-26 07:50:32 -0700

    Bump container versions

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -27,13 +27,13 @@ var DockerComposeFileFormatVersion = "3.6"
 var WebImg = "drud/ddev-webserver"
 
 // WebTag defines the default web image tag for drud dev
-var WebTag = "v1.4.0" // Note that this can be overridden by make
+var WebTag = "readd_apt_user_to_etc_passwd_file" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "drud/ddev-dbserver"
 
 // DBTag defines the default db image tag for drud dev
-var DBTag = "v1.4.0" // Note that this may be overridden by make
+var DBTag = "readd_apt_user_to_etc_passwd_file" // Note that this may be overridden by make
 
 // DBAImg defines the default phpmyadmin image tag used for applications.
 var DBAImg = "drud/phpmyadmin"
