fc1f587fbe01985d781665ea47fa794e58840723
Randy Fay <randy@randyfay.com>
2018-12-07 09:51:28 -0700

    Add sync nuance to healthcheck for bgsync

--- containers/ddev-bgsync/files/healthcheck.sh
+++ containers/ddev-bgsync/files/healthcheck.sh
@@ -9,36 +9,34 @@ set -o errexit
 
 
 if [ ! -f "/var/tmp/unison_start_authorized" ] ; then
-  echo "unison start has not yet been authorized"
+  echo -n "unison start has not yet been authorized"
   exit 101
 fi
 
 if ! pkill -0 unison ; then
-  echo "unison does not appear to be running"
+  echo -n "unison does not appear to be running"
   exit 102
 fi
 
+WAIT_FOR_SYNC=2
+CHECKFILE="healthcheck.$(date +%Y%m%d%H%M%S)"
+
+SYNC_SOURCE=${SYNC_SOURCE:-/source}
+SYNC_DESTINATION=${SYNC_DESTINATION:-/destination}
+HEALTHCHECK_DIR=.bgsync_healthcheck
+
+function cleanup {
+    rm -f ${SYNC_SOURCE}/${HEALTHCHECK_DIR}/healthcheck.* ${SYNC_DESTINATION}/${HEALTHCHECK_DIR}/healthcheck.*
+}
+trap cleanup EXIT
+
+mkdir -p "$SYNC_DESTINATION/$HEALTHCHECK_DIR" "$SYNC_SOURCE/$HEALTHCHECK_DIR"
+touch "${SYNC_SOURCE}/${HEALTHCHECK_DIR}/$CHECKFILE" && sleep "$WAIT_FOR_SYNC"
+if [ ! -f "${SYNC_DESTINATION}/${HEALTHCHECK_DIR}/$CHECKFILE" ]; then
+  echo -n "Sync not yet fully active"
+else
+    echo -n "Sync active"
+    touch /var/tmp/sync_active.txt
+fi
+
 exit 0
-
-
-# Below is a more complex actual-usage approach that we might want to redo later
-
-#WAIT_FOR_SYNC=2
-#CHECKFILE="healthcheck.$(date +%Y%m%d%H%M%S)"
-#
-#SYNC_SOURCE=${SYNC_SOURCE:-/source}
-#SYNC_DESTINATION=${SYNC_DESTINATION:-/destination}
-#HEALTHCHECK_DIR=.bgsync_healthcheck
-#
-#function cleanup {
-#    rm -f ${SYNC_SOURCE}/${HEALTHCHECK_DIR}/healthcheck.* ${SYNC_DESTINATION}/${HEALTHCHECK_DIR}/healthcheck.*
-#}
-#trap cleanup EXIT
-#
-#mkdir -p "$SYNC_DESTINATION/$HEALTHCHECK_DIR" "$SYNC_SOURCE/$HEALTHCHECK_DIR"
-#touch "${SYNC_SOURCE}/${HEALTHCHECK_DIR}/$CHECKFILE" && sleep "$WAIT_FOR_SYNC"
-#if [ ! -f "${SYNC_DESTINATION}/${HEALTHCHECK_DIR}/$CHECKFILE" ]; then
-#  echo "Sync is not working after $WAIT_FOR_SYNC seconds"
-#  exit 101
-#fi
-#echo "Sync successful for $CHECKFILE"
