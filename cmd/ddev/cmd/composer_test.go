39e840ec578141908bb1b65cf0187821a4d5e5b8
Randy Fay <randy@randyfay.com>
2018-12-10 20:29:24 -0700

    Make composer create execute inside container if webcache is on, add test content

--- cmd/ddev/cmd/composer_test.go
+++ cmd/ddev/cmd/composer_test.go
@@ -1,11 +1,14 @@
 package cmd
 
 import (
+	"github.com/drud/ddev/pkg/ddevapp"
+	"github.com/drud/ddev/pkg/fileutil"
+	"github.com/drud/ddev/pkg/testcommon"
 	"github.com/drud/ddev/pkg/util"
 	"os"
+	"path/filepath"
 	"testing"
-
-	"github.com/drud/ddev/pkg/testcommon"
+	"time"
 
 	"github.com/drud/ddev/pkg/exec"
 	asrt "github.com/stretchr/testify/assert"
@@ -34,18 +37,26 @@ func TestComposerCmd(t *testing.T) {
 	assert.NoError(err)
 	assert.Contains(out, "Available commands:")
 
+	// Get an app just so we can do waits and check webcacheenabled etc.
+	app, err := ddevapp.NewApp(tmpDir, "")
+	assert.NoError(err)
+
 	// Test create-project
 	// ddev composer create cweagans/composer-patches --prefer-dist --no-interaction
 	args = []string{"composer", "create", "--prefer-dist", "--no-interaction", "--no-dev", "psr/log", "1.1.0"}
 	out, err = exec.RunCommand(DdevBin, args)
 	assert.NoError(err, "failed to run %v: err=%v, output=\n=====\n%s\n=====\n", args, out)
 	assert.Contains(out, "Created project in ")
+	waitForSync(app, 2)
+	assert.FileExists(filepath.Join(tmpDir, "Psr/Log/LogLevel.php"))
 
 	// Test a composer require, with passthrough args
 	args = []string{"composer", "require", "sebastian/version", "--no-plugins", "--ansi"}
 	out, err = exec.RunCommand(DdevBin, args)
 	assert.NoError(err, "failed to run %v: err=%v, output=\n=====\n%s\n=====\n", args, out)
 	assert.Contains(out, "Generating autoload files")
+	waitForSync(app, 2)
+	assert.FileExists(filepath.Join(tmpDir, "vendor/sebastian/version/composer.json"))
 
 	// Test a composer remove
 	if util.IsDockerToolbox() {
@@ -57,4 +68,16 @@ func TestComposerCmd(t *testing.T) {
 	out, err = exec.RunCommand(DdevBin, args)
 	assert.NoError(err, "failed to run %v: err=%v, output=\n=====\n%s\n=====\n", args, out)
 	assert.Contains(out, "Generating autoload files")
+	waitForSync(app, 2)
+	assert.False(fileutil.FileExists(filepath.Join(tmpDir, "vendor/sebastian")))
+}
+
+// waitForSync is a test helper; it's hard to know exactly when the bgsync
+// container will have completed syncing an operation, so we do app.WaitSync() and
+// add the number of seconds provided.
+func waitForSync(app *ddevapp.DdevApp, seconds int) {
+	if app.WebcacheEnabled {
+		_ = app.WaitSync()
+		time.Sleep(time.Duration(seconds) * time.Second)
+	}
 }
