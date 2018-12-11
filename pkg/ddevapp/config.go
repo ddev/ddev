9dbc3ecfe76648e15700684fac6344422ba057a1
Randy Fay <randy@randyfay.com>
2018-12-10 20:46:19 -0700

    Fix typo introduced in rebase

--- pkg/ddevapp/config.go
+++ pkg/ddevapp/config.go
@@ -233,7 +233,7 @@ func (app *DdevApp) ReadConfig() error {
 
 	if app.DBAImage == "" {
 		app.DBAImage = version.GetDBAImage()
-	}'
+	}
 	if app.BgsyncImage == "" {
 		app.BgsyncImage = version.GetBgsyncImage()
 	}
