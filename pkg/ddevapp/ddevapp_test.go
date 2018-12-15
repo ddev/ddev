fd43edf70ca1c77ec66386f92f69c7c51d632272
Randy Fay <randy@randyfay.com>
2018-12-14 19:20:51 -0700

    Attempt to figure out why TestDdevOldMariadb() is failing

--- pkg/ddevapp/ddevapp_test.go
+++ pkg/ddevapp/ddevapp_test.go
@@ -666,13 +666,24 @@ func TestDdevOldMariaDB(t *testing.T) {
 	testcommon.ClearDockerEnv()
 	err := app.Init(site.Dir)
 	assert.NoError(err)
+
+	// Make sure there isn't an old db laying around
+	_ := dockerutil.RemoveVolume(app.Name + "-mariadb")
+	//nolint: errcheck
+	defer dockerutil.RemoveVolume(app.Name + "-mariadb")
+
 	app.MariaDBVersion = ddevapp.MariaDB101
 	app.DBImage = version.GetDBImage(app.MariaDBVersion)
-	err = app.StartAndWaitForSync(2)
+	startErr := app.StartAndWaitForSync(5)
+	if startErr != nil {
+		appLogs, err := ddevapp.GetErrLogsFromApp(app, startErr)
+		assert.NoError(err)
+		require.NoError(t, err, "app start failure; logs:\n=====\n%s\n=====\n", appLogs)
+	}
+
 	//nolint: errcheck
 	defer app.Down(true, false)
 
-	require.NoError(t, err)
 	importPath := filepath.Join(testDir, "testdata", "users.sql")
 	err = app.ImportDB(importPath, "")
 	require.NoError(t, err)
