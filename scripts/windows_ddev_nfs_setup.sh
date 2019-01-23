664e5071bb1b7a371b5b461a01d5824a2a4b52e5
Randy Fay <randy@randyfay.com>
2019-01-23 15:42:29 -0700

    Improve windows ddev nfs install script

--- scripts/windows_ddev_nfs_setup.sh
+++ scripts/windows_ddev_nfs_setup.sh
@@ -6,16 +6,23 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-if [ $(nssm status nfsd) = "SERVICE_RUNNING" ] ; then
-    echo "nfsd is already running. You can configure its behavior with 'nssm edit nfsd'." && exit 0
+status=uninstalled
+if nssm status nfsd 2>/dev/null ; then
+    status=$(nssm status nfsd)
+fi
+if [ "$status" = "SERVICE_RUNNING" ] ; then
+    echo "nfsd is already running. You can configure its behavior with 'nssm edit nfsd'."
+    exit 0
 fi
 
-if [ $(nssm status nfsd) = "SERVICE_STOPPED" ] ; then
-    sudo nssm start nfsd && ( echo "nfsd was already installed, starting it. You can configure its behavior with 'nssm edit nfsd'" && exit 0)
+if [ "$status" = "SERVICE_STOPPED" ] ; then
+    echo "nfsd was already installed, starting it. You can configure its behavior with 'nssm edit nfsd'"
+    sudo nssm start nfsd
+    exit 0
 fi
 
 sudo nssm install nfsd "C:\Program Files\ddev\winnfsd.exe" -log off "C:\\Users"
 sudo nssm start nfsd
 
-echo "winnfsd has been installed serving C:\Users"
-echo "You can edit its behavior with 'nssm edit nfsd'"
+echo "winnfsd has been installed as service nfsd serving C:\Users"
+echo "You can edit its behavior with 'sudo nssm edit nfsd'"
