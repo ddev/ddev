fb54142d15c3957bb5dd5dafdfa7394648e3f11f
Randy Fay <randy@randyfay.com>
2019-01-23 21:37:20 -0700

    Delete the nfs volume on start in case it's hanging around

--- pkg/ddevapp/ddevapp.go
+++ pkg/ddevapp/ddevapp.go
@@ -710,9 +710,10 @@ func (app *DdevApp) Start() error {
 		return err
 	}
 
-	// Delete the webcachevol before we bring up docker-compose.
+	// Delete the webcachevol and NFS volumes before we bring up docker-compose.
 	// We don't care if the volume wasn't there
 	_ = dockerutil.RemoveVolume(app.GetWebcacheVolName())
+	_ = dockerutil.RemoveVolume(app.GetNFSMountVolName())
 
 	_, _, err = dockerutil.ComposeCmd(files, "up", "-d")
 	if err != nil {
