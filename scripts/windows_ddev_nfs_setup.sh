5002e161159e7f11661e0522ea1fd83b2ce98513
Randy Fay <randy@randyfay.com>
2019-01-24 12:24:51 -0700

    Windows nfs mount should be of uid 1000 in setup script

--- scripts/windows_ddev_nfs_setup.sh
+++ scripts/windows_ddev_nfs_setup.sh
@@ -6,6 +6,11 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+# Currently Windows ddev containers run as UID 1000
+# We want the NFS mount to do the same.
+DDEV_WINDOWS_UID=1000
+DDEV_WINDOWS_GID=1000
+
 status=uninstalled
 if nssm status nfsd 2>/dev/null ; then
     status=$(nssm status nfsd)
@@ -21,7 +26,7 @@ if [ "$status" = "SERVICE_STOPPED" ] ; then
     exit 0
 fi
 
-sudo nssm install nfsd "C:\Program Files\ddev\winnfsd.exe" -log off "C:\\Users"
+sudo nssm install nfsd "C:\Program Files\ddev\winnfsd.exe" -id ${DDEV_WINDOWS_UID} ${DDEV_WINDOWS_GID} -log off "C:\\Users"
 sudo nssm start nfsd
 
 echo "winnfsd has been installed as service nfsd serving C:\Users"
