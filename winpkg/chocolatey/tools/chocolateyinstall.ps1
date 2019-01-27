13ddb0c9008a41ddbda33afd97ffeb1a1902a0cc
Randy Fay <randy@randyfay.com>
2019-01-27 13:48:36 -0700

    Add chocolatey basic package

---
+++ winpkg/chocolatey/tools/chocolateyinstall.ps1
@@ -0,0 +1,43 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64      = 'https://github.com/drud/ddev/releases/download/v1.5.2/ddev_windows_installer.v1.5.2.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'ddev*'
+
+  checksum      = ''
+  checksumType  = 'sha256'
+  checksum64    = ''
+  checksumType64= 'sha256'
+
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
