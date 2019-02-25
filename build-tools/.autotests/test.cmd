6f0a71f619bcc7efea17f67803877c614757270b
Randy Fay <randy@randyfay.com>
2019-02-25 16:55:54 -0700

    Remove build-tools .autotests which should not have come over

--- build-tools/.autotests/test.cmd
+++
@@ -1,9 +0,0 @@
-@echo "Building using bash and build.sh PWD=%cd%"
-"C:\Program Files\git\bin\bash" .autotests/test.sh
-
-if %ERRORLEVEL% EQU 0 (
-   @echo Successful build
-) else (
-   @echo Failure Reason Given is %errorlevel%
-   exit /b %errorlevel%
-)
