a39441988f2d34fdb32ccc21f6ce94eae3106509
Randy Fay <randy@randyfay.com>
2019-03-05 07:31:45 -0700

    Temporary set -x to see what's wrong with nfs addition

--- .autotests/nfstest.sh
+++ .autotests/nfstest.sh
@@ -26,6 +26,7 @@ esac
 # Find host.docker.internal name using host-docker-internal.sh script
 hostDockerInternal=$($(dirname $0)/../scripts/host-docker-internal.sh)
 
+set -x
 docker volume create --driver local --opt type=nfs --opt o=addr=${hostDockerInternal},hard,nolock,rw --opt device=:${share} nfstest >/dev/null
 docker run -t --rm -v nfstest:/tmp/nfs busybox ls //tmp/nfs >/dev/null
 echo "nfsd seems to be set up ok"
