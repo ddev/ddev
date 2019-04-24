73430e83910fc0f7359c8e221482545078f01e9d
Randy Fay <randy@randyfay.com>
2019-04-24 11:37:25 -0600

    Fix OS detection for windows in container test

--- containers/ddev-router/test/containertest.sh
+++ containers/ddev-router/test/containertest.sh
@@ -7,8 +7,6 @@ set -x
 
 DOCKER_IMAGE=$(awk '{print $1}' .docker_image)
 CONTAINER_NAME=ddev-router-test
-OS=$(uname)
-if [[ ${OS} == "MINGW*" ]]; then OS="windows"; fi
 
 # Wait for container to be ready.
 function containercheck {
@@ -56,7 +54,7 @@ fi
 # Make sure we can access http and https ports successfully (and with valid cert)
 (curl -s -I http://127.0.0.1:8080 | grep 503) || (echo "Failed to get 503 from nginx-router by default" && exit 102)
 # mkcert is not respected by git-bash curl, so don't try the test on windows.
-if [ ${OS} != "windows" ]; then
+if [ "${OS}" != "Windows_NT" ]; then
     (curl -s -I https://127.0.0.1:8443 | grep 503) || (echo "Failed to get 503 from nginx-router via https by default" && exit 103)
 fi
 # Make sure internal access to https is working
