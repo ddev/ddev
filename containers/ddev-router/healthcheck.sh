4da26ec1215cbf9b682c8aa4731066ba428def86
Randy Fay <randy@randyfay.com>
2019-04-29 11:25:47 -0600

    curl in healthcheck should give better information

--- containers/ddev-router/healthcheck.sh
+++ containers/ddev-router/healthcheck.sh
@@ -5,4 +5,4 @@ set -o pipefail
 # Check nginx config
 nginx -t || exit 1
 # Check our healthcheck endpoint
-curl -s --fail --connect-timeout 2 --retry 2 http://127.0.0.1/healthcheck || (echo "ddev-router healthcheck endpoint not responding" && exit 2)
+curl --fail --connect-timeout 2 --retry 2 http://127.0.0.1/healthcheck || (echo "ddev-router healthcheck endpoint not responding" && exit 2)
