41e9954367773cd3d9b7ee5e001fe86db5f6a3e0
Randy Fay <randy@randyfay.com>
2019-05-01 20:58:22 -0600

    apache-cgi should respect php overrides, fixes #1556 (#1557)
    * apache-cgi should respect php overrides, fixes #1556
    
    * Add TestPHPOverrides test

--- pkg/ddevapp/config_test.go
+++ pkg/ddevapp/config_test.go
@@ -3,6 +3,7 @@ package ddevapp_test
 import (
 	"bufio"
 	"fmt"
+	"github.com/drud/ddev/pkg/nodeps"
 	"github.com/stretchr/testify/require"
 	"io/ioutil"
 	"os"
@@ -607,6 +608,60 @@ func TestConfigOverrideDetection(t *testing.T) {
 		assert.NotContains(out, "nginx-site.conf")
 	}
 	assert.Contains(out, "Custom configuration takes effect")
+	runTime()
+}
+
+// TestPHPOverrides tests to make sure that PHP overrides work in all webservers.
+func TestPHPOverrides(t *testing.T) {
+	assert := asrt.New(t)
+	app := &DdevApp{}
+	testDir, _ := os.Getwd()
+
+	site := TestSites[0]
+	switchDir := site.Chdir()
+	defer switchDir()
+
+	runTime := testcommon.TimeTrack(time.Now(), fmt.Sprintf("%s PHPOverrides", site.Name))
+
+	// Copy test overrides into the project .ddev directory
+	err := fileutil.CopyDir(filepath.Join(testDir, "testdata/TestPHPOverrides/.ddev/php"), filepath.Join(site.Dir, ".ddev/php"))
+	assert.NoError(err)
+	err = fileutil.CopyFile(filepath.Join(testDir, "testdata/TestPHPOverrides/phpinfo.php"), filepath.Join(site.Dir, site.Docroot, "phpinfo.php"))
+	assert.NoError(err)
+
+	// And when we're done, we have to clean those out again.
+	defer func() {
+		_ = os.RemoveAll(filepath.Join(site.Dir, ".ddev/php"))
+		_ = os.RemoveAll(filepath.Join(site.Dir, "phpinfo.php"))
+	}()
+
+	for _, webserverType := range []string{WebserverNginxFPM, WebserverApacheFPM, WebserverApacheCGI} {
+		testcommon.ClearDockerEnv()
+		app.WebserverType = webserverType
+		err = app.Init(site.Dir)
+		assert.NoError(err)
+		_ = app.Stop(true, false)
+		// nolint: errcheck
+		defer app.Stop(true, false)
+		startErr := app.StartAndWaitForSync(2)
+		if startErr != nil {
+			logs, _ := GetErrLogsFromApp(app, startErr)
+			t.Logf("failed app.StartAndWait(): %v", startErr)
+			t.Fatalf("============== logs from app.StartAndWait() ==============\n%s\n", logs)
+		}
+
+		// On Docker Toolbox, it appearas that the change notification gets to the router
+		// slower than on other platforms. Give it time to come through.
+		// Otherwise the SSL cert may not yet have been created
+		if nodeps.IsDockerToolbox() {
+			time.Sleep(time.Duration(5) * time.Second)
+		}
+
+		_, _ = testcommon.EnsureLocalHTTPContent(t, "http://"+app.GetHostname()+"/phpinfo.php", `max_input_time</td><td class="v">999`)
+		err = app.Stop(true, false)
+		assert.NoError(err)
+	}
+
 	runTime()
 }
 
