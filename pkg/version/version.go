8727e547ba28f069c0f100552ec594ceab9b8503
Randy Fay <randy@randyfay.com>
2019-05-10 07:47:42 -0600

    Bump container versions to v1.8.0

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -45,35 +45,35 @@ var DockerComposeFileFormatVersion = "3.6"
 var WebImg = "drud/ddev-webserver"
 
 // WebTag defines the default web image tag for drud dev
-var WebTag = "nginx-dont-hide-upstream-errors" // Note that this can be overridden by make
+var WebTag = "v1.8.0" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "drud/ddev-dbserver"
 
 // BaseDBTag is the main tag, DBTag is constructed from it
-var BaseDBTag = "20190430_use_build_for_dynamic_containers"
+var BaseDBTag = "v1.8.0"
 
 // DBAImg defines the default phpmyadmin image tag used for applications.
 var DBAImg = "drud/phpmyadmin"
 
 // DBATag defines the default phpmyadmin image tag used for applications.
-var DBATag = "v1.7.0" // Note that this can be overridden by make
+var DBATag = "v1.8.0" // Note that this can be overridden by make
 
 // BgsyncImg defines the default bgsync image tag used for applications.
 var BgsyncImg = "drud/ddev-bgsync"
 
 // BgsyncTag defines the default phpmyadmin image tag used for applications.
-var BgsyncTag = "v1.7.0" // Note that this can be overridden by make
+var BgsyncTag = "v1.8.0" // Note that this can be overridden by make
 
 // RouterImage defines the image used for the router.
 var RouterImage = "drud/ddev-router"
 
 // RouterTag defines the tag used for the router.
-var RouterTag = "20190417_trust_ssl_certs" // Note that this can be overridden by make
+var RouterTag = "v1.8.0" // Note that this can be overridden by make
 
 var SSHAuthImage = "drud/ddev-ssh-agent"
 
-var SSHAuthTag = "v1.7.0"
+var SSHAuthTag = "v1.8.0"
 
 // COMMIT is the actual committish, supplied by make
 var COMMIT = "COMMIT should be overridden"
