fd8535f650c8b0aedecb7a22f94410978e19db27
Randy Fay <randy@randyfay.com>
2019-05-11 15:48:00 -0600

    Remove --yes drush behavior when in ddev exec behavior, fixes #1584

--- containers/ddev-webserver/files/etc/drush/drushrc.php
+++ containers/ddev-webserver/files/etc/drush/drushrc.php
@@ -2,7 +2,3 @@
 if (!empty($_ENV['DDEV_URL'])) {
     $options['uri'] = $_ENV['DDEV_URL'];
 }
-# Skip confirmations since `ddev exec` cannot support interactive prompts
-if (!empty($_ENV['DDEV_EXEC'])) {
-    $options['yes'] = 1;
-}
