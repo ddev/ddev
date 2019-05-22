ae1940866813afa426245ac32d37e0280be79f1a
Randy Fay <randy@randyfay.com>
2019-05-22 13:08:57 -0600

    Make sure internet is active before trying operations that might hang, fixes #1601, Fixes #431 (#1607)

--- pkg/nodeps/utils.go
+++ pkg/nodeps/utils.go
@@ -1,6 +1,9 @@
 package nodeps
 
-import "os"
+import (
+	"net"
+	"os"
+)
 
 // ArrayContainsString returns true if slice contains element
 func ArrayContainsString(slice []string, element string) bool {
@@ -28,4 +31,12 @@ func IsDockerToolbox() bool {
 		return true
 	}
 	return false
+}
+
+//IsInternetActive() checks to see if we have a viable
+// internet connection. It just tries a quick DNS query.
+// This requires that the named record be query-able.
+func IsInternetActive() bool {
+	_, err := net.LookupHost("i-exist.ddev.site")
+	return err == nil
 }
