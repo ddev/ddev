d52a67005bdd8e7849b6333f85f1e80d0c0b156b
Randy Fay <randy@randyfay.com>
2019-05-23 12:22:00 -0600

    Linting failures

--- pkg/ddevapp/config_test.go
+++ pkg/ddevapp/config_test.go
@@ -139,7 +139,7 @@ func TestHostName(t *testing.T) {
 	assert.NoError(err)
 	app.Name = util.RandString(32)
 
-	assert.Equal(app.GetHostname(), app.Name+"."+version.DDevTLD)
+	assert.Equal(app.GetHostname(), app.Name+"."+app.ProjectTLD)
 }
 
 // TestWriteDockerComposeYaml tests the writing of a docker-compose.yaml file.
