d60480f08c7d91973386c136cb2b8428fbd45972
Randy Fay <randy@randyfay.com>
2019-05-27 16:12:45 -0600

    drushrc.php had go syntax, not php :)

--- pkg/ddevapp/drupal.go
+++ pkg/ddevapp/drupal.go
@@ -525,7 +525,7 @@ func WriteDrushrc(app *DdevApp, filePath string) error {
 /** ` + DdevFileSignature + `: Automatically generated drushrc.php file.
  ddev manages this file and may delete or overwrite the file unless this comment is removed.
  */
-options['uri'] = "` + uri + `";
+$options['l'] = "` + uri + `";
 `)
 
 	// Ensure target directory exists and is writable
