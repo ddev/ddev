6f105fb0ecaa744fa9babf14626786c127bb3cff
Randy Fay <randy@randyfay.com>
2019-06-18 12:32:03 -0600

    Add hirak/prestissimo, fixes #1650

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -45,7 +45,7 @@ var DockerComposeFileFormatVersion = "3.6"
 var WebImg = "drud/ddev-webserver"
 
 // WebTag defines the default web image tag for drud dev
-var WebTag = "20190603_drush_launcher" // Note that this can be overridden by make
+var WebTag = "20190618_prestissimo" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "drud/ddev-dbserver"
