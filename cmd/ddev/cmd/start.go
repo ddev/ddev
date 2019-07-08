385319eeb4c5d1d864ef2f85b686334b8c9a2134
Randy Fay <randy@randyfay.com>
2019-07-08 12:48:19 -0600

    Refactor segment into own file

--- cmd/ddev/cmd/start.go
+++ cmd/ddev/cmd/start.go
@@ -8,7 +8,6 @@ import (
 	"github.com/drud/ddev/pkg/version"
 	"github.com/getsentry/raven-go"
 	"gopkg.in/segmentio/analytics-go.v3"
-	"runtime"
 	"strings"
 
 	"github.com/drud/ddev/pkg/dockerutil"
@@ -38,7 +37,7 @@ any directory by running 'ddev start projectname [projectname ...]'`,
 		}
 
 		// Look for version change and opt-in Sentry if it has changed.
-		err = checkDdevVersionAndOptInSentry()
+		err = checkDdevVersionAndOptInInstrumentation()
 		if err != nil {
 			util.Failed(err.Error())
 		}
@@ -59,41 +58,41 @@ any directory by running 'ddev start projectname [projectname ...]'`,
 			if project.WebcacheEnabled {
 				util.Warning("All contents were copied to fast docker filesystem,\nbut bidirectional sync operation may not be fully functional for a few minutes.")
 			}
+
+			reportInstrumentation(projects)
 		}
 	},
 	PostRun: func(cmd *cobra.Command, args []string) {
-		sentryNotSetupWarning()
-
-		if globalconfig.DdevGlobalConfig.InstrumentationOptIn && nodeps.IsInternetActive() {
-			_ = raven.CaptureMessageAndWait("Usage: ddev start", map[string]string{"severity-level": "info", "report-type": "usage"})
-
-			client := analytics.New("avAqwwHH835Tu73GxeVmEhbIpqRL9b4Q")
-			hashedID, err := machineid.ProtectedID("myAppName")
-			err = client.Enqueue(analytics.Identify{
-				UserId: hashedID,
-				Traits: analytics.NewTraits().
-					Set("OS", runtime.GOOS).
-					Set("ddev-version", version.VERSION),
-			})
-
-			if err != nil {
-				util.Warning("err: %v", err)
-			}
-
-			err = client.Enqueue(analytics.Track{
-				UserId: hashedID,
-				Event:  cmd.CalledAs(),
-			})
-			if err != nil {
-				util.Warning("trouble with segment: %v", err)
-			}
-			err = client.Close()
-			if err != nil {
-				util.Warning("client.close() failed: %v", err)
-			}
-
-		}
 	},
+}
+
+func reportInstrumentation(projects []*ddevapp.DdevApp) {
+	instrumentationNotSetUpWarning()
+
+	if globalconfig.DdevGlobalConfig.InstrumentationOptIn && nodeps.IsInternetActive() {
+		_ = raven.CaptureMessageAndWait("Usage: ddev start", map[string]string{"severity-level": "info", "report-type": "usage"})
+
+		client := analytics.New(version.SegmentKey)
+		hashedID, _ := machineid.ProtectedID("ddev")
+
+		err := ddevapp.SegmentUser(client, hashedID)
+		if err != nil {
+			util.Warning("error sending hashedID to segment: %v", err)
+		}
+
+		for _, project := range projects {
+			err = ddevapp.SegmentEvent(client, hashedID, "start", project)
+			if err != nil {
+				util.Warning("error sending event to segment: %v", err)
+			}
+		}
+		err = client.Close()
+		if err != nil {
+			util.Warning("segment analytics client.close() failed: %v", err)
+		}
+
+	}
+
 }
 
 func init() {
@@ -101,7 +100,7 @@ func init() {
 	RootCmd.AddCommand(StartCmd)
 }
 
-func sentryNotSetupWarning() {
+func instrumentationNotSetUpWarning() {
 	if version.SentryDSN == "" && globalconfig.DdevGlobalConfig.InstrumentationOptIn {
 		output.UserOut.Warning("Instrumentation is opted in, but SentryDSN is not available.")
 	}
@@ -110,10 +109,10 @@ func sentryNotSetupWarning() {
 	}
 }
 
-// checkDdevVersionAndOptInSentry() reads global config and checks to see if current version is different
+// checkDdevVersionAndOptInInstrumentation() reads global config and checks to see if current version is different
 // from the last saved version. If it is, prompt to request anon ddev usage stats
 // and update the info.
-func checkDdevVersionAndOptInSentry() error {
+func checkDdevVersionAndOptInInstrumentation() error {
 	if !output.JSONOutput && version.COMMIT != globalconfig.DdevGlobalConfig.LastUsedVersion && globalconfig.DdevGlobalConfig.InstrumentationOptIn == false && !globalconfig.DdevNoSentry {
 		allowStats := util.Confirm("It looks like you have a new ddev release.\nMay we send anonymous ddev usage statistics and errors?\nTo know what we will see please take a look at\nhttps://ddev.readthedocs.io/en/latest/users/cli-usage/#opt-in-usage-information\nPermission to beam up?")
 		if allowStats {
