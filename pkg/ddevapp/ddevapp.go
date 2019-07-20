59c61a73b09c4bbf8f683f2ea12e350aee5e05ca
Aron Novak <aron@gizra.com>
2019-07-20 05:37:28 +0200

    gofmt fixes

--- pkg/ddevapp/ddevapp.go
+++ pkg/ddevapp/ddevapp.go
@@ -676,32 +676,32 @@ func (app *DdevApp) ProcessHooks(hookName string) error {
 
 // Compares two version strings.
 func compareVer(a, b string) (ret int) {
-  as := strings.Split(a, ".")
-  bs := strings.Split(b, ".")
-  loopMax := len(bs)
-  if len(as) > len(bs) {
-    loopMax = len(as)
-  }
-  for i := 0; i < loopMax; i++ {
-    var x, y string
-    if len(as) > i {
-      x = as[i]
-    }
-    if len(bs) > i {
-      y = bs[i]
-    }
-    xi,_ := strconv.Atoi(x)
-    yi,_ := strconv.Atoi(y)
-    if xi > yi {
-      ret = -1
-    } else if xi < yi {
-      ret = 1
-    }
-    if ret != 0 {
-      break
-    }
-  }
-  return
+	as := strings.Split(a, ".")
+	bs := strings.Split(b, ".")
+	loopMax := len(bs)
+	if len(as) > len(bs) {
+		loopMax = len(as)
+	}
+	for i := 0; i < loopMax; i++ {
+		var x, y string
+		if len(as) > i {
+			x = as[i]
+		}
+		if len(bs) > i {
+			y = bs[i]
+		}
+		xi, _ := strconv.Atoi(x)
+		yi, _ := strconv.Atoi(y)
+		if xi > yi {
+			ret = -1
+		} else if xi < yi {
+			ret = 1
+		}
+		if ret != 0 {
+			break
+		}
+	}
+	return
 }
 
 // Start initiates docker-compose up
