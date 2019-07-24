b62e12670e37cdb080f23feedd7c952acee59238
Randy Fay <randy@randyfay.com>
2019-07-23 18:30:48 -0600

    Skip chocolatey on artifacts if it doesn't exist

--- .circleci/generate_artifacts.sh
+++ .circleci/generate_artifacts.sh
@@ -55,7 +55,9 @@ zip $ARTIFACTS/ddev_linux.$VERSION.zip ddev ddev_bash_completion.sh
 cd $BASE_DIR/.gotmp/bin/windows_amd64
 tar -czf $ARTIFACTS/ddev_windows.$VERSION.tar.gz ddev.exe ddev_bash_completion.sh
 zip $ARTIFACTS/ddev_windows.$VERSION.zip ddev.exe ddev_bash_completion.sh
-tar -czf $ARTIFACTS/ddev_chocolatey.$VERSION.tar.gz chocolatey
+if [ -f chocolatey ]; then
+    tar -czf $ARTIFACTS/ddev_chocolatey.$VERSION.tar.gz chocolatey
+fi
 
 cp ddev_windows_installer*.exe $ARTIFACTS
 
