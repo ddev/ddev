ecef04fdc54040e1f4ab1a7ef663fec0561dab42
Randy Fay <randy@randyfay.com>
2019-08-21 19:16:11 -0600

    Also bump router version of mkcert :(

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -73,7 +73,7 @@ var BgsyncTag = "v1.10.0" // Note that this can be overridden by make
 var RouterImage = "drud/ddev-router"
 
 // RouterTag defines the tag used for the router.
-var RouterTag = "v1.10.0" // Note that this can be overridden by make
+var RouterTag = "20190819_update_mkcert" // Note that this can be overridden by make
 
 var SSHAuthImage = "drud/ddev-ssh-agent"
 
