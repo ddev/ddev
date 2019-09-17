6e7dd34ea117779f083ee606fc63c65d3076dabd
Aron Novak <AronNovak@users.noreply.github.com>
2019-09-17 15:27:13 +0200

    Better version suggestion on composer.yaml mismatch (#1707)

--- pkg/ddevapp/ddevapp.go
+++ pkg/ddevapp/ddevapp.go
@@ -22,6 +22,7 @@ import (
 	"path"
 	"time"
 
+	"github.com/Masterminds/semver"
 	"github.com/drud/ddev/pkg/appimport"
 	"github.com/drud/ddev/pkg/archive"
 	"github.com/drud/ddev/pkg/ddevhosts"
@@ -680,8 +681,22 @@ func (app *DdevApp) Start() error {
 
 	app.DockerEnv()
 
-	if app.APIVersion != version.DdevVersion {
-		util.Warning("Your %s version is %s, but ddev is version %s. \nPlease run 'ddev config' to update your config.yaml. \nddev may not operate correctly until you do.", app.ConfigPath, app.APIVersion, version.DdevVersion)
+	APIVersion, err := semver.NewVersion(app.APIVersion)
+	if err != nil {
+		return err
+	}
+	DdevVersion, err := semver.NewVersion(version.DdevVersion)
+	if err != nil {
+		return err
+	}
+
+	// It returns -1, 0, or 1 if the version smaller, equal, or larger than the other version.
+	compareResult := APIVersion.Compare(DdevVersion)
+
+	if compareResult == -1 {
+		util.Warning("Your %s version is %s, but ddev is (newer) version %s. \nPlease run 'ddev config' to update your config.yaml. \nddev may not operate correctly until you do.", app.ConfigPath, app.APIVersion, version.DdevVersion)
+	} else if compareResult == 1 {
+		util.Warning("Your %s version is %s, but ddev is (older) version %s. \nPlease update ddev, see https://ddev.readthedocs.io/en/stable/.\nddev may not operate correctly until you do.", app.ConfigPath, app.APIVersion, version.DdevVersion)
 	}
 
 	// Make sure that any ports allocated are available.
