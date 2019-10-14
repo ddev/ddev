efff4f0fcde6dd05fc98350d9d67cf857f2ad9bb
Randy Fay <randy@randyfay.com>
2019-10-14 14:33:48 -0600

    Use nsis from linuxbrew (3.04) instead of from apt-get (ancient) (#1884)
    * Use nsis from linuxbrew (3.04) instead of from apt-get (ancient)
    * Install stubs and plugins that are required

--- .circleci/linux_circle_vm_setup.sh
+++ .circleci/linux_circle_vm_setup.sh
@@ -6,7 +6,7 @@ set -x
 # Basic tools
 
 sudo apt-get update -qq
-sudo apt-get install -qq mysql-client realpath zip nsis jq expect nfs-kernel-server build-essential curl git libnss3-tools libcurl4-gnutls-dev
+sudo apt-get install -qq mysql-client realpath zip jq expect nfs-kernel-server build-essential curl git libnss3-tools libcurl4-gnutls-dev
 
 curl -sSL --fail -o /tmp/ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && sudo unzip -d /usr/local/bin /tmp/ngrok.zip
 
@@ -19,9 +19,12 @@ echo "export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH" >>~/.bashrc
 . ~/.bashrc
 
 brew update && brew tap drud/ddev
-for item in osslsigncode golang mkcert ddev; do
+for item in osslsigncode golang mkcert ddev makensis; do
     brew install $item || /home/linuxbrew/.linuxbrew/bin/brew upgrade $item
 done
+
+# Get the Stubs and Plugins for makensis; the linux makensis build doesn't do this.
+wget https://sourceforge.net/projects/nsis/files/NSIS%203/3.04/nsis-3.04.zip/download && sudo unzip -d /usr/local/share download && sudo mv /usr/local/share/nsis-3.04 /usr/local/share/nsis
 
 # Temporarily overwrite the brew-loaded broken-on-Ubuntu1604 mkcert
 brew unlink mkcert
