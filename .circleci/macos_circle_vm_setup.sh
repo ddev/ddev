39d08b0a37d514291c0bac7ce8c1e378ea887daa
Randy Fay <randy@randyfay.com>
2019-11-24 20:43:39 -0700

    openssl technique seems not to work

--- .circleci/macos_circle_vm_setup.sh
+++ .circleci/macos_circle_vm_setup.sh
@@ -19,11 +19,8 @@ nohup /Applications/Docker.app/Contents/MacOS/Docker --unattended &
 
 brew tap drud/ddev
 
-# Some CircleCI images seem to have root-owned /usr/local
-sudo chown -R distiller /usr/local/*
 brew install mariadb zip nsis jq expect coreutils golang ddev mkcert osslsigncode ghr
 brew link mariadb zip nsis jq expect coreutils golang ddev mkcert osslsigncode ghr
-brew reinstall openssl@1.1
 
 # homebrew sometimes removes /usr/local/etc/my.cnf.d
 mkdir -p /usr/local/etc/my.cnf.d
