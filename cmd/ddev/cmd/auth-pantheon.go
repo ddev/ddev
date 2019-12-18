8283ca2d0a750acec02f722931d92a582964969d
Randy Fay <randy@randyfay.com>
2019-12-18 14:29:36 -0700

    Minor verbiage changes

--- cmd/ddev/cmd/auth-pantheon.go
+++ cmd/ddev/cmd/auth-pantheon.go
@@ -24,7 +24,7 @@ var PantheonAuthCommand = &cobra.Command{
 		uid, _, _ := util.GetContainerUIDGid()
 		_, out, err := dockerutil.RunSimpleContainer(version.GetWebImage(), "", []string{"terminus", "auth:login", "--machine-token=" + args[0]}, nil, []string{"HOME=/tmp"}, []string{"ddev-global-cache:/mnt/ddev-global-cache"}, uid, true)
 		if err == nil {
-			util.Success("Authentication successful!\nYou may now use the 'ddev config pantheon' command when configuring sites!")
+			util.Success("Authentication successful!\nYou may now use the 'ddev config pantheon' command when configuring projects.")
 		} else {
 			util.Failed("Failed to authenticate: %v (%v)", err, out)
 		}
