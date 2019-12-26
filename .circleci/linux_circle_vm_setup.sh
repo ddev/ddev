3b92665966a59f4afe66d5700ac9eca6bcdf1dd0
Randy Fay <randy@randyfay.com>
2019-12-26 16:06:54 -0700

    Try to make markdownlint run in staticrequired phase

--- .circleci/linux_circle_vm_setup.sh
+++ .circleci/linux_circle_vm_setup.sh
@@ -5,6 +5,8 @@ set -x
 
 # Basic tools
 
+npm install --global markdownlint-cli
+
 sudo apt-get update -qq
 sudo apt-get install -qq mysql-client realpath zip jq expect nfs-kernel-server build-essential curl git libnss3-tools libcurl4-gnutls-dev
 
