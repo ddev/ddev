5ab8c51eb8dd8788dd114b992fdafb5dd8b4ccd7
Randy Fay <randy@randyfay.com>
2020-01-01 20:10:48 -0700

    Get trigger_release.sh going on macOS

--- .circleci/trigger_release.sh
+++ .circleci/trigger_release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# trigger_release.sh --release-tag=v1.x.1 --circleci-token=token --github-token=githubPersonalToken --windows-signing-password=windowspass
+# .circleci/trigger_release.sh x --release-tag="v1.12.0-20" --circleci-token=circletoken  --build-image-tarballs=false --windows-signing-password=winsignpasswd --macos-signing-password=macsigningpwd --macos-app-password="macapppwd"
 
-# .circleci/trigger_release.sh --release-tag=v1.7.1 --circleci-token=circleToken900908b3443ea58316baf928b --github-token=githubPersonalToken853ae6f72c40525cd21036f742904a   --windows-signing-password=windowscodepassword | jq -r 'del(.circle_yml)'  | jq -r 'del(.circle_yml)'
+# .circleci/trigger_release.sh --release-tag=v1.11.1 --circleci-token=circletoken --github-token=githubtoken --build-image-tarballs=true --windows-signing-password=winsignpwd —macos-signing-password=macsignpwd —macos_app_password=macapppwd | jq -r 'del(.circle_yml)'
 
 # api docs: https://circleci.com/docs/api
 # Trigger a new job: https://circleci.com/docs/api/v1-reference/#new-build
@@ -27,16 +27,14 @@ if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
     exit 1
 fi
 
+LONGOPTS=circleci-token:,github-token:,release-tag:,github-project:,windows-signing-password:,macos-signing-password:,build-image-tarballs:,chocolatey-api-key:,github-org:,macos-app-password:
 
-OPTIONS=c:g:r:p:s:b:h:o:m:a:
-LONGOPTS=circleci-token:,github-token:,release-tag:,github-project:,windows-signing-password:,macos-signing-password:,build-image-tarballs:,chocolatey-api-key:,github-org:,macos-app-password
-
-! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
+! PARSED=$(getopt --longoptions=$LONGOPTS --name "$0" -- "$@" dummyarg)
 if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
     # e.g. return value is 1
     #  then getopt has complained about wrong arguments to stdout
     printf "\n\nFailed parsing options:\n"
-    getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@"
+    getopt --longoptions=$LONGOPTS --name "$0" -- "$@"
     exit 2
 fi
 
@@ -44,40 +42,40 @@ eval set -- "$PARSED"
 
 while true; do
     case "$1" in
-    -c|--circleci-token)
+    --circleci-token)
         CIRCLE_TOKEN=$2
         shift 2
         ;;
-    -g|--github-token)
+    --github-token)
         GITHUB_TOKEN=$2
         shift 2
         ;;
-    -t|--release-tag)
+    --release-tag)
         RELEASE_TAG=$2
         shift 2
         ;;
-    -p|--github-project)
+    --github-project)
         GITHUB_PROJECT=$2
         shift 2
         ;;
-    -s|--windows-signing-password)
+    --windows-signing-password)
         DDEV_WINDOWS_SIGNING_PASSWORD=$2
         shift 2
         ;;
-      -h|--chocolatey-api-key)
+    --chocolatey-api-key)
         CHOCOLATEY_API_KEY=$2
         shift 2
         ;;
-    -m|--macos-signing-password)
+    --macos-signing-password)
         DDEV_MACOS_SIGNING_PASSWORD=$2
         shift 2
         ;;
-    -a|--macos-app-password)
+    --macos-app-password)
         DDEV_MACOS_APP_PASSWORD=$2
         shift 2
         ;;
     # For debugging we can set BUILD_IMAGE_TARBALLS=false to avoid waiting for that.
-    -b|--build-image-tarballs)
+    --build-image-tarballs)
         BUILD_IMAGE_TARBALLS=$2
         shift 2
         ;;
@@ -94,7 +92,7 @@ done
 trigger_build_url=https://circleci.com/api/v1.1/project/github/$GITHUB_PROJECT?circle-token=${CIRCLE_TOKEN}
 
 set -x
-BUILD_PARAMS="\"CIRCLE_JOB\": \"release_build\", \"job_name\": \"release_build\", \"GITHUB_TOKEN\":\"${GITHUB_TOKEN:-}\", \"RELEASE_TAG\": \"${RELEASE_TAG}\",\"DDEV_WINDOWS_SIGNING_PASSWORD\":\"${DDEV_WINDOWS_SIGNING_PASSWORD:-}\",\"DDEV_MACOS_SIGNING_PASSWORD\":\"${DDEV_MACOS_SIGNING_PASSWORD:-}\",\"CHOCOLATEY_API_KEY\":\"${CHOCOLATEY_API_KEY:-}\",\"BUILD_IMAGE_TARBALLS\":\"${BUILD_IMAGE_TARBALLS:-true}\",\"GITHUB_ORG\":\"${GITHUB_ORG}\""
+BUILD_PARAMS="\"CIRCLE_JOB\": \"release_build\", \"job_name\": \"release_build\", \"GITHUB_TOKEN\":\"${GITHUB_TOKEN:-}\", \"RELEASE_TAG\": \"${RELEASE_TAG}\",\"DDEV_WINDOWS_SIGNING_PASSWORD\":\"${DDEV_WINDOWS_SIGNING_PASSWORD:-}\",\"DDEV_MACOS_SIGNING_PASSWORD\":\"${DDEV_MACOS_SIGNING_PASSWORD:-}\",\"DDEV_MACOS_APP_PASSWORD\":\"${DDEV_MACOS_APP_PASSWORD:-}\",\"CHOCOLATEY_API_KEY\":\"${CHOCOLATEY_API_KEY:-}\",\"BUILD_IMAGE_TARBALLS\":\"${BUILD_IMAGE_TARBALLS:-true}\",\"GITHUB_ORG\":\"${GITHUB_ORG}\""
 if [ "${RELEASE_TAG:-}" != "" ]; then
     DATA="\"tag\": \"$RELEASE_TAG\","
 fi
