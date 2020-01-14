f9df96d4830f2bbf9415a3db93c0ee7273b92faf
Randy Fay <randy@randyfay.com>
2020-01-14 10:21:46 -0700

    Fix files destination to be relative to docroot

--- pkg/ddevapp/magento.go
+++ pkg/ddevapp/magento.go
@@ -69,7 +69,7 @@ func setMagentoSiteSettingsPaths(app *DdevApp) {
 
 // magentoImportFilesAction defines the magento workflow for importing project files.
 func magentoImportFilesAction(app *DdevApp, importPath, extPath string) error {
-	destPath := filepath.Join(app.GetAppRoot(), app.GetUploadDir())
+	destPath := filepath.Join(app.GetAppRoot(), app.GetDocroot(), app.GetUploadDir())
 
 	// parent of destination dir should exist
 	if !fileutil.FileExists(filepath.Dir(destPath)) {
