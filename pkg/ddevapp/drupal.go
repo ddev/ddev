ef396f338bfffc27d3df5853398fdf490bfc5776
Randy Fay <randy@randyfay.com>
2020-01-23 12:44:33 -0700

    Improve settings.ddev.php handling of config_sync_directory (#2039)

--- pkg/ddevapp/drupal.go
+++ pkg/ddevapp/drupal.go
@@ -161,13 +161,12 @@ $settings['trusted_host_patterns'] = ['.*'];
 $settings['class_loader_auto_detect'] = FALSE;
 
 // This specifies the default configuration sync directory.
-// Both $config_directories (pre-Drupal 8.8) and
-// $settings['config_sync_directory'] are provided
+// $config_directories (pre-Drupal 8.8) and
+// $settings['config_sync_directory'] are supported
 // so it should work on any Drupal 8 or 9 version.
-if (empty($config_directories[CONFIG_SYNC_DIRECTORY])) {
+if (defined('CONFIG_SYNC_DIRECTORY') && empty($config_directories[CONFIG_SYNC_DIRECTORY])) {
   $config_directories[CONFIG_SYNC_DIRECTORY] = '{{ joinPath $config.SitePath $config.SyncDir }}';
-}
-if (empty($settings['config_sync_directory'])) {
+} else if (empty($settings['config_sync_directory'])) {
   $settings['config_sync_directory'] = '{{ joinPath $config.SitePath $config.SyncDir }}';
 }
 `
