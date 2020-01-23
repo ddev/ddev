2b6026987d196ed097f00eae5c87fbaf350d9df2
Randy Fay <randy@randyfay.com>
2020-01-23 15:46:34 -0700

    Improve tests so they don't remove the required web-build directory

--- pkg/fileutil/files.go
+++ pkg/fileutil/files.go
@@ -342,3 +342,24 @@ func ReplaceSimulatedLinks(path string) {
 	util.Success("Replaced these simulated symlinks with real symlinks: %v", replacedLinks)
 	return
 }
+
+// RemoveContents removes contents of passed directory
+// From https://stackoverflow.com/questions/33450980/how-to-remove-all-contents-of-a-directory-using-golang
+func RemoveContents(dir string) error {
+	d, err := os.Open(dir)
+	if err != nil {
+		return err
+	}
+	defer d.Close()
+	names, err := d.Readdirnames(-1)
+	if err != nil {
+		return err
+	}
+	for _, name := range names {
+		err = os.RemoveAll(filepath.Join(dir, name))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
