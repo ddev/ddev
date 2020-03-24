208a0ed4ba0d3707cc130e09bf24d4ffbf54c833
Randy Fay <randy@randyfay.com>
2020-03-24 13:28:48 -0600

    Improve IsInternetActive() by using random subdomain so not cached [skip ci][ci skip]

--- pkg/nodeps/utils.go
+++ pkg/nodeps/utils.go
@@ -2,6 +2,8 @@ package nodeps
 
 import (
 	"context"
+	"fmt"
+	"math/rand"
 	"net"
 	"os"
 	"time"
@@ -37,12 +39,36 @@ func IsDockerToolbox() bool {
 
 //IsInternetActive() checks to see if we have a viable
 // internet connection. It just tries a quick DNS query.
-// This requires that the named record be query-able.
+// This requires that the named record be query-able
 func IsInternetActive() bool {
 	const timeout = 500 * time.Millisecond
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 
-	_, err := net.DefaultResolver.LookupHost(ctx, "i-exist.ddev.site")
-	return err == nil && ctx.Err() == nil
+	randomURL := RandomString(10) + ".ddev.site"
+	addrs, err := net.DefaultResolver.LookupHost(ctx, randomURL)
+
+	// Internet is active (active == true) if both err and ctx.Err() were nil
+	active := err == nil && ctx.Err() == nil
+	if os.Getenv("DDEV_DEBUG") != "" {
+		fmt.Printf("IsInternetActive DEBUG: err=%v ctx.Err()=%v addrs=%v IsInternetactive==%v, randomURL=%v\n", err, ctx.Err(), addrs, active, randomURL)
+	}
+	if active == false {
+		fmt.Println("Internet connection not detected")
+	}
+	return active
+}
+
+// From https://www.calhoun.io/creating-random-strings-in-go/
+var seededRand *rand.Rand = rand.New(
+	rand.NewSource(time.Now().UnixNano()))
+
+func RandomString(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyz"
+
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
 }
