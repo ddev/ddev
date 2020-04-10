2e08f69812f06f9a9fa902d177a340137756f826
Randy Fay <randy@randyfay.com>
2020-04-10 11:47:59 -0600

    Stop providing default to BASE_IMAGE arg in Dockerfile.example, fixes #2155 (#2161)

--- pkg/ddevapp/config.go
+++ pkg/ddevapp/config.go
@@ -246,7 +246,7 @@ func (app *DdevApp) WriteConfig() error {
 	contents := []byte(`
 # You can copy this Dockerfile.example to Dockerfile to add configuration
 # or packages or anything else to your webimage
-ARG BASE_IMAGE=` + app.WebImage + `
+ARG BASE_IMAGE
 FROM $BASE_IMAGE
 RUN npm install --global gulp-cli
 `)
@@ -258,7 +258,7 @@ RUN npm install --global gulp-cli
 	contents = []byte(`
 # You can copy this Dockerfile.example to Dockerfile to add configuration
 # or packages or anything else to your dbimage
-ARG BASE_IMAGE=` + app.GetDBImage() + `
+ARG BASE_IMAGE
 FROM $BASE_IMAGE
 RUN echo "Built from ` + app.GetDBImage() + `" >/var/tmp/built-from.txt
 `)
