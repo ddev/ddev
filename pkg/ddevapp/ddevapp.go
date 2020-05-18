eb05e12a1dc4e5669d939aaf7de72ebe42eb578c
Christopher Kaster <me@atomicptr.de>
2020-05-18 14:19:46 +0200

    use bash -ic instead of -ci

--- pkg/ddevapp/ddevapp.go
+++ pkg/ddevapp/ddevapp.go
@@ -977,7 +977,7 @@ func (app *DdevApp) Exec(opts *ExecOpts) (string, string, error) {
 	if !nodeps.ArrayContainsString([]string{"web", "db", "dba"}, opts.Service) {
 		shell = "sh"
 	}
-	exec = append(exec, shell, "-ci", opts.Cmd)
+	exec = append(exec, shell, "-ic", opts.Cmd)
 
 	files, err := app.ComposeFiles()
 	if err != nil {
