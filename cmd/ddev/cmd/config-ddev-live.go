0d026cf0de2a70fae27b2fd1eef0bc21946cfd69
Randy Fay <randy@randyfay.com>
2020-05-24 14:10:51 -0600

    Add org flag to config ddev-live

--- cmd/ddev/cmd/config-ddev-live.go
+++ cmd/ddev/cmd/config-ddev-live.go
@@ -1,13 +1,12 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/drud/ddev/pkg/nodeps"
 
 	"github.com/drud/ddev/pkg/ddevapp"
 	"github.com/spf13/cobra"
 )
-
-var ddevListOrgName string
 
 // ddevliveConfigCommand is the the `ddev config ddevlive` command
 var ddevliveConfigCommand *cobra.Command = &cobra.Command{
@@ -23,20 +22,24 @@ var ddevliveConfigCommand *cobra.Command = &cobra.Command{
 
 func init() {
 	ddevliveConfigCommand.Flags().AddFlagSet(ConfigCommand.Flags())
-	ddevliveConfigCommand.Flags().StringVarP(&ddevListOrgName, "org", "", "", "provide the DDEV-Live org name")
+	ddevliveConfigCommand.Flags().String("org", "", "DDEV-Live org name to use")
 	ConfigCommand.AddCommand(ddevliveConfigCommand)
 }
 
 // handleDdevLiveFlags is the ddevlive-specific flag handler
 func handleDdevLiveFlags(cmd *cobra.Command, args []string, app *ddevapp.DdevApp) error {
-	//provider, err := app.GetProvider()
-	//if err != nil {
-	//	return fmt.Errorf("failed to GetProvider: %v", err)
-	//}
-	//ddevliveProvider := provider.(*ddevapp.DdevLiveProvider)
-	//if err != nil {
-	//	return err
-	//}
+	provider, err := app.GetProvider()
+	if err != nil {
+		return fmt.Errorf("failed to GetProvider: %v", err)
+	}
+	ddevliveProvider := provider.(*ddevapp.DdevLiveProvider)
+	if err != nil {
+		return err
+	}
+
+	if cmd.Flag("org").Changed {
+		ddevliveProvider.OrgName = cmd.Flag("org").Value.String()
+	}
 
 	return nil
 }
