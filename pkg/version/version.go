18c38f77c2937b8685759a5a33760e20732106bf
Randy Fay <randy@randyfay.com>
2020-06-02 17:50:46 -0600

    Rework ddev-webserver to base on ddev-images PHP version (#2290)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -45,7 +45,7 @@ var DockerComposeFileFormatVersion = "3.6"
 var WebImg = "drud/ddev-webserver"
 
 // WebTag defines the default web image tag for drud dev
-var WebTag = "20200513_laravel" // Note that this can be overridden by make
+var WebTag = "20200601_inherit_php_from_ddev_images" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "drud/ddev-dbserver"
