ae4c30e4515d213b0bf91ec9275a5757534dd133
Randy Fay <randy@randyfay.com>
2020-06-25 14:02:49 -0700

    Add environment variables to web container that got lost in reorg (#2336)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -45,7 +45,7 @@ var DockerComposeFileFormatVersion = "3.6"
 var WebImg = "drud/ddev-webserver"
 
 // WebTag defines the default web image tag for drud dev
-var WebTag = "20200613_global_homeadditions" // Note that this can be overridden by make
+var WebTag = "20200625_container_vars" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "drud/ddev-dbserver"
