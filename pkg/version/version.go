bc2173b15a8e9b0d0db1b2b0aeb33c08f31ba3fa
Randy Fay <randy@randyfay.com>
2020-06-27 17:34:20 -0700

    Bump all images built with no cache to v1.15-rc1 (#2340)
    * Bump image versions for v1.15-rc1
    * Bump webserver base to 0.3.6

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -45,13 +45,13 @@ var DockerComposeFileFormatVersion = "3.6"
 var WebImg = "drud/ddev-webserver"
 
 // WebTag defines the default web image tag for drud dev
-var WebTag = "20200625_container_vars" // Note that this can be overridden by make
+var WebTag = "v1.15-rc1" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "drud/ddev-dbserver"
 
 // BaseDBTag is the main tag, DBTag is constructed from it
-var BaseDBTag = "20200624_longer_mysql_timeout"
+var BaseDBTag = "v1.15-rc1"
 
 // DBAImg defines the default phpmyadmin image tag used for applications.
 var DBAImg = "phpmyadmin/phpmyadmin"
@@ -63,11 +63,11 @@ var DBATag = "5" // Note that this can be overridden by make
 var RouterImage = "drud/ddev-router"
 
 // RouterTag defines the tag used for the router.
-var RouterTag = "v1.14.0" // Note that this can be overridden by make
+var RouterTag = "v1.15-rc1" // Note that this can be overridden by make
 
 var SSHAuthImage = "drud/ddev-ssh-agent"
 
-var SSHAuthTag = "v1.14.0"
+var SSHAuthTag = "v1.15-rc1"
 
 // COMMIT is the actual committish, supplied by make
 var COMMIT = "COMMIT should be overridden"
