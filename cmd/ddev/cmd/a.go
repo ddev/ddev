4c95e32dc1d23f0325f44c5535d1e602b3b1308a
Randy Fay <randy@randyfay.com>
2020-06-30 15:49:46 -0600

    Make absolutely sure the root check happens early

--- cmd/ddev/cmd/a.go
+++ cmd/ddev/cmd/a.go
@@ -2,9 +2,7 @@ package cmd
 
 import (
 	"github.com/drud/ddev/pkg/globalconfig"
-	"github.com/drud/ddev/pkg/output"
 	"github.com/drud/ddev/pkg/util"
-	"os"
 )
 
 // This file is a.go because global config must be loaded before anybody else
@@ -12,12 +10,6 @@ import (
 // uninitialized data
 
 func init() {
-	// Prevent running as root for most cases
-	// We really don't want ~/.ddev to have root ownership, breaks things.
-	if os.Geteuid() == 0 && len(os.Args) > 1 && os.Args[1] != "hostname" {
-		output.UserOut.Fatal("ddev is not designed to be run with root privileges, please run as normal user and without sudo")
-	}
-
 	err := globalconfig.ReadGlobalConfig()
 	if err != nil {
 		util.Failed("unable to read global config: %v", err)
--- cmd/ddev/main.go
+++ cmd/ddev/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"github.com/drud/ddev/cmd/ddev/cmd"
+	"github.com/drud/ddev/pkg/output"
+	"os"
 )
 
 func main() {
+	// Prevent running as root for most cases
+	// We really don't want ~/.ddev to have root ownership, breaks things.
+	if os.Geteuid() == 0 && len(os.Args) > 1 && os.Args[1] != "hostname" {
+		output.UserOut.Fatal("ddev is not designed to be run with root privileges, please run as normal user and without sudo")
+	}
+
 	cmd.Execute()
 }
