ad5528ca22505f4490d80c4ad4777b4ba18e2087
Randy Fay <randy@randyfay.com>
2020-08-12 10:26:50 -0600

    Refer to pushed image

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -40,7 +40,7 @@ var DockerComposeFileFormatVersion = "3.6"
 var WebImg = "drud/ddev-webserver"
 
 // WebTag defines the default web image tag for drud dev
-var WebTag = "20210107_hardened_drush" // Note that this can be overridden by make
+var WebTag = "20200812_lolautruche_blackfire_cli" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "drud/ddev-dbserver"
