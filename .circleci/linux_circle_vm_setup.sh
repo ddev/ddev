ec60cae8f70c3c89c19db0dc86189c53d5f45955
Dennis Ameling <dennis@dennisameling.com>
2020-08-15 13:17:12 +0200

    Install qemu for multi-arch builds

--- .circleci/linux_circle_vm_setup.sh
+++ .circleci/linux_circle_vm_setup.sh
@@ -6,7 +6,7 @@ set -x
 # Basic tools
 
 sudo apt-get update -qq
-sudo apt-get install -qq mysql-client realpath zip jq expect nfs-kernel-server build-essential curl git libnss3-tools libcurl4-gnutls-dev
+sudo apt-get install -qq mysql-client realpath zip jq expect nfs-kernel-server build-essential curl git libnss3-tools libcurl4-gnutls-dev qemu-user-static
 
 curl -sSL --fail -o /tmp/ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && sudo unzip -o -d /usr/local/bin /tmp/ngrok.zip
 
