c907d6bfd958a76940a32a96788992e9d2e123bf
Randy Fay <randy@randyfay.com>
2020-08-17 10:56:46 -0600

    Allow custom certs to be added per project

--- pkg/ddevapp/ddevapp.go
+++ pkg/ddevapp/ddevapp.go
@@ -939,6 +939,17 @@ func (app *DdevApp) Start() error {
 		}
 	}
 
+	certPath := app.GetConfigPath("custom_certs")
+	if fileutil.FileExists(certPath) {
+		uid, _, _ := util.GetContainerUIDGid()
+		err = dockerutil.CopyToVolume(certPath, "ddev-global-cache", "custom_certs", uid)
+		if err != nil {
+			util.Warning("failed to copy custom certs into docker volume ddev-global-cache/custom_certs: %v", err)
+		} else {
+			util.Success("Copied custom certs in %s to ddev-global-cache/custom_certs", certPath)
+		}
+	}
+
 	// WriteConfig .ddev-docker-compose-*.yaml
 	err = app.WriteDockerComposeYAML()
 	if err != nil {
