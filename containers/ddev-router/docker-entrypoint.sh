1948a47c9781c83f88424280b25aadca420d9b01
Randy Fay <randy@randyfay.com>
2020-08-21 07:23:02 -0600

    Fix unbound variable issue

--- containers/ddev-router/docker-entrypoint.sh
+++ containers/ddev-router/docker-entrypoint.sh
@@ -28,7 +28,7 @@ if [ "${socketMissing:-}" = 1 -a "$1" = forego -a "$2" = start -a "$3" = '-r' ];
 	exit 1
 fi
 
-if [ ! -z "${USE_LETSENCRYPT}" ]; then
+if [ ! -z "${USE_LETSENCRYPT:-}" ]; then
   echo "Lets Encrypt is enabled:"
   certbot certificates
 fi
