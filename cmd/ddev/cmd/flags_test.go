22a0317ae7882610632b282861cf4b2d78830e23
Gilbertsoft <25326036+gilbertsoft@users.noreply.github.com>
2020-09-10 09:13:12 +0200

    Simplify tests

--- cmd/ddev/cmd/flags_test.go
+++ cmd/ddev/cmd/flags_test.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"testing"
 
+	"github.com/spf13/cobra"
 	asrt "github.com/stretchr/testify/assert"
 )
 
@@ -31,23 +32,17 @@ func TestUnitCmdFlagsLoadFromJSON(t *testing.T) {
 	assert := asrt.New(t)
 	subject := getSubject()
 
-	var err error
-
 	// No data
-	err = subject.LoadFromJSON(``)
-	assert.NoError(err)
+	assert.NoError(subject.LoadFromJSON(``))
 
 	// Invalid JSON
-	err = subject.LoadFromJSON(`this is no valid JSON`)
-	assert.Error(err)
+	assert.Error(subject.LoadFromJSON(`this is no valid JSON`))
 
 	// Empty array
-	err = subject.LoadFromJSON(`[]`)
-	assert.NoError(err)
+	assert.NoError(subject.LoadFromJSON(`[]`))
 
 	// Minimal
-	err = subject.LoadFromJSON(`[{"Name":"test","Usage":"Usage of test"}]`)
-	assert.NoError(err)
+	assert.NoError(subject.LoadFromJSON(`[{"Name":"test","Usage":"Usage of test"}]`))
 	assert.Exactly("test", subject.Definition[0].Name)
 	assert.Exactly("", subject.Definition[0].Shorthand)
 	assert.Exactly("Usage of test", subject.Definition[0].Usage)
@@ -57,8 +52,7 @@ func TestUnitCmdFlagsLoadFromJSON(t *testing.T) {
 	assert.Empty(subject.Definition[0].Annotations)
 
 	// Full
-	err = subject.LoadFromJSON(`[{"Name":"test-1","Shorthand":"t","Usage":"Usage of test 1","Type":"bool","DefValue":"true","NoOptDefVal":"true","Annotations":{"test-1":["test-1-1","test-1-2"]}},{"Name":"test-2","Usage":"Usage of test 2","Type":"bool","DefValue":"true","NoOptDefVal":"true","Annotations":{"test-2":["test-2-1","test-2-2"]}}]`)
-	assert.NoError(err)
+	assert.NoError(subject.LoadFromJSON(`[{"Name":"test-1","Shorthand":"t","Usage":"Usage of test 1","Type":"bool","DefValue":"true","NoOptDefVal":"true","Annotations":{"test-1":["test-1-1","test-1-2"]}},{"Name":"test-2","Usage":"Usage of test 2","Type":"bool","DefValue":"true","NoOptDefVal":"true","Annotations":{"test-2":["test-2-1","test-2-2"]}}]`))
 	assert.Exactly("test-1", subject.Definition[0].Name)
 	assert.Exactly("t", subject.Definition[0].Shorthand)
 	assert.Exactly("Usage of test 1", subject.Definition[0].Usage)
@@ -75,22 +69,52 @@ func TestUnitCmdFlagsLoadFromJSON(t *testing.T) {
 	assert.Exactly(map[string][]string{"test-2": {"test-2-1", "test-2-2"}}, subject.Definition[1].Annotations)
 
 	// Duplicate flag
-	err = subject.LoadFromJSON(`[{"Name":"test-1","Shorthand":"t","Usage":"Usage of test 1"},{"Name":"test-1","Usage":"Test duplicate"}]`)
-	assert.EqualError(err, "The following problems were found in the flags definition of the command 'command' in 'script':\n - flag 'test-1' already defined")
+	assert.EqualError(subject.LoadFromJSON(`[{"Name":"test-1","Shorthand":"t","Usage":"Usage of test 1"},{"Name":"test-1","Usage":"Test duplicate"}]`),
+		"The following problems were found in the flags definition of the command 'command' in 'script':\n - flag 'test-1' already defined")
 
 	// Duplicate shorthand
-	err = subject.LoadFromJSON(`[{"Name":"test-1","Shorthand":"t","Usage":"Usage of test 1"},{"Name":"test-2","Shorthand":"t","Usage":"Usage of test 2 with existing shorthand"}]`)
-	assert.EqualError(err, "The following problems were found in the flags definition of the command 'command' in 'script':\n - shorthand 't' is already defined flag 'test-1'")
+	assert.EqualError(subject.LoadFromJSON(`[{"Name":"test-1","Shorthand":"t","Usage":"Usage of test 1"},{"Name":"test-2","Shorthand":"t","Usage":"Usage of test 2 with existing shorthand"}]`),
+		"The following problems were found in the flags definition of the command 'command' in 'script':\n - shorthand 't' is already defined flag 'test-1'")
 
 	// Invalid shorthand
-	err = subject.LoadFromJSON(`[{"Name":"test-1","Shorthand":"t1","Usage":"Usage of test 1"}]`)
-	assert.EqualError(err, "The following problems were found in the flags definition of the command 'command' in 'script':\n - shorthand 't1' for flag 'test-1' is more than one ASCII character")
+	assert.EqualError(subject.LoadFromJSON(`[{"Name":"test-1","Shorthand":"t1","Usage":"Usage of test 1"}]`),
+		"The following problems were found in the flags definition of the command 'command' in 'script':\n - shorthand 't1' for flag 'test-1' is more than one ASCII character")
 
 	// Empty usage in multiple commands
-	err = subject.LoadFromJSON(`[{"Name":"test-1","Shorthand":"t","Usage":""},{"Name":"test-2"}]`)
-	assert.EqualError(err, "The following problems were found in the flags definition of the command 'command' in 'script':\n - no usage defined for flag 'test-1'\n - no usage defined for flag 'test-2'")
+	assert.EqualError(subject.LoadFromJSON(`[{"Name":"test-1","Shorthand":"t","Usage":""},{"Name":"test-2"}]`),
+		"The following problems were found in the flags definition of the command 'command' in 'script':\n - no usage defined for flag 'test-1'\n - no usage defined for flag 'test-2'")
 
 	// Invalid and not implemented type
-	err = subject.LoadFromJSON(`[{"Name":"test-1","Shorthand":"t","Usage":"Usage of test 1","Type":"invalid"},{"Name":"test-2","Usage":"Usage of test 2","Type":"notimplemented"}]`)
-	assert.EqualError(err, "The following problems were found in the flags definition of the command 'command' in 'script':\n - type 'invalid' for flag 'test-1' is not known\n - type 'notimplemented' for flag 'test-2' is not implemented")
+	assert.EqualError(subject.LoadFromJSON(`[{"Name":"test-1","Shorthand":"t","Usage":"Usage of test 1","Type":"invalid"},{"Name":"test-2","Usage":"Usage of test 2","Type":"notimplemented"}]`),
+		"The following problems were found in the flags definition of the command 'command' in 'script':\n - type 'invalid' for flag 'test-1' is not known\n - type 'notimplemented' for flag 'test-2' is not implemented")
+}
+
+func getCommand() cobra.Command {
+	return cobra.Command{
+		Use:     "usage of command",
+		Short:   "short description",
+		Example: "example",
+		FParseErrWhitelist: cobra.FParseErrWhitelist{
+			UnknownFlags: true,
+		},
+	}
+
+}
+
+// TestUnitCmdFlagsAssignToCommand checks AssignToCommand works correctly and
+// handles user errors.
+func TestUnitCmdFlagsAssignToCommand(t *testing.T) {
+	assert := asrt.New(t)
+	subject := getSubject()
+
+	var c cobra.Command
+
+	// No flags
+	c = getCommand()
+	assert.NoError(subject.AssignToCommand(&c))
+
+	// No flags
+	c = getCommand()
+	assert.NoError(subject.LoadFromJSON(`[{"Name":"test-1","Shorthand":"t","Usage":"Usage of test 1","Type":"bool","DefValue":"true","NoOptDefVal":"true","Annotations":{"test-1":["test-1-1","test-1-2"]}},{"Name":"test-2","Usage":"Usage of test 2","Type":"bool","DefValue":"true","NoOptDefVal":"true","Annotations":{"test-2":["test-2-1","test-2-2"]}}]`))
+	assert.NoError(subject.AssignToCommand(&c))
 }
