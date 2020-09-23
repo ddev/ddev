b4c69a074deaddbf4e6acaf108ecce049d89bd09
Dennis Ameling <dennis@dennisameling.com>
2020-09-23 09:30:46 +0200

    Temp exclude MySQL 5.6 from tests

--- containers/ddev-dbserver/test/test_dbserver.sh
+++ containers/ddev-dbserver/test/test_dbserver.sh
@@ -24,7 +24,9 @@ for v in 5.5 10.0 10.1 10.2 10.3 10.4 10.5; do
 done
 
 export DB_TYPE=mysql
-for v in 5.5 5.6 5.7 8.0; do
+# Temporarily disable 5.6 tests due to https://github.com/drud/ddev/pull/2454#issuecomment-697137114
+for v in 5.5 5.7 8.0; do
+#for v in 5.5 5.6 5.7 8.0; do
     export IMAGE="drud/ddev-dbserver-$DB_TYPE-$v:$tag"
     export DB_VERSION=$v
     # /usr/local/bin is added for git-bash, where it may not be in the $PATH.
