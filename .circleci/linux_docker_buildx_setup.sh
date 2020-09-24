c7b7236be59b38fd17fbd547234dfbaaea470076
Dennis Ameling <dennis@dennisameling.com>
2020-09-24 09:31:09 +0200

    Try to fix circleci build

--- .circleci/linux_docker_buildx_setup.sh
+++ .circleci/linux_docker_buildx_setup.sh
@@ -1,6 +1,6 @@
 sudo apt-get install docker-ce-cli binfmt-support qemu-user-static
 
-BUILDX_BINARY_URL="https://github.com/docker/buildx/releases/download/v0.4.1/buildx-v0.4.1.linux-amd64"
+BUILDX_BINARY_URL="https://github.com/docker/buildx/releases/download/v0.4.2/buildx-v0.4.2.linux-amd64"
 
 curl --output docker-buildx \
     --silent --show-error --location --fail --retry 3 \
@@ -9,3 +9,8 @@ curl --output docker-buildx \
 mkdir -p ~/.docker/cli-plugins
 mv docker-buildx ~/.docker/cli-plugins
 chmod a+x ~/.docker/cli-plugins/docker-buildx
+
+# We need this to get arm64 qemu to work https://github.com/docker/buildx/issues/138#issuecomment-569240559
+	docker run --privileged --rm tonistiigi/binfmt --install all
+	if ! docker buildx inspect ddev-builder-multi --bootstrap >/dev/null; then docker buildx create --name ddev-builder-multi; fi
+	docker buildx use ddev-builder-multi
