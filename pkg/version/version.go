b4e1e581afbbaa825e5ae1208444cb290448f02e
Randy Fay <randy@randyfay.com>
2020-10-23 09:28:09 -0600

    Bump db version to pinned 8.0.21

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -48,7 +48,7 @@ var WebTag = "20201017_php8.0" // Note that this can be overridden by make
 var DBImg = "drud/ddev-dbserver"
 
 // BaseDBTag is the main tag, DBTag is constructed from it
-var BaseDBTag = "20201017_unpin_mysql_8_0"
+var BaseDBTag = "20201023_mysql_version_bump"
 
 // DBAImg defines the default phpmyadmin image tag used for applications.
 var DBAImg = "phpmyadmin"
