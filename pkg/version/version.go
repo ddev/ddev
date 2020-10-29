56d008d5637cb547e77b377eeeb481879a90aa8e
Randy Fay <randy@randyfay.com>
2020-10-29 08:00:55 -0600

    Remove mailhog, some other references to /home/.* old prototype

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -42,7 +42,7 @@ var DockerComposeFileFormatVersion = "3.6"
 var WebImg = "drud/ddev-webserver"
 
 // WebTag defines the default web image tag for drud dev
-var WebTag = "20201026_new_upstream_webimage" // Note that this can be overridden by make
+var WebTag = "20201026_hardened" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "drud/ddev-dbserver"
