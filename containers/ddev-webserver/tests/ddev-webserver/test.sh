52d0a963f3d106b346d407471c5e407e3ab3a284
Randy Fay <randy@randyfay.com>
2020-10-31 21:32:16 -0600

    Tests only - use busybox with root to set up ddev-global-cache

--- containers/ddev-webserver/tests/ddev-webserver/test.sh
+++ containers/ddev-webserver/tests/ddev-webserver/test.sh
@@ -7,10 +7,8 @@ set -o nounset
 if [ $# != "1" ]; then echo "docker image spec must be \$1"; exit 1; fi
 DOCKER_IMAGE=$1
 export IS_HARDENED=false
-SUDO=sudo
 DOCKER_REPO=${DOCKER_IMAGE##:*}
 if [ ${DOCKER_REPO%_prod} ]; then
-  SUDO=""
   IS_HARDENED=true
 fi
 
@@ -28,6 +26,8 @@ MOUNTGID=33
 export PATH="/usr/local/bin:$PATH"
 
 mkcert -install
+docker run -t --rm  -v "$(mkcert -CAROOT):/mnt/mkcert" -v ddev-global-cache:/mnt/ddev-global-cache busybox sh -c "mkdir -p /mnt/ddev-global-cache/mkcert && chmod -R ugo+w /mnt/ddev-global-cache/* && cp -R /mnt/mkcert /mnt/ddev-global-cache"
+
 
 # Wait for container to be ready.
 function containerwait {
@@ -55,7 +55,7 @@ trap cleanup EXIT
 cleanup
 
 # We have to push the CA into the ddev-global-cache volume so it will be respected
-docker run -t --rm -u "$MOUNTUID:$MOUNTGID" -v "$(mkcert -CAROOT):/mnt/mkcert" -v ddev-global-cache:/mnt/ddev-global-cache $DOCKER_IMAGE bash -c "${SUDO} mkdir -p /mnt/ddev-global-cache/mkcert && ${SUDO} chmod -R ugo+w /mnt/ddev-global-cache/* && ${SUDO} cp -R /mnt/mkcert /mnt/ddev-global-cache"
+docker run --rm  -v "$(mkcert -CAROOT):/mnt/mkcert" -v ddev-global-cache:/mnt/ddev-global-cache ${DOCKER_IMAGE} bash -c "mkdir -p /mnt/ddev-global-cache/{mkcert,bashhistory,ddev-live,terminus} && cp -R /mnt/mkcert /mnt/ddev-global-cache/ && chown -Rf ${MOUNTUID}:${MOUNTGID} /mnt/ddev-global-cache/* && chmod -Rf ugo+w /mnt/ddev-global-cache/*"
 
 # Run general tests with a default container
 docker run -u "$MOUNTUID:$MOUNTGID" -p $HOST_HTTP_PORT:$CONTAINER_HTTP_PORT -p $HOST_HTTPS_PORT:$CONTAINER_HTTPS_PORT -e "DOCROOT=docroot" -e "DDEV_PHP_VERSION=${PHP_VERSION}" -e "DDEV_WEBSERVER_TYPE=${WEBSERVER_TYPE}" -d --name $CONTAINER_NAME -v ddev-global-cache:/mnt/ddev-global-cache -d $DOCKER_IMAGE >/dev/null
