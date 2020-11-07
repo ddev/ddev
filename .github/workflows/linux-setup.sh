7f274a953951d502159c5c578047e76ed9812ac1
Dennis Ameling <dennis@dennisameling.com>
2020-11-07 15:14:37 +0100

    Install more Linux deps

--- .github/workflows/linux-setup.sh
+++ .github/workflows/linux-setup.sh
@@ -2,5 +2,7 @@
 
 # Install misc dependencies
 sudo apt-get update -qq
-sudo apt-get install build-essential libnss3-tools libcurl4-gnutls-dev
+sudo apt-get install mysql-client zip jq expect nfs-kernel-server build-essential curl git libnss3-tools libcurl4-gnutls-dev
 brew install make mysql-client mkcert
+
+mkcert -install
