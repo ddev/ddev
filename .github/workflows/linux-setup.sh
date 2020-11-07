0779ca0db7675180acd6fbaa3efbbcbfc5b662b5
Dennis Ameling <dennis@dennisameling.com>
2020-11-07 14:05:47 +0100

    Try to run both on macos and linux

--- .github/workflows/linux-setup.sh
+++ .github/workflows/linux-setup.sh
@@ -1,54 +1,17 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-set -o errexit
+# Setup Docker + Docker buildx
+sudo apt-get install docker-ce-cli
 
-# Basic tools
+BUILDX_BINARY_URL="https://github.com/docker/buildx/releases/download/v0.4.2/buildx-v0.4.2.linux-amd64"
 
-set -x
+curl --output docker-buildx \
+    --silent --show-error --location --fail --retry 3 \
+    "$BUILDX_BINARY_URL"
 
-if [ ! -z "${DOCKERHUB_PULL_USERNAME:-}" ]; then
-  set +x
-  echo "${DOCKERHUB_PULL_PASSWORD}" | docker login --username "${DOCKERHUB_PULL_USERNAME}" --password-stdin
-  set -x
-fi
+mkdir -p ~/.docker/cli-plugins
+mv docker-buildx ~/.docker/cli-plugins
+chmod a+x ~/.docker/cli-plugins/docker-buildx
 
-sudo apt-get update -qq
-sudo apt-get install -qq mysql-client zip jq expect nfs-kernel-server build-essential curl git libnss3-tools libcurl4-gnutls-dev
-
-curl -sSL --fail -o /tmp/ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && sudo unzip -o -d /usr/local/bin /tmp/ngrok.zip
-
-if [ ! -d /home/linuxbrew/.linuxbrew/bin ] ; then
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-fi
-
-echo "export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH" >>~/.bashrc
-
-# Without this .curlrc CircleCI linux image doesn't respect mkcert certs
-echo "capath=/etc/ssl/certs/" >>~/.curlrc
-
-. ~/.bashrc
-
-export HOMEBREW_NO_AUTO_UPDATE=1
-export HOMEBREW_NO_INSTALL_CLEANUP=1
-
-brew tap drud/ddev >/dev/null
-for item in golang golangci-lint mkcert bats-core; do
-    brew install $item >/dev/null || /home/linuxbrew/.linuxbrew/bin/brew upgrade $item >/dev/null
-done
-
-mkcert -install
-
-primary_ip=$(ip route get 1 | awk '{gsub("^.*src ",""); print $1; exit}')
-
-sudo bash -c "cat <<EOF >/etc/exports
-${HOME} ${primary_ip}/255.255.255.255(rw,sync,no_subtree_check)
-/tmp ${primary_ip}/255.255.255.255(rw,sync,no_subtree_check)
-EOF"
-
-sudo service nfs-kernel-server restart
-
-# Install ghr
-GHR_RELEASE="v0.13.0"
-curl -fsL -o /tmp/ghr.tar.gz https://github.com/tcnksm/ghr/releases/download/${GHR_RELEASE}/ghr_${GHR_RELEASE}_linux_amd64.tar.gz
-sudo tar -C /usr/local/bin --strip-components=1 -xzf /tmp/ghr.tar.gz ghr_${GHR_RELEASE}_linux_amd64/ghr
-ghr -v
+# Install other dependencies
+brew install make mysql-client mkcert
