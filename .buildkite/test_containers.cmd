4c26dd0c2d114a801f40cd6693ce54fff9729ceb
Dennis Ameling <dennis@dennisameling.com>
2020-11-07 15:43:34 +0100

    Add back win10_container

---
+++ .buildkite/test_containers.cmd
@@ -0,0 +1,9 @@
+@echo "Building using bash and build.sh"
+"C:\Program Files\git\bin\bash" .buildkite/test_containers.sh
+
+if %ERRORLEVEL% EQU 0 (
+   @echo Successful build
+) else (
+   @echo Failure Reason Given is %errorlevel%
+   exit /b %errorlevel%
+)
