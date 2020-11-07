23a6cd94fde02087dd79d52d618a80388c72b60f
Dennis Ameling <dennis@dennisameling.com>
2020-11-07 15:00:29 +0100

    Add libcurl4 dev as well

--- .github/workflows/linux-setup.sh
+++ .github/workflows/linux-setup.sh
@@ -2,5 +2,5 @@
 
 # Install misc dependencies
 sudo apt-get update -qq
-sudo apt-get install build-essential libnss3-tools
+sudo apt-get install build-essential libnss3-tools libcurl4-gnutls-dev
 brew install make mysql-client mkcert
