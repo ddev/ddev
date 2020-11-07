9004665b4743ecc036cc868982819b5252147ef4
Dennis Ameling <dennis@dennisameling.com>
2020-11-07 14:51:18 +0100

    Install libnss3 on Linux

--- .github/workflows/linux-setup.sh
+++ .github/workflows/linux-setup.sh
@@ -2,5 +2,5 @@
 
 # Install misc dependencies
 sudo apt-get update -qq
-sudo apt-get install build-essential
+sudo apt-get install build-essential libnss3-tools
 brew install make mysql-client mkcert
