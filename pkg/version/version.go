fc4610433228908087846efec41c17b6610c04c5
Randy Fay <randy@randyfay.com>
2020-11-11 14:24:03 -0700

    Use simpler ssl_protocols config in ddev_router, accept TLSv1, 1.1, 1.2, 1.3 (#2632)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -43,7 +43,7 @@ var DockerComposeFileFormatVersion = "3.6"
 var WebImg = "drud/ddev-webserver"
 
 // WebTag defines the default web image tag for drud dev
-var WebTag = "20201109_ddev_live_update" // Note that this can be overridden by make
+var WebTag = "20201111_ssl_protocols" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "drud/ddev-dbserver"
@@ -61,7 +61,7 @@ var DBATag = "5" // Note that this can be overridden by make
 var RouterImage = "drud/ddev-router"
 
 // RouterTag defines the tag used for the router.
-var RouterTag = "v1.16.0-rc1" // Note that this can be overridden by make
+var RouterTag = "20201111_ssl_protocols" // Note that this can be overridden by make
 
 var SSHAuthImage = "drud/ddev-ssh-agent"
 
