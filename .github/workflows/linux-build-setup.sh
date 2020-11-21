36f59082bfac20564226c952ba5c992d90022aa6
Dennis Ameling <dennis@dennisameling.com>
2020-11-21 14:00:26 +0100

    Add makensis for Windows build

---
+++ .github/workflows/linux-build-setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Needed for the build jobs, e.g. for Windows makensis
+
+export HOMEBREW_NO_AUTO_UPDATE=1
+export HOMEBREW_NO_INSTALL_CLEANUP=1
+
+for item in osslsigncode makensis; do
+    brew install $item >/dev/null || /home/linuxbrew/.linuxbrew/bin/brew upgrade $item >/dev/null
+done
+
+# Get the Stubs and Plugins for makensis; the linux makensis build doesn't do this.
+wget https://sourceforge.net/projects/nsis/files/NSIS%203/3.04/nsis-3.04.zip/download && sudo unzip -o -d /usr/local/share download && sudo mv /usr/local/share/nsis-3.04 /usr/local/share/nsis
+wget https://github.com/GsNSIS/EnVar/releases/latest/download/EnVar-Plugin.zip && sudo unzip -o -d /usr/local/share/nsis EnVar-Plugin.zip
+wget https://github.com/DigitalMediaServer/NSIS-INetC-plugin/releases/latest/download/INetC.zip && sudo unzip -o -d /usr/local/share/nsis/Plugins INetC.zip
