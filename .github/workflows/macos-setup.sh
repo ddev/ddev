2c4d22e40f6ba16c575d4187f3ec65b2d802c92c
Dennis Ameling <dennis@dennisameling.com>
2020-12-22 12:53:00 +0100

    Apply some feedback, try running on macOS as well

--- .github/workflows/macos-setup.sh
+++ .github/workflows/macos-setup.sh
@@ -4,7 +4,7 @@
 mkdir ~/.docker
 brew install docker docker-machine docker-compose
 mkdir ~/.docker/cli-plugins
-curl -sSL https://github.com/docker/buildx/releases/download/v0.4.2/buildx-v0.4.2.darwin-amd64 -o ~/.docker/cli-plugins/docker-buildx
+curl -sSL https://github.com/docker/buildx/releases/download/v0.5.1/buildx-v0.5.1.darwin-amd64 -o ~/.docker/cli-plugins/docker-buildx
 chmod a+x ~/.docker/cli-plugins/docker-buildx
 docker buildx version
 docker-machine create --driver virtualbox default
