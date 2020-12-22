23bfbfd5e8bdaa92660e9011b1a03656238fa658
Dennis Ameling <dennis@dennisameling.com>
2020-12-22 14:44:41 +0100

    Add boot2docker ISO on Mac

--- .github/workflows/linux-build-setup.sh
+++ .github/workflows/linux-build-setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -eu -o pipefail
+
 # Needed for the build jobs, e.g. for Windows makensis
 
 export HOMEBREW_NO_AUTO_UPDATE=1
@@ -13,3 +15,5 @@ done
 wget https://sourceforge.net/projects/nsis/files/NSIS%203/3.06.1/nsis-3.06.1.zip/download && sudo unzip -o -d /usr/local/share download && sudo mv /usr/local/share/nsis-3.06.1 /usr/local/share/nsis
 wget https://github.com/GsNSIS/EnVar/releases/latest/download/EnVar-Plugin.zip && sudo unzip -o -d /usr/local/share/nsis EnVar-Plugin.zip
 wget https://github.com/DigitalMediaServer/NSIS-INetC-plugin/releases/latest/download/INetC.zip && sudo unzip -o -d /usr/local/share/nsis/Plugins INetC.zip
+
+set +eu
