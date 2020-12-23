fec198e27795f6429410b8ee20aebeb37c4fc64b
Dennis Ameling <dennis@dennisameling.com>
2020-12-23 22:26:46 +0100

    Add Windows self-hosted CI

---
+++ .github/workflows/test.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+# This script is used to build drud/ddev using GH Actions
+
+echo "--- GitHub Actions building at $(date) on $(hostname) for OS=$(go env GOOS) in $PWD with golang=$(go version) docker=$(docker version --format '{{.Server.Version}}') and docker-compose $(docker-compose version --short) ddev version=$(ddev --version)"
+
+export GOTEST_SHORT=1
+export DRUD_NONINTERACTIVE=true
+
+set -o errexit
+set -o pipefail
+set -o nounset
+set -x
+
+# On macOS, restart docker to avoid bugs where containers can't be deleted
+if [ "${OSTYPE%%[0-9]*}" = "darwin" ]; then
+  killall Docker || true
+  nohup /Applications/Docker.app/Contents/MacOS/Docker --unattended &
+  sleep 10
+fi
+
+# Make sure docker is working
+echo "Waiting for docker to come up: $(date)"
+date && timeout -v 10m bash -c 'while ! docker ps >/dev/null 2>&1 ; do
+  sleep 10
+  echo "Waiting for docker to come up: $(date)"
+done'
+echo "Testing again to make sure docker came up: $(date)"
+if ! docker ps >/dev/null 2>&1 ; then
+  echo "Docker is not running, exiting"
+  exit 1
+fi
+
+if [ ! -z "${DOCKERHUB_PULL_USERNAME:-}" ]; then
+  set +x
+  echo "${DOCKERHUB_PULL_PASSWORD:-}" | docker login --username "${DOCKERHUB_PULL_USERNAME}" --password-stdin
+  set -x
+fi
+
+rm -rf ~/.ddev/Test* ~/.ddev/global_config.yaml ~/.ddev/homeadditions ~/.ddev/commands
+
+# There are discrepancies in golang hash checking in 1.11+, so kill off modcache to solve.
+# See https://github.com/golang/go/issues/27925
+# This can probably be removed when current work is merged 2018-12-27
+# go clean -modcache  (Doesn't work due to current bug in golang)
+chmod -R u+w ~/go/pkg && rm -rf ~/go/pkg/*
+
+# Run any testbot maintenance that may need to be done
+echo "--- running testbot_maintenance.sh"
+bash $(dirname $0)/testbot_maintenance.sh
+
+echo "--- cleaning up docker and Test directories"
+echo "Warning: deleting all docker containers and deleting ~/.ddev/Test*"
+ddev poweroff || true
+if [ "$(docker ps -aq | wc -l )" -gt 0 ] ; then
+	docker rm -f $(docker ps -aq) >/dev/null 2>&1 || true
+fi
+docker system prune --volumes --force >/dev/null || true
+
+# Our testbot should be sane, run the testbot checker to make sure.
+echo "--- running sanetestbot.sh"
+./.github/workflows/sanetestbot.sh
+
+# Update all images that could have changed
+( docker images | awk '/drud/ {print $1":"$2 }' | xargs -L1 docker pull ) || true
+
+# homebrew sometimes removes /usr/local/etc/my.cnf.d
+mkdir -p /usr/local/etc/my.cnf.d
+
+echo "Running tests..."
+time make test
+RV=$?
+echo "build.sh completed with status=$RV"
+ddev poweroff || true
+exit $RV
