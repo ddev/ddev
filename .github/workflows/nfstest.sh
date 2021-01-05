d51f0512c677c4abefba133cbe15a99844523506
Randy Fay <randy@randyfay.com>
2021-01-04 17:54:18 -0700

    Fix the host-docker-internal reference

--- .github/workflows/nfstest.sh
+++ .github/workflows/nfstest.sh
@@ -18,7 +18,7 @@ if [ -d "/System/Volumes/Data${HOME}/.ddev" ] ; then
 fi
 
 # Find host.docker.internal name using host-docker-internal.sh script
-hostDockerInternal=$($(dirname $0)/../scripts/host-docker-internal.sh)
+hostDockerInternal=$($(dirname $0)/../../scripts/host-docker-internal.sh)
 
 docker volume create --driver local --opt type=nfs --opt o=addr=${hostDockerInternal},hard,nolock,rw --opt device=:${share} nfstest >/dev/null
 docker run -t --rm -v nfstest:/tmp/nfs busybox ls //tmp/nfs >/dev/null
