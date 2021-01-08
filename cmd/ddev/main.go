743152069321f21e9f43a15da97280711716cfd2
Randy Fay <randy@randyfay.com>
2021-01-08 11:58:09 -0700

    Dummy change to be reverted

--- cmd/ddev/main.go
+++ cmd/ddev/main.go
@@ -17,6 +17,5 @@ func main() {
 	if os.Geteuid() == 0 && len(os.Args) > 1 && os.Args[1] != "hostname" {
 		output.UserOut.Fatal("ddev is not designed to be run with root privileges, please run as normal user and without sudo")
 	}
-
 	cmd.Execute()
 }
