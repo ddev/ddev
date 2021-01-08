511be21e117db652a060c175de36734845fc6f53
Randy Fay <randy@randyfay.com>
2021-01-08 12:03:40 -0700

    Revert "Dummy change to be reverted"
    This reverts commit 743152069321f21e9f43a15da97280711716cfd2.

--- cmd/ddev/main.go
+++ cmd/ddev/main.go
@@ -17,5 +17,6 @@ func main() {
 	if os.Geteuid() == 0 && len(os.Args) > 1 && os.Args[1] != "hostname" {
 		output.UserOut.Fatal("ddev is not designed to be run with root privileges, please run as normal user and without sudo")
 	}
+
 	cmd.Execute()
 }
