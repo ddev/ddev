5427087df88b16f67531b953f0ce0bd10a57f4aa
Randy Fay <randy@randyfay.com>
2021-01-12 19:07:28 -0700

    Use default_server in nginx configs instead of server_name _, fixes #2729 (#2731)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -40,7 +40,7 @@ var DockerComposeFileFormatVersion = "3.6"
 var WebImg = "drud/ddev-webserver"
 
 // WebTag defines the default web image tag for drud dev
-var WebTag = "20201229_php7.4" // Note that this can be overridden by make
+var WebTag = "20210106_nginx_default_server" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "drud/ddev-dbserver"
@@ -58,7 +58,7 @@ var DBATag = "5" // Note that this can be overridden by make
 var RouterImage = "drud/ddev-router"
 
 // RouterTag defines the tag used for the router.
-var RouterTag = "20201227_travis_router_http2_max_header" // Note that this can be overridden by make
+var RouterTag = "20210106_nginx_default_server" // Note that this can be overridden by make
 
 var SSHAuthImage = "drud/ddev-ssh-agent"
 
