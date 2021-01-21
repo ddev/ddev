33cd5d481a627ad4a2bf904805f7c6aaf882b794
Randy Fay <randy@randyfay.com>
2021-01-21 07:25:02 -0700

    Some thinking

--- cmd/ddev/cmd/auth.go
+++ cmd/ddev/cmd/auth.go
@@ -10,7 +10,8 @@ var AuthCmd = &cobra.Command{
 	Use:   "auth [command]",
 	Short: "A collection of authentication commands",
 	Example: `ddev auth ssh
-ddev auth pantheon`,
+ddev auth pantheon
+ddev auth ddev-live`,
 	Run: func(cmd *cobra.Command, args []string) {
 		err := cmd.Usage()
 		util.CheckErr(err)
@@ -19,4 +20,10 @@ ddev auth pantheon`,
 
 func init() {
 	RootCmd.AddCommand(AuthCmd)
+
+	// Add additional auth. But they're currently in app. Should they be in global?
+	// Try creating an app. If we get one, add it and add any items we find.
+	// Or perhaps we should be loading completely different file.
+	// How about calling it something other than "provider"? I guess the idea was "hosting provider"
+	// maybe.
 }
