9db0ee510d0062a106018e9e625f3cb3a4fe2830
Randy Fay <randy@randyfay.com>
2021-02-02 11:51:28 -0700

    Platform test broke somehow

--- pkg/ddevapp/providerPlatform_test.go
+++ pkg/ddevapp/providerPlatform_test.go
@@ -85,7 +85,7 @@ func TestPlatformPull(t *testing.T) {
 	err = app.WriteConfig()
 	require.NoError(t, err)
 
-	provider, err := app.GetProvider("")
+	provider, err := app.GetProvider("platform")
 	require.NoError(t, err)
 	err = app.Start()
 	require.NoError(t, err)
