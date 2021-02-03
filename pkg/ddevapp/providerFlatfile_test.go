ce36a509953213f2f6a5347ba26898b036b33e97
Randy Fay <randy@randyfay.com>
2021-02-03 15:42:40 -0700

    Make flatfile test work on Windows

--- pkg/ddevapp/providerFlatfile_test.go
+++ pkg/ddevapp/providerFlatfile_test.go
@@ -1,13 +1,13 @@
 package ddevapp_test
 
 import (
-	"fmt"
 	"github.com/drud/ddev/pkg/dockerutil"
-	"github.com/drud/ddev/pkg/exec"
 	"github.com/drud/ddev/pkg/nodeps"
 	"github.com/stretchr/testify/require"
 	"io/ioutil"
 	"os"
+	"os/exec"
+	"path"
 	"path/filepath"
 	"strings"
 	"testing"
@@ -48,13 +48,13 @@ func TestFlatfilePull(t *testing.T) {
 		assert.NoError(err)
 	})
 
-	_, err = exec.RunCommand("bash", []string{"-c", fmt.Sprintf("%s >/dev/null", DdevBin)})
+	_, err = exec.Command(DdevBin).CombinedOutput()
 	require.NoError(t, err)
 
 	// Build our Flatfile.yaml from the example file
 	s, err := ioutil.ReadFile(app.GetConfigPath("providers/flatfile.yaml.example"))
 	require.NoError(t, err)
-	x := strings.Replace(string(s), "~/Dropbox", filepath.Join(testDir, "testdata", t.Name()), -1)
+	x := strings.Replace(string(s), "~/Dropbox", path.Join(dockerutil.MassageWindowsHostMountpoint(testDir), "testdata", t.Name()), -1)
 	appRoot := dockerutil.MassageWindowsHostMountpoint(app.AppRoot)
 	x = strings.Replace(x, "/full/path/to/project/root", appRoot, -1)
 	err = ioutil.WriteFile(app.GetConfigPath("providers/flatfile.yaml"), []byte(x), 0666)
@@ -71,7 +71,10 @@ func TestFlatfilePull(t *testing.T) {
 	assert.NoError(err)
 
 	assert.FileExists(filepath.Join(app.AppRoot, app.Docroot, app.GetUploadDir(), "docs/developers/building-contributing.md"))
-	out, err := exec.RunCommand("bash", []string{"-c", fmt.Sprintf(`echo 'select COUNT(*) from users_field_data where mail="margaret.hopper@example.com";' | %s mysql -N`, DdevBin)})
+	out, _, err := app.Exec(&ExecOpts{
+		Cmd:     "echo 'select COUNT(*) from users_field_data where mail=\"margaret.hopper@example.com\";' | mysql -N",
+		Service: "db",
+	})
 	assert.NoError(err)
 	assert.True(strings.HasPrefix(out, "1\n"))
 }
