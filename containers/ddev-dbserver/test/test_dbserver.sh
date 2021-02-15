a820be0b53722f3909cb2becd0b3fcd5dfa131b6
Randy Fay <randy@randyfay.com>
2021-02-14 17:12:04 -0700

    Update test_dbserver.sh to take simpler args

--- containers/ddev-dbserver/test/test_dbserver.sh
+++ containers/ddev-dbserver/test/test_dbserver.sh
@@ -1,59 +1,24 @@
 #!/bin/bash
 
 # Find the directory of this script
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 
 set -o errexit
 set -o pipefail
 set -o nounset
 
-if [ -z "$1" ]; then echo "Argument 'version' is required"; exit 1; fi
-VERSION=$1
-
-if [[ -n "${2-}" ]]; then
-    ONLY_TESTS_FOR=$2
-else
-    ONLY_TESTS_FOR=
+if [ $# != 3 ]; then
+  echo "Usage: $0 <db_type> <db_version> <tag>, for example $0 mariadb 10.3 v1.17.0"
+  exit 1
 fi
+export DB_TYPE=$1
+export DB_VERSION=$2
+export TAG=$3
+export IMAGE=drud/ddev-dbserver-${DB_TYPE}-${DB_VERSION}:${TAG}
 
-function cleanup {
-    true
-}
-trap cleanup EXIT
+export CURRENT_ARCH=$(arch)
 
-export tag=${VERSION}
-export CURRENT_ARCH=$($DIR/../../get_arch.sh)
-
-# Get database versions per database type
-source $DIR/../database-versions
-
-export MARIADB_VERSIONS="MARIADB_VERSIONS_$CURRENT_ARCH"
-export MYSQL_VERSIONS="MYSQL_VERSIONS_$CURRENT_ARCH"
-
-export DB_TYPE=mariadb
-for v in ${!MARIADB_VERSIONS}; do
-    if [[ "$ONLY_TESTS_FOR" == "mysql" ]]; then continue; fi;
-    export baseversion=$(echo $v | awk -F ':' '{print $1}')
-    export fullversion=$(echo $v | awk -F ':' '{print $2}')
-    export IMAGE="drud/ddev-dbserver-$DB_TYPE-$baseversion:$tag"
-    export DB_VERSION=$baseversion
-    # /usr/local/bin is added for git-bash, where it may not be in the $PATH.
-    export PATH="/usr/local/bin:$PATH"
-    bats test || ( echo "bats tests failed for $DB_TYPE $baseversion" && exit 5 )
-    printf "Test successful for $DB_TYPE $baseversion\n\n"
-done
-
-export DB_TYPE=mysql
-for v in ${!MYSQL_VERSIONS}; do
-    if [[ "$ONLY_TESTS_FOR" == "mariadb" ]]; then continue; fi;
-    export baseversion=$(echo $v | awk -F ':' '{print $1}')
-	export fullversion=$(echo $v | awk -F ':' '{print $2}')
-    export IMAGE="drud/ddev-dbserver-$DB_TYPE-$baseversion:$tag"
-    export DB_VERSION=$baseversion
-    # /usr/local/bin is added for git-bash, where it may not be in the $PATH.
-    export PATH="/usr/local/bin:$PATH"
-    bats test || ( echo "bats tests failed for $DB_TYPE $baseversion" && exit 5 )
-    printf "Test successful for $DB_TYPE $baseversion\n\n"
-done
-
-echo "Test successful"
+# /usr/local/bin is added for git-bash, where it may not be in the $PATH.
+export PATH="/usr/local/bin:$PATH"
+bats test || (echo "bats tests failed for DB_TYPE ${DB_TYPE} DB_VERSION=${DB_VERSION} TAG=${TAG}" && exit 2)
+echo "Test successful for DB_TYPE ${DB_TYPE} DB_VERSION=${DB_VERSION} TAG=${TAG}"
