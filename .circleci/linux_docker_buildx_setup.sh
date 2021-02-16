9eed4ce9366b387060783605602937d61168231d
Randy Fay <randy@randyfay.com>
2021-02-16 16:37:27 -0700

    Bump Circleci buildx to current 0.5.1

--- .circleci/linux_docker_buildx_setup.sh
+++ .circleci/linux_docker_buildx_setup.sh
@@ -1,6 +1,6 @@
 sudo apt-get install docker-ce-cli binfmt-support qemu-user-static
 
-BUILDX_BINARY_URL="https://github.com/docker/buildx/releases/download/v0.4.2/buildx-v0.4.2.linux-amd64"
+BUILDX_BINARY_URL="https://github.com/docker/buildx/releases/download/v0.5.1/buildx-v0.5.1.linux-amd64"
 
 curl --output docker-buildx \
     --silent --show-error --location --fail --retry 3 \
