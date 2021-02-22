8e51b642b1550e918164d9aa283b1a2d9946d141
Randy Fay <randy@randyfay.com>
2021-02-22 11:11:41 -0700

    Revert "Remove magerun from web container as people use composer-installed version (#2768)"
    This reverts commit 8c939be09c6739acdaf250c44a4d98905ceae756.

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -40,7 +40,7 @@ var DockerComposeFileFormatVersion = "3.6"
 var WebImg = "drud/ddev-webserver"
 
 // WebTag defines the default web image tag for drud dev
-var WebTag = "20210225_fix_acquia_auth" // Note that this can be overridden by make
+var WebTag = "20210126_php8_extensions" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "drud/ddev-dbserver"
