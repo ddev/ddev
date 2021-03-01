ecbc867d5241b9550fa02016c64ace5f94b29d32
Randy Fay <randy@randyfay.com>
2021-03-01 15:32:12 -0700

    Remove -qq so we can see what and why

--- .travis/linux_travis_arm64_setup.sh
+++ .travis/linux_travis_arm64_setup.sh
@@ -13,7 +13,7 @@ if [ ! -z "${DOCKERHUB_PULL_USERNAME:-}" ]; then
 fi
 
 sudo apt-get -qq update
-sudo rm -f /usr/local/bin/jq && sudo apt-get -qq install -y mysql-client zip jq expect nfs-kernel-server build-essential curl git libnss3-tools libcurl4-gnutls-dev docker-compose
+sudo rm -f /usr/local/bin/jq && sudo apt-get install -y mysql-client zip jq expect nfs-kernel-server build-essential curl git libnss3-tools libcurl4-gnutls-dev docker-compose
 
 # Copy docker-compose to /usr/local/bin because Travis' pre-installed version leads to exec format error
 sudo cp /usr/bin/docker-compose /usr/local/bin/docker-compose
