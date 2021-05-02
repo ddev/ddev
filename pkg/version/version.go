b192df0427924ced678cff9c01508d390a2a745f
Randy Fay <randy@randyfay.com>
2021-05-02 17:22:06 -0600

    Bump version.go to new webserver version

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -40,7 +40,7 @@ var DockerComposeFileFormatVersion = "3.6"
 var WebImg = "drud/ddev-webserver"
 
 // WebTag defines the default web image tag for drud dev
-var WebTag = "v1.17.2" // Note that this can be overridden by make
+var WebTag = "20210502_thomasdiluccio_blackfire" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "drud/ddev-dbserver"
