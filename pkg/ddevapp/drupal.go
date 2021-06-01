40f9dbb09720383a028b79d59c3d185412c94cfc
Mark Halliwell <mark@unicorn.fail>
2021-06-01 15:47:01 -0500

    Check ENV variable first before doing a FS stat in settings.ddev.php (#3017)

--- pkg/ddevapp/drupal.go
+++ pkg/ddevapp/drupal.go
@@ -69,7 +69,7 @@ func NewDrupalSettings(app *DdevApp) *DrupalSettings {
 const settingsIncludeStanza = `
 // Automatically generated include for settings managed by ddev.
 $ddev_settings = dirname(__FILE__) . '/settings.ddev.php';
-if (is_readable($ddev_settings) && getenv('IS_DDEV_PROJECT') == 'true') {
+if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
   require $ddev_settings;
 }
 `
