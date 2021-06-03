2b1e70cfe6116081ef3d6748186c8d7c7f6551e7
Randy Fay <randy@randyfay.com>
2021-06-03 11:45:16 -0600

    Update docker images for upcoming ddev v1.17.3 (#3035)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -40,13 +40,13 @@ var DockerComposeFileFormatVersion = "3.6"
 var WebImg = "drud/ddev-webserver"
 
 // WebTag defines the default web image tag for drud dev
-var WebTag = "20210602_ddev_webserver_apache_LimitRequestFieldSize" // Note that this can be overridden by make
+var WebTag = "v1.17.3" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "drud/ddev-dbserver"
 
 // BaseDBTag is the main tag, DBTag is constructed from it
-var BaseDBTag = "v1.17.0"
+var BaseDBTag = "v1.17.3"
 
 // DBAImg defines the default phpmyadmin image tag used for applications.
 var DBAImg = "phpmyadmin"
@@ -58,7 +58,7 @@ var DBATag = "5" // Note that this can be overridden by make
 var RouterImage = "drud/ddev-router"
 
 // RouterTag defines the tag used for the router.
-var RouterTag = "20210602_ddev_router_large_client_headers" // Note that this can be overridden by make
+var RouterTag = "v1.17.3" // Note that this can be overridden by make
 
 // SSHAuthImage is image for agent
 var SSHAuthImage = "drud/ddev-ssh-agent"
