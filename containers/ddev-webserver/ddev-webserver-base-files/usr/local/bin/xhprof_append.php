0b896fd8aac0216ecc6f8bde68a6586030d70c40
Randy Fay <randy@randyfay.com>
2021-06-12 07:35:29 -0600

    Proper quotes on DDEV_HOSTNAME

--- containers/ddev-webserver/ddev-webserver-base-files/usr/local/bin/xhprof_append.php
+++ containers/ddev-webserver/ddev-webserver-base-files/usr/local/bin/xhprof_append.php
@@ -1,6 +1,6 @@
 <?php
 $xhprof_data = xhprof_disable();
-$appNamespace = getenv(DDEV_HOSTNAME);
+$appNamespace = getenv("DDEV_HOSTNAME");
 include_once '/var/www/xhprof/xhprof_lib/utils/xhprof_lib.php';
 include_once '/var/www/xhprof/xhprof_lib/utils/xhprof_runs.php';
 
