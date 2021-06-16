ffcc8ae68ecdc2d719301588dbf215e4f5f1de89
Randy Fay <randy@randyfay.com>
2021-06-15 21:57:53 -0600

    Make it output a nice link on bottom of screen

--- containers/ddev-webserver/ddev-webserver-base-files/usr/local/bin/xhprof_append.php
+++ containers/ddev-webserver/ddev-webserver-base-files/usr/local/bin/xhprof_append.php
@@ -4,5 +4,11 @@ $appNamespace = "ddev";
 include_once '/var/www/xhprof/xhprof_lib/utils/xhprof_lib.php';
 include_once '/var/www/xhprof/xhprof_lib/utils/xhprof_runs.php';
 
+
 $xhprof_runs = new XHProfRuns_Default();
-$xhprof_runs->save_run($xhprof_data, $appNamespace);
+$run_id =$xhprof_runs->save_run($xhprof_data, $appNamespace);
+
+$base_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]/xhprof/";
+
+$profiler_url = sprintf('%s/index.php?run=%s&amp;source=%s', $base_link, $run_id, $profiler_namespace);
+echo '<a href="'. $profiler_url .'" target="_blank">xhprof profiler output</a>';
