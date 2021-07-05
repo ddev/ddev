e1e71e75c2fb1c2cc0226d8020aec50f32c91c06
Randy Fay <randy@randyfay.com>
2021-07-05 16:55:36 -0600

    Go back to just using the traditional xhprof page

--- containers/ddev-webserver/ddev-webserver-base-files/usr/local/bin/xhprof_prepend.php
+++ containers/ddev-webserver/ddev-webserver-base-files/usr/local/bin/xhprof_prepend.php
@@ -20,19 +20,6 @@ function xhprof_completion() {
     include_once '/var/www/xhprof/xhprof_lib/utils/xhprof_lib.php';
     include_once '/var/www/xhprof/xhprof_lib/utils/xhprof_runs.php';
 
-    if (!is_dir($xhprof_link_dir) && !mkdir($xhprof_link_dir, 0777, true)) {
-      exit("failed to create $xhprof_link_dir");
-    }
-
     $xhprof_runs = new XHProfRuns_Default();
     $run_id = $xhprof_runs->save_run($xhprof_data, $appNamespace);
-
-    $base_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]/xhprof/";
-
-    $run_url = sprintf('%s/xhprof/index.php?run=%s&amp;source=%s', getenv('DDEV_PRIMARY_URL') , $run_id, $appNamespace);
-
-    $run_content = sprintf("<div id='xhprof-footer'><a class='xhprof-link' href='%s'>Click for latest xhprof run</a> or <a class='xhprof-link' target=_blank href='/xhprof/'>all runs</a></div>", $run_url);
-    if (!file_put_contents($xhprof_link_dir . "index.html", $run_content)) {
-        exit("Failed to file_put_contents($xhprof_link_dir/index.html)");
-    }
 }
