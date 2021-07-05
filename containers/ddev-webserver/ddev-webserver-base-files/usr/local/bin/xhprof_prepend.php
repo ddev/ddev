47d405eae371cdd48b4187bec514ea3880099862
Randy Fay <randy@randyfay.com>
2021-07-05 15:07:12 -0600

    Stop trying to output a link on the page itself; breaks all api calls and other things

--- containers/ddev-webserver/ddev-webserver-base-files/usr/local/bin/xhprof_prepend.php
+++ containers/ddev-webserver/ddev-webserver-base-files/usr/local/bin/xhprof_prepend.php
@@ -1,57 +1,38 @@
 <?php
-$xhprof_link_dir = "/var/www/xhprof/xhprof_html/latest/";
-if (!defined('STDIN') && extension_loaded('xhprof') && strpos($_SERVER['REQUEST_URI'], '/xhprof') === false) {
+
+$uri = "none";
+if (!empty($_SERVER) && array_key_exists('REQUEST_URI', $_SERVER)) {
+    $uri = $_SERVER['REQUEST_URI'];
+}
+
+// Enable xhprof profiling if we're not on an xhprof page
+if (extension_loaded('xhprof') && strpos($uri, '/xhprof') === false) {
     xhprof_enable();
-    if (!is_dir($xhprof_link_dir) && !mkdir($xhprof_link_dir, 0777, true)) {
-      exit("failed to create $xhprof_link_dir");
-    }
-
-    print("
-    <style>
-    #xhprof-footer {
-      position: fixed;
-      left: 0;
-      bottom: 0;
-      width: 100%;
-      background-color: blue;
-      text-align: center;
-      color: white
-    }
-    .xhprof-link {
-      color: white
-    }
-    </style>
-    <div id=xhprof-footer><a class=xhprof-link target=_blank href='/xhprof/latest/'>Click for latest xhprof run</a> or <a class='xhprof-link' target=_blank href='/xhprof/'>all runs</a></div>
-
-    <script type='text/javascript'>
-    function xhprof_footer(l) {
-      document.getElementById('xhprof-footer').innerHTML = l;
-    }
-    </script>
-
-    ");
-
     register_shutdown_function('xhprof_completion');
 }
 
+// Write to the xhprof_html output and latest on completion
 function xhprof_completion() {
+    $xhprof_link_dir = "/var/www/xhprof/xhprof_html/latest/";
+
     $xhprof_data = xhprof_disable();
     $appNamespace = "ddev";
     include_once '/var/www/xhprof/xhprof_lib/utils/xhprof_lib.php';
     include_once '/var/www/xhprof/xhprof_lib/utils/xhprof_runs.php';
 
+    if (!is_dir($xhprof_link_dir) && !mkdir($xhprof_link_dir, 0777, true)) {
+      exit("failed to create $xhprof_link_dir");
+    }
 
     $xhprof_runs = new XHProfRuns_Default();
     $run_id = $xhprof_runs->save_run($xhprof_data, $appNamespace);
 
     $base_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]/xhprof/";
 
-    $profiler_url = sprintf('%sindex.php?run=%s&amp;source=%s', $base_link, $run_id, $appNamespace);
+    $run_url = sprintf('%s/xhprof/index.php?run=%s&amp;source=%s', getenv('DDEV_PRIMARY_URL') , $run_id, $appNamespace);
 
-    $run_content = sprintf("<div id='xhprof-footer'><a class='xhprof-link' href='%s'>Click for latest xhprof run</a> or <a class='xhprof-link' target=_blank href='/xhprof/'>all runs</a></div>", $profiler_url);
-    if (!file_put_contents($GLOBALS['xhprof_link_dir'] . "index.html", $run_content)) {
+    $run_content = sprintf("<div id='xhprof-footer'><a class='xhprof-link' href='%s'>Click for latest xhprof run</a> or <a class='xhprof-link' target=_blank href='/xhprof/'>all runs</a></div>", $run_url);
+    if (!file_put_contents($xhprof_link_dir . "index.html", $run_content)) {
         exit("Failed to file_put_contents($xhprof_link_dir/index.html)");
     }
-    $c = "<a class=xhprof-link href='$profiler_url' target=_blank>Click for this xhprof run</a>  or <a class='xhprof-link' target=_blank href='/xhprof/'>all runs</a>";
-    printf('<script type="text/javascript">window.onload = xhprof_footer("%s");</script>', $c);
 }
