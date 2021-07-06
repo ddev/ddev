0ebd0fab58cb2ee4883bbc502ecb0d5e936b3c97
Randy Fay <randy@randyfay.com>
2021-07-05 21:05:35 -0600

    Improve xhprof availability and revert to just using the classic run list page (#3087)
    * Remove usage of xhprof_append.php, unneeded
    * Now not doing xhprof on xhprof pages
    * Improve the xhprof.conf so it doesn't redirect wrong
    * Stop trying to output a link on the page itself; breaks all api calls and other things
    * Go back to just using the traditional xhprof page

--- containers/ddev-webserver/ddev-webserver-base-files/usr/local/bin/xhprof_prepend.php
+++ containers/ddev-webserver/ddev-webserver-base-files/usr/local/bin/xhprof_prepend.php
@@ -1,4 +1,25 @@
 <?php
-if (extension_loaded('xhprof')) {
+
+$uri = "none";
+if (!empty($_SERVER) && array_key_exists('REQUEST_URI', $_SERVER)) {
+    $uri = $_SERVER['REQUEST_URI'];
+}
+
+// Enable xhprof profiling if we're not on an xhprof page
+if (extension_loaded('xhprof') && strpos($uri, '/xhprof') === false) {
     xhprof_enable();
+    register_shutdown_function('xhprof_completion');
+}
+
+// Write to the xhprof_html output and latest on completion
+function xhprof_completion() {
+    $xhprof_link_dir = "/var/www/xhprof/xhprof_html/latest/";
+
+    $xhprof_data = xhprof_disable();
+    $appNamespace = "ddev";
+    include_once '/var/www/xhprof/xhprof_lib/utils/xhprof_lib.php';
+    include_once '/var/www/xhprof/xhprof_lib/utils/xhprof_runs.php';
+
+    $xhprof_runs = new XHProfRuns_Default();
+    $run_id = $xhprof_runs->save_run($xhprof_data, $appNamespace);
 }
