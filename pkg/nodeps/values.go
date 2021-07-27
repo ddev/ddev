fa76dd9349b4e05c77132be3cb013424f5b5ecc6
Randy Fay <randy@randyfay.com>
2021-07-27 13:16:38 -0600

    Bump required mutagen version to beta4

--- pkg/nodeps/values.go
+++ pkg/nodeps/values.go
@@ -88,7 +88,7 @@ const (
 	// DdevDefaultTLD is the top-level-domain used by default, can be overridden
 	DdevDefaultTLD                  = "ddev.site"
 	InternetDetectionTimeoutDefault = 750
-	RequiredMutagenVersion          = "0.12.0-beta3"
+	RequiredMutagenVersion          = "0.12.0-beta4"
 )
 
 // IsValidPHPVersion is a helper function to determine if a PHP version is valid, returning
