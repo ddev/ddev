ce78060d301d425f6cbdeba987b4f419a8ed9608
Randy Fay <randy@randyfay.com>
2021-08-08 16:30:14 -0600

    Bump ghr version to current

--- .circleci/macos_circle_vm_setup.sh
+++ .circleci/macos_circle_vm_setup.sh
@@ -16,8 +16,8 @@ brew install homebrew/cask/ngrok >/dev/null
 brew install drud/ddev/ddev >/dev/null
 brew unlink python@2 >/dev/null || true
 
-brew install mysql-client zip makensis jq expect coreutils golang drud/ddev/ddev mkcert ghr gnu-getopt libgsf glib pcre >/dev/null || true
-brew link mysql-client zip makensis jq expect coreutils golang ddev mkcert ghr gnu-getopt libgsf glib pcre >/dev/null
+brew install mysql-client zip makensis jq expect coreutils golang drud/ddev/ddev mkcert gnu-getopt libgsf glib pcre >/dev/null || true
+brew link mysql-client zip makensis jq expect coreutils golang ddev mkcert gnu-getopt libgsf glib pcre >/dev/null
 
 brew link --force mysql-client >/dev/null
 
