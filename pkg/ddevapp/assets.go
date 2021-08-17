28fa471fd70fc53171a0272dbaadd0c6c9f660b4
Randy Fay <randy@randyfay.com>
2021-08-17 08:54:27 -0600

    Minor comment improvement

--- pkg/ddevapp/assets.go
+++ pkg/ddevapp/assets.go
@@ -6,15 +6,20 @@ import (
 )
 
 //The bundled assets for the project .ddev directory are in directory dotddev_assets
+// And the global .ddev assets are in directory global_dotddev_assets
 //go:embed dotddev_assets global_dotddev_assets
 var bundledAssets embed.FS
 
 // PopulateExamplesCommandsHomeadditions grabs embedded assets and
 // installs them into the named directory
+// Note that running this with no appName (very common) can result in updating
+// a *different* projects assets. So `ddev start something` will first update the current
+// directory's assets (if it's a project) and then later (when called with appName) update
+// the actual project's assets.
 func PopulateExamplesCommandsHomeadditions(appName string) error {
 	app, err := GetActiveApp(appName)
 	// If we have an error from GetActiveApp, it means we're not in a project directory
-	// That's no an error, just means we can't do this work, so return nil.
+	// That's not an error, just means we can't do this work, so return nil.
 	if err != nil {
 		return nil
 	}
