0fbc5604764d3b5e2a72a0b8ce162b3c3eae2ebe
Randy Fay <randy@randyfay.com>
2021-08-20 21:52:53 -0600

    For numeric group names, also try adding user with group id, fixes #3187

--- pkg/ddevapp/config.go
+++ pkg/ddevapp/config.go
@@ -825,7 +825,7 @@ FROM $BASE_IMAGE
 ARG username
 ARG uid
 ARG gid
-RUN (groupadd --gid $gid "$username" || groupadd "$username" || true) && (useradd  -l -m -s "/bin/bash" --gid "$username" --comment '' --uid $uid "$username" || useradd  -l -m -s "/bin/bash" --gid "$username" --comment '' "$username")
+RUN (groupadd --gid $gid "$username" || groupadd "$username" || true) && (useradd  -l -m -s "/bin/bash" --gid "$username" --comment '' --uid $uid "$username" || useradd  -l -m -s "/bin/bash" --gid "$username" --comment '' "$username" || useradd  -l -m -s "/bin/bash" --gid "$gid" --comment '' "$username")
 `
 	if extraPackages != nil {
 		contents = contents + `
