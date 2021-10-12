7fc83727af3b0c5e21264af34819aafc12617023
Christian Münch <christian@muench-worms.de>
2021-10-12 14:49:46 +0200

    Fix tests by starting the whole ddev environment

--- cmd/ddev/cmd/debug-router-nginx-config_test.go
+++ cmd/ddev/cmd/debug-router-nginx-config_test.go
@@ -4,21 +4,32 @@ import (
 	"github.com/drud/ddev/pkg/ddevapp"
 	"github.com/drud/ddev/pkg/exec"
 	asrt "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
 // TestDebugRouterNginxConfigCmd ensures the debug router-nginx-config prints the nginx config to stdout
 func TestDebugRouterNginxConfigCmd(t *testing.T) {
 	assert := asrt.New(t)
+
 	site := TestSites[0]
 	cleanup := site.Chdir()
-	defer cleanup()
 
-	_, err := ddevapp.GetActiveApp(site.Name)
+	app, err := ddevapp.NewApp(site.Dir, false)
 	assert.NoError(err)
 
+	t.Cleanup(func() {
+		// Make sure all databases are back to default empty
+		_ = app.Stop(true, false)
+		cleanup()
+	})
+
+	err = app.Start()
+	require.NoError(t, err)
+
 	// Test default invocation
-	out, err := exec.RunHostCommand(DdevBin, "debug", "router-nginx-config")
+	args := []string{"debug", "router-nginx-config"}
+	out, err := exec.RunCommand(DdevBin, args)
 	assert.NoError(err)
 	assert.Contains(
 		out,
