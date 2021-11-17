8be5e26587c4f7cb6ad3afafa0bab398f3f92b7a
Randy Fay <randy@randyfay.com>
2021-11-17 14:25:02 -0700

    Dont' break startup if .my.cnf doesn't exist

--- containers/ddev-webserver/ddev-webserver-base-scripts/start.sh
+++ containers/ddev-webserver/ddev-webserver-base-scripts/start.sh
@@ -84,7 +84,9 @@ sudo mkdir -p ${CAROOT} && sudo chmod -R ugo+rw ${CAROOT}
 # This will install the certs from $CAROOT (/mnt/ddev-global-cache/mkcert)
 mkcert -install
 
-perl -pi -e "s/host=db/host=ddev-${DDEV_PROJECT}-db/" ~/.my.cnf
+if [ -f ~/.my.cnf ]; then
+  perl -pi -e "s/host=db/host=ddev-${DDEV_PROJECT}-db/" ~/.my.cnf
+fi
 
 # VIRTUAL_HOST is a comma-delimited set of fqdns, convert it to space-separated and mkcert
 CAROOT=$CAROOT mkcert -cert-file /etc/ssl/certs/master.crt -key-file /etc/ssl/certs/master.key ${VIRTUAL_HOST//,/ } localhost 127.0.0.1 ${DOCKER_IP} web ddev-${DDEV_PROJECT:-}-web ddev-${DDEV_PROJECT:-}-web.ddev_default
