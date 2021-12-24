c3eec085af193c0d1a81b911ba258a2af7e15224
Randy Fay <randy@randyfay.com>
2021-12-24 11:32:19 -0700

    Turn off the mkcert again, don't think it can work

--- .github/workflows/macos-colima-setup.sh
+++ .github/workflows/macos-colima-setup.sh
@@ -13,8 +13,13 @@ brew link go
 # This command allows adding CA (in mkcert, etc) without the popup trust prompt
 # Mentioned in https://github.com/actions/virtual-environments/issues/4519#issuecomment-970202641
 sudo security authorizationdb write com.apple.trust-settings.admin allow
+
 # Github actions macOS runners have 14BG RAM so might as well use it.
 # https://docs.github.com/en/actions/using-github-hosted-runners/about-github-hosted-runners#supported-runners-and-hardware-resources
 colima start --cpu 3 --memory 6
 
-sudo mkcert -install && sudo chown -R $UID "$(mkcert -CAROOT)"
+# I haven't been able to get mkcert-trusted certs in there, not sure why
+# You can't answer the security prompt, but that's what the
+# sudo security authorizationdb write com.apple.trust-settings.admin allow
+# was supposed to fix.  rfay 2021-12-24
+#sudo mkcert -install && sudo chown -R $UID "$(mkcert -CAROOT)"
