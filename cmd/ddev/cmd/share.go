25f441f3970e3ef712d77f512402bce7db8fe622
Randy Fay <randy@randyfay.com>
2021-12-26 14:36:57 -0700

    Improve TestShareCmd so it doesn't fail so often (#3475)
    Also removes some obsolete features of ddev share

--- cmd/ddev/cmd/share.go
+++ cmd/ddev/cmd/share.go
@@ -7,7 +7,6 @@ import (
 	"os"
 	"os/exec"
 	"strings"
-	"time"
 )
 
 // DdevShareCommand contains the "ddev share" command
@@ -17,7 +16,6 @@ var DdevShareCommand = &cobra.Command{
 	Long:  `Use "ddev share" or add on extra ngrok commands, like "ddev share --subdomain some-subdomain". Although a few ngrok commands are supported directly, any ngrok flag can be added in the ngrok_args section of .ddev/config.yaml. Requires a free or paid account on ngrok.com; use the "ngrok authtoken" command to set up ngrok.`,
 	Example: `ddev share
 ddev share --subdomain some-subdomain
-ddev share --use-http
 ddev share myproject`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) > 1 {
@@ -37,17 +35,7 @@ ddev share myproject`,
 		if ngrokLoc == "" || err != nil {
 			util.Failed("ngrok not found in path, please install it, see https://ngrok.com/download")
 		}
-		urls := []string{app.GetWebContainerDirectHTTPSURL(), app.GetWebContainerDirectHTTPURL()}
-
-		// If they provided the --use-http flag, we'll not try both https and http
-		useHTTP, err := cmd.Flags().GetBool("use-http")
-		if err != nil {
-			util.Failed("failed to get use-http flag: %v", err)
-		}
-
-		if useHTTP {
-			urls = []string{app.GetWebContainerDirectHTTPURL()}
-		}
+		urls := []string{app.GetWebContainerDirectHTTPURL()}
 
 		var ngrokErr error
 		for _, url := range urls {
@@ -64,16 +52,16 @@ ddev share myproject`,
 				ngrokArgs = append(ngrokArgs, "-subdomain="+sub)
 			}
 
-			if strings.Contains(url, "http://") {
-				util.Warning("Using local http URL, your data may be exposed on the internet. Create a free ngrok account instead...")
-				time.Sleep(time.Second * 3)
-			}
-			util.Success("Running %s %s", ngrokLoc, strings.Join(ngrokArgs, " "))
 			ngrokCmd := exec.Command(ngrokLoc, ngrokArgs...)
 			ngrokCmd.Stdout = os.Stdout
 			ngrokCmd.Stderr = os.Stderr
-			ngrokErr = ngrokCmd.Run()
+			err = ngrokCmd.Start()
+			if err != nil {
+				util.Failed("Failed to run %s %s: %v", ngrokLoc, strings.Join(ngrokArgs, " "), err)
+			}
+			util.Success("Running %s %s", ngrokLoc, strings.Join(ngrokArgs, " "))
 
+			ngrokErr = ngrokCmd.Wait()
 			// nil result means ngrok ran and exited normally.
 			// It seems to do this fine when hit by SIGTERM or SIGINT
 			if ngrokErr == nil {
@@ -103,5 +91,4 @@ ddev share myproject`,
 func init() {
 	RootCmd.AddCommand(DdevShareCommand)
 	DdevShareCommand.Flags().String("subdomain", "", `ngrok --subdomain argument, as in "ngrok --subdomain my-subdomain:, requires paid ngrok.com account"`)
-	DdevShareCommand.Flags().Bool("use-http", false, `Set to true to use unencrypted http local tunnel (required if you do not have an ngrok.com account)"`)
 }
