fd2c4b301504089d39fb5ca9ff21c0d1bb5b2cc5
Randy Fay <randy@randyfay.com>
2022-01-12 18:09:08 -0700

    First shot at downloading, parsing, etc

--- cmd/ddev/cmd/service-get.go
+++ cmd/ddev/cmd/service-get.go
@@ -1,11 +1,25 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"github.com/drud/ddev/pkg/archive"
+	"github.com/drud/ddev/pkg/exec"
 	"github.com/drud/ddev/pkg/fileutil"
 	"github.com/drud/ddev/pkg/util"
+	"github.com/google/go-github/github"
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
+	"os"
+	"path/filepath"
+	"strings"
 )
+
+type serviceDesc struct {
+	Name    string
+	Files   []string
+	Actions []string
+}
 
 // ServiceGet implements the ddev service get command
 var ServiceGet = &cobra.Command{
@@ -26,15 +40,92 @@ var ServiceGet = &cobra.Command{
 		}
 		app := apps[0]
 		serviceRepo := args[0]
-		fName := fmt.Sprintf("docker-compose.%s.yaml", serviceName)
-		if fileutil.FileExists(app.GetConfigPath(fName)) {
-			util.Warning("Service %s already enabled, overwriting it, see %s", serviceName, fName)
+
+		parts := strings.Split(serviceRepo, "/")
+		if len(parts) != 2 {
+			util.Failed("Invalid service name %s", serviceRepo)
 		}
-		err = util.DownloadFile(app.GetConfigPath(fName), serviceRepo, true)
+		owner := parts[0]
+		repo := parts[1]
+		client := github.NewClient(nil)
+		ctx := context.Background()
+		releases, _, err := client.Repositories.ListReleases(ctx, owner, repo, &github.ListOptions{})
+		//util.Success("releases=%v, rsp=%v, err=%v", releases, rsp, err)
 		if err != nil {
-			util.Failed("Download of %s failed: %v", serviceRepo, err)
+			util.Failed("Unable to get releases for %v: %v", serviceRepo, err)
 		}
-		util.Success("Downloaded and enabled service %s, use `ddev restart` to turn it on.", serviceName)
+		if len(releases) == 0 {
+			util.Failed("No releases found for %v", serviceRepo)
+		}
+		f, err := os.CreateTemp("", fmt.Sprintf("%s-%s*.tar.gz", owner, repo))
+		// nolint: errcheck
+		defer f.Close()
+		tarball := f.Name()
+		defer os.RemoveAll(tarball)
+		err = util.DownloadFile(tarball, releases[0].GetTarballURL(), true)
+		if err != nil {
+			util.Failed("Unable to download %v: %v", releases[0].GetTarballURL(), err)
+		}
+		srcDest, err := os.MkdirTemp("", "service_repo_")
+		if err != nil {
+			util.Failed("Unable to create temp dir: %v", err)
+		}
+		defer os.RemoveAll(srcDest)
+
+		err = archive.Untar(tarball, srcDest, "")
+		if err != nil {
+			util.Failed("Unable to untar %v: %v", srcDest, err)
+		}
+
+		list, err := fileutil.ListFilesInDir(srcDest)
+		if err != nil {
+			util.Failed("Unable to list files in %v: %v", srcDest, err)
+		}
+		if len(list) == 0 {
+			util.Failed("No files found in %v", srcDest)
+		}
+		removeDir := list[0]
+
+		yamlFile := filepath.Join(srcDest, removeDir, "install.yaml")
+		yamlContent, err := fileutil.ReadFileIntoString(yamlFile)
+		var s serviceDesc
+		err = yaml.Unmarshal([]byte(yamlContent), &s)
+		if err != nil {
+			util.Failed("Unable to parse %v: %v", yamlFile, err)
+		}
+
+		for _, file := range s.Files {
+			src := filepath.Join(srcDest, removeDir, file)
+			//TODO: How dangerous is this? Rethink.
+			_ = os.RemoveAll(app.GetConfigPath(file))
+			if fileutil.IsDirectory(src) {
+				err = fileutil.CopyDir(src, app.GetConfigPath(file))
+			} else {
+				err = fileutil.CopyFile(src, app.GetConfigPath(file))
+			}
+			if err != nil {
+				util.Failed("Unable to copy %v to %v: %v", src, app.GetConfigPath(file), err)
+			}
+		}
+
+		origDir, _ := os.Getwd()
+
+		//nolint: errcheck
+		defer os.Chdir(origDir)
+		err = os.Chdir(app.GetConfigPath(""))
+		if err != nil {
+			util.Failed("Unable to chdir to %v: %v", app.GetConfigPath(""), err)
+		}
+
+		for _, action := range s.Actions {
+			//TODO: This is ugly and relies on sh for no reason. Fix.
+			out, err := exec.RunHostCommand("sh", "-c", action)
+			if err != nil {
+				util.Failed("Unable to run action %v: %v, output=%s", action, err, out)
+			}
+		}
+
+		util.Success("Downloaded and enabled service %s, use `ddev restart` to turn it on.", serviceRepo)
 	},
 }
 
