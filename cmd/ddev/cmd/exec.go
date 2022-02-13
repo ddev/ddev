d42f2009c876235a03b8566a4900f66870f8001a
Randy Fay <randy@randyfay.com>
2022-02-13 16:29:37 -0700

    Add raw capability to ddev exec

--- cmd/ddev/cmd/exec.go
+++ cmd/ddev/cmd/exec.go
@@ -18,8 +18,11 @@ var DdevExecCmd = &cobra.Command{
 	Use:     "exec <command>",
 	Aliases: []string{"."},
 	Short:   "Execute a shell command in the container for a service. Uses the web service by default.",
-	Long:    `Execute a shell command in the container for a service. Uses the web service by default. To run your command in the container for another service, run "ddev exec --service <service> <cmd>"`,
-	Example: "ddev exec ls /var/www/html\nddev exec --service db\nddev exec -s db\nddev exec -s solr (assuming an add-on service named 'solr')",
+	Long:    `Execute a shell command in the container for a service. Uses the web service by default. To run your command in the container for another service, run "ddev exec --service <service> <cmd>". If you want to use raw, uninterpreted command inside container use --raw as in example.`,
+	Example: `ddev exec ls /var/www/html
+ddev exec --service db\nddev exec -s db
+ddev exec -s solr (assuming an add-on service named 'solr')
+ddev exec --raw -- ls -lR`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) == 0 {
 			err := cmd.Usage()
@@ -42,12 +45,18 @@ var DdevExecCmd = &cobra.Command{
 
 		app.DockerEnv()
 
-		_, _, err = app.Exec(&ddevapp.ExecOpts{
+		opts := &ddevapp.ExecOpts{
 			Service: serviceType,
 			Dir:     execDirArg,
 			Cmd:     strings.Join(args, " "),
 			Tty:     true,
-		})
+		}
+
+		// If they've chosen raw, use the actual passed values
+		if useRaw, _ := cmd.Flags().GetBool("raw"); useRaw {
+			opts.RawCmd = args
+		}
+		_, _, err = app.Exec(opts)
 
 		if err != nil {
 			util.Failed("Failed to execute command %s: %v", strings.Join(args, " "), err)
@@ -58,6 +67,7 @@ var DdevExecCmd = &cobra.Command{
 func init() {
 	DdevExecCmd.Flags().StringVarP(&serviceType, "service", "s", "web", "Defines the service to connect to. [e.g. web, db]")
 	DdevExecCmd.Flags().StringVarP(&execDirArg, "dir", "d", "", "Defines the execution directory within the container")
+	DdevExecCmd.Flags().Bool("raw", true, "Use raw exec (do not interpret with bash inside container)")
 	// This requires flags for exec to be specified prior to any arguments, allowing for
 	// flags to be ignored by cobra for commands that are to be executed in a container.
 	DdevExecCmd.Flags().SetInterspersed(false)
