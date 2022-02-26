2399dbce024e2877c41dad419be9b993f48839d9
Randy Fay <randy@randyfay.com>
2022-02-26 14:27:52 -0700

    Add docker-compose and docker-desktop platform info and colima version

--- cmd/ddev/cmd/debug-test.go
+++ cmd/ddev/cmd/debug-test.go
@@ -1,46 +1,27 @@
 package cmd
 
 import (
-	"fmt"
 	"github.com/drud/ddev/pkg/dockerutil"
-	exec2 "github.com/drud/ddev/pkg/exec"
 	"github.com/drud/ddev/pkg/util"
 	"github.com/drud/ddev/pkg/version"
 	"github.com/spf13/cobra"
 	"os"
-	"os/exec"
-	"strings"
 )
 
-// DebugDockercheckCmd implements the ddev debug dockercheck command
-var DebugDockercheckCmd = &cobra.Command{
-	Use:     "dockercheck",
-	Short:   "Diagnose DDEV docker/colima setup",
-	Example: "ddev debug dockercheck",
+// DebugTestCmdCmd implements the ddev debug test command
+var DebugTestCmdCmd = &cobra.Command{
+	Use:     "test",
+	Short:   "Run diagnostics on ddev using the test_ddev.sh script",
+	Example: "ddev debug test",
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) != 0 {
 			util.Failed("This command takes no additional arguments")
 		}
+		// Ability to do a build on a container
+		// Internet connectivity inside container
+		// Ability to do a build on a container that accesses internet
 
-		versionInfo := version.GetVersionInfo()
-		util.Success("Docker platform: %v", versionInfo["docker-platform"])
-		switch versionInfo["docker-platform"] {
-		case "colima":
-			p, err := exec.LookPath("colima")
-			if err == nil {
-				bashPath := util.FindBashPath()
-				out, err := exec2.RunHostCommand(bashPath, "-c", fmt.Sprintf("%s version | awk '/colima version/ {print $3}'", p))
-				out = strings.Trim(out, "\r\n ")
-				if err == nil {
-					util.Success("Colima version: %v", out)
-				}
-			}
-
-		case "docker desktop":
-			dockerutil.IsDockerDesktop()
-		}
 		util.Success("Using docker context: %s (%s)", dockerutil.DockerContext, dockerutil.DockerHost)
-		util.Success("docker-compose: %s", versionInfo["docker-compose"])
 
 		dockerHost := os.Getenv("DOCKER_HOST")
 		if dockerHost != "" {
@@ -75,14 +56,14 @@ var DebugDockercheckCmd = &cobra.Command{
 		if err != nil {
 			util.Warning("Unable to run simple container: %v", err)
 		} else {
-			util.Success("Able to run simple container that mounts a volume.")
+			util.Success("Able to run simple container that mounts a volume")
 		}
 
 		_, _, err = dockerutil.RunSimpleContainer(version.GetWebImage(), "", []string{"curl", "-sLI", "https://google.com"}, []string{}, []string{}, []string{"ddev-global-cache" + ":/mnt/ddev-globa-cache"}, uid, true, false, nil)
 		if err != nil {
 			util.Warning("Unable to run use internet inside container, many things will fail: %v", err)
 		} else {
-			util.Success("Able to use internet inside container.")
+			util.Success("Able to use internet inside container")
 		}
 
 		dockerutil.CheckAvailableSpace()
@@ -90,5 +71,5 @@ var DebugDockercheckCmd = &cobra.Command{
 }
 
 func init() {
-	DebugCmd.AddCommand(DebugDockercheckCmd)
+	DebugCmd.AddCommand(DebugTestCmdCmd)
 }
