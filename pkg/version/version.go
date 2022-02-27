5764350e5824869388cdd38ca8e672474aae0cbb
Randy Fay <randy@randyfay.com>
2022-02-27 11:46:02 -0700

    Use .bashrc.d to add to $PATH, fixes #3022

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -41,7 +41,7 @@ var DockerComposeFileFormatVersion = "3.6"
 var WebImg = "drud/ddev-webserver"
 
 // WebTag defines the default web image tag
-var WebTag = "20220225_gilbertsoft_graphicsmagick" // Note that this can be overridden by make
+var WebTag = "20220227_path_alter" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "drud/ddev-dbserver"
