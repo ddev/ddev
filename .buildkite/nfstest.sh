7575b584ef8b0a68bad24bed307339cce17ec39e
Randy Fay <randy@randyfay.com>
2022-02-26 19:53:11 -0700

    Fix broken symlink for host-docker-internal.sh

--- .buildkite/nfstest.sh
+++ .buildkite/nfstest.sh
@@ -18,7 +18,8 @@ if [ -d "/System/Volumes/Data${HOME}/.ddev" ] ; then
 fi
 
 # Find host.docker.internal name using host-docker-internal.sh script
-hostDockerInternal=$($(dirname $0)/../scripts/host-docker-internal.sh)
+# This needs to point to the "real" directory because Windows testbots can't follow symlink
+hostDockerInternal=$($(dirname $0)/../cmd/ddev/cmd/scripts/host-docker-internal.sh)
 
 docker volume create --driver local --opt type=nfs --opt o=addr=${hostDockerInternal},hard,nolock,rw --opt device=:${share} nfstest >/dev/null
 docker run -t --rm -v nfstest:/tmp/nfs busybox:stable ls //tmp/nfs >/dev/null
