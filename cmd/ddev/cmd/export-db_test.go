0302b9974e833cb0a313bc0c8be1c8fe66f70837
Randy Fay <randy@randyfay.com>
2022-03-20 09:13:37 -0600

    Add compression type coverage to TestCmdExportDB

--- cmd/ddev/cmd/export-db_test.go
+++ cmd/ddev/cmd/export-db_test.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/drud/ddev/pkg/archive"
 	"github.com/drud/ddev/pkg/ddevapp"
+	exec2 "github.com/drud/ddev/pkg/exec"
 	"github.com/drud/ddev/pkg/fileutil"
 	"github.com/drud/ddev/pkg/testcommon"
 	asrt "github.com/stretchr/testify/assert"
@@ -26,12 +28,12 @@ func TestCmdExportDB(t *testing.T) {
 	assert.NoError(err)
 
 	t.Cleanup(func() {
+		err = os.Chdir(origDir)
+		assert.NoError(err)
 		// Make sure all databases are back to default empty
 		err = app.Stop(true, false)
 		assert.NoError(err)
 		err = app.Start()
-		assert.NoError(err)
-		err = os.Chdir(origDir)
 		assert.NoError(err)
 	})
 
@@ -51,13 +53,13 @@ func TestCmdExportDB(t *testing.T) {
 
 	// Export the database and verify content of output
 	_ = os.MkdirAll(filepath.Join("tmp", t.Name()), 0755)
-	outputFileName := filepath.Join(site.Dir, "tmp", t.Name(), "output.sql")
-	_ = os.RemoveAll(outputFileName)
-	command = exec.Command(DdevBin, "export-db", "-f="+outputFileName, "--gzip=false")
+	outputFile := filepath.Join(site.Dir, "tmp", t.Name(), "output.sql")
+	_ = os.RemoveAll(outputFile)
+	command = exec.Command(DdevBin, "export-db", "-f="+outputFile, "--gzip=false")
 	byteout, err := command.CombinedOutput()
 	require.NoError(t, err, "byteout=%s", string(byteout))
-	assert.FileExists(outputFileName)
-	assert.True(fileutil.FgrepStringInFile(outputFileName, "13751eca-19cf-41c2-90d4-9363f3a07c45"))
+	assert.FileExists(outputFile)
+	assert.True(fileutil.FgrepStringInFile(outputFile, "13751eca-19cf-41c2-90d4-9363f3a07c45"))
 
 	// Test with named project (outside project directory)
 	tmpDir := testcommon.CreateTmpDir("TestCmdExportDB")
@@ -66,14 +68,46 @@ func TestCmdExportDB(t *testing.T) {
 
 	err = app.MutagenSyncFlush()
 	assert.NoError(err)
-	err = os.RemoveAll(outputFileName)
+	err = os.RemoveAll(outputFile)
 	assert.NoError(err)
-	command = exec.Command(DdevBin, "export-db", site.Name, "-f="+outputFileName, "--gzip=false")
+	command = exec.Command(DdevBin, "export-db", site.Name, "-f="+outputFile, "--gzip=false")
 	byteout, err = command.CombinedOutput()
 	assert.NoError(err, "export-db failure output=%s", string(byteout))
-	assert.FileExists(outputFileName)
-	assert.True(fileutil.FgrepStringInFile(outputFileName, "13751eca-19cf-41c2-90d4-9363f3a07c45"))
+	assert.FileExists(outputFile)
+	assert.True(fileutil.FgrepStringInFile(outputFile, "13751eca-19cf-41c2-90d4-9363f3a07c45"))
 
+	// Export with various types of compression
+	cTypes := map[string]string{
+		"gzip":  "gz",
+		"bzip2": "bz2",
+		"xz":    "xz",
+	}
+	outDir := filepath.Join(site.Dir, "tmp")
+	err = os.MkdirAll(outDir, 0755)
+	assert.NoError(err)
+	err = os.Chdir(app.AppRoot)
+	require.NoError(t, err)
+	for cType, ext := range cTypes {
+		outputFile := filepath.Join(outDir, "db.sql."+ext)
+		out, err := exec2.RunHostCommand(DdevBin, "export-db", "-f="+outputFile, "--"+cType)
+		require.NoError(t, err, "export-db output failure for %s, output=%s", outputFile, out)
+		require.FileExists(t, outputFile)
+		uncompressedFile := filepath.Join(outDir, "db.sql")
+		_ = os.RemoveAll(uncompressedFile)
+
+		switch cType {
+		case "gzip":
+			err = archive.Ungzip(outputFile, outDir)
+			assert.NoError(err)
+		case "bzip2":
+			err = archive.UnBzip2(outputFile, outDir)
+			assert.NoError(err)
+		case "xz":
+			err = archive.UnXz(outputFile, outDir)
+			assert.NoError(err)
+		}
+		assert.True(fileutil.FgrepStringInFile(uncompressedFile, "13751eca-19cf-41c2-90d4-9363f3a07c45"))
+	}
 	// Work with a non-default database named "nondefault"
 	// Read in a database
 	inputFileName = filepath.Join(origDir, "testdata", t.Name(), "nondefault.sql")
@@ -88,13 +122,13 @@ func TestCmdExportDB(t *testing.T) {
 	})
 	assert.NoError(err)
 
-	outputFileName = filepath.Join(tmpDir, "nondefault_output.sql")
-	command = exec.Command(DdevBin, "export-db", site.Name, "-d=nondefault", "-f="+outputFileName, "--gzip=false")
+	outputFile = filepath.Join(tmpDir, "nondefault_output.sql")
+	command = exec.Command(DdevBin, "export-db", site.Name, "-d=nondefault", "-f="+outputFile, "--gzip=false")
 	byteout, err = command.CombinedOutput()
 	assert.NoError(err, "export-db failure output=%s", string(byteout))
-	assert.Contains(string(byteout), fmt.Sprintf("Wrote database dump from project '%s' database '%s' to file %s in plain text format", site.Name, "nondefault", outputFileName))
-	assert.FileExists(outputFileName)
-	assert.True(fileutil.FgrepStringInFile(outputFileName, "INSERT INTO `nondefault_table` VALUES (0,'13751eca-19cf-41c2-90d4-9363f3a07c45','en'),"))
+	assert.Contains(string(byteout), fmt.Sprintf("Wrote database dump from project '%s' database '%s' to file %s in plain text format", site.Name, "nondefault", outputFile))
+	assert.FileExists(outputFile)
+	assert.True(fileutil.FgrepStringInFile(outputFile, "INSERT INTO `nondefault_table` VALUES (0,'13751eca-19cf-41c2-90d4-9363f3a07c45','en'),"))
 
 	_, _, err = app.Exec(&ddevapp.ExecOpts{
 		Service: "db",
