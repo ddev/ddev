bf8522b8916974f9930d3fcb4972a4de280a4743
Randy Fay <randy@randyfay.com>
2022-05-05 21:32:13 -0600

    Add ddev debug refresh command to solve composer issues (#3808)

---
+++ cmd/ddev/cmd/debug-refresh.go
@@ -0,0 +1,58 @@
+package cmd
+
+import (
+	"github.com/drud/ddev/pkg/ddevapp"
+	"github.com/drud/ddev/pkg/dockerutil"
+	"github.com/drud/ddev/pkg/util"
+	"github.com/spf13/cobra"
+)
+
+// DebugRefreshCmd implements the ddev debug refresh command
+var DebugRefreshCmd = &cobra.Command{
+	Use:   "refresh",
+	Short: "Refreshes docker cache for project",
+	Run: func(cmd *cobra.Command, args []string) {
+		projectName := ""
+
+		if len(args) > 1 {
+			util.Failed("This command only takes one optional argument: project name")
+		}
+
+		if len(args) == 1 {
+			projectName = args[0]
+		}
+
+		app, err := ddevapp.GetActiveApp(projectName)
+		if err != nil {
+			util.Failed("Failed to get project: %v", err)
+		}
+
+		if app.SiteStatus() != ddevapp.SiteRunning {
+			if err = app.Start(); err != nil {
+				util.Failed("Failed to start %s: %v", app.Name, err)
+			}
+		}
+
+		app.DockerEnv()
+		if err = app.WriteDockerComposeYAML(); err != nil {
+			util.Failed("Failed to get compose-config: %v", err)
+		}
+
+		util.Debug("Executing docker-compose -f %s build --no-cache", app.DockerComposeFullRenderedYAMLPath())
+		_, _, err = dockerutil.ComposeCmd([]string{app.DockerComposeFullRenderedYAMLPath()}, "build", "--no-cache")
+
+		if err != nil {
+			util.Failed("Failed to execute docker-compose -f %s build --no-cache: %v", err)
+		}
+		err = app.Restart()
+		if err != nil {
+			util.Failed("Failed to restart project: %v", err)
+		}
+
+		util.Success("Refreshed docker cache for project %s", app.Name)
+	},
+}
+
+func init() {
+	DebugCmd.AddCommand(DebugRefreshCmd)
+}
---
+++ cmd/ddev/cmd/debug-refresh_test.go
@@ -0,0 +1,79 @@
+package cmd
+
+import (
+	"github.com/drud/ddev/pkg/ddevapp"
+	"github.com/drud/ddev/pkg/fileutil"
+	asrt "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"os"
+	"testing"
+
+	"github.com/drud/ddev/pkg/exec"
+	"github.com/drud/ddev/pkg/testcommon"
+)
+
+// TestDebugRefreshCmd tests that ddev debug refresh actually clears docker caache
+func TestDebugRefreshCmd(t *testing.T) {
+	assert := asrt.New(t)
+
+	// Create a temporary directory and switch to it.
+	origDir, _ := os.Getwd()
+	tmpdir := testcommon.CreateTmpDir(t.Name())
+
+	err := os.Chdir(tmpdir)
+	assert.NoError(err)
+	_, err = exec.RunHostCommand(DdevBin, "config", "--auto")
+	assert.NoError(err)
+
+	app, err := ddevapp.GetActiveApp("")
+	assert.NoError(err)
+
+	t.Cleanup(func() {
+		_ = os.Chdir(origDir)
+		err = app.Stop(true, false)
+		assert.NoError(err)
+		err := os.RemoveAll(tmpdir)
+		assert.NoError(err)
+	})
+
+	err = fileutil.AppendStringToFile(app.GetConfigPath("web-build/Dockerfile"), `
+ARG BASE_IMAGE
+FROM $BASE_IMAGE
+RUN shuf -i 0-99999 -n1 > /random.txt
+`)
+	require.NoError(t, err)
+
+	// This is normally done in root's init() - probably shouldn't be.
+	err = ddevapp.PopulateExamplesCommandsHomeadditions("")
+	require.NoError(t, err)
+
+	err = app.Start()
+	require.NoError(t, err)
+
+	origRandom, _, err := app.Exec(&ddevapp.ExecOpts{
+		Cmd: "cat /random.txt",
+	})
+	require.NoError(t, err)
+
+	// Make sure that in the ordinary case, the original cache/Dockerfile is same
+	err = app.Restart()
+	require.NoError(t, err)
+	newRandom, _, err := app.Exec(&ddevapp.ExecOpts{
+		Cmd: "cat /random.txt",
+	})
+	require.NoError(t, err)
+	assert.Equal(origRandom, newRandom)
+
+	// Now run ddev debug refresh to blow away the docker cache
+	_, err = exec.RunHostCommand(DdevBin, "debug", "refresh")
+	require.NoError(t, err)
+
+	// Now with refresh having been done, we should see a new value for random
+	err = app.Restart()
+	require.NoError(t, err)
+	freshRandom, _, err := app.Exec(&ddevapp.ExecOpts{
+		Cmd: "cat /random.txt",
+	})
+	require.NoError(t, err)
+	assert.NotEqual(origRandom, freshRandom)
+}
