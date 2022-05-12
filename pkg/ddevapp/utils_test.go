c9efba2b1ff87425e4870c92354e79be5882677b
Randy Fay <randy@randyfay.com>
2022-05-11 20:47:30 -0600

    Make sure at least one project exists

--- pkg/ddevapp/utils_test.go
+++ pkg/ddevapp/utils_test.go
@@ -5,15 +5,32 @@ import (
 	"github.com/drud/ddev/pkg/exec"
 	asrt "github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"os"
 	"testing"
 )
 
 func TestGetInactiveProjects(t *testing.T) {
 	assert := asrt.New(t)
 
-	// Make sure we have no running sites.
-	// Pause all sites.
-	_, err := exec.RunCommand(DdevBin, []string{"pause", "--all"})
+	origDir, _ := os.Getwd()
+	app := &ddevapp.DdevApp{}
+
+	site := TestSites[0]
+
+	t.Cleanup(func() {
+		err := os.Chdir(origDir)
+		assert.NoError(err)
+		err = app.Stop(true, false)
+		assert.NoError(err)
+	})
+	// Make sure we have one app in existence
+	err := app.Init(site.Dir)
+	require.NoError(t, err)
+	err = app.Start()
+	require.NoError(t, err)
+
+	// Stop all sites
+	_, err = exec.RunCommand(DdevBin, []string{"stop", "--all"})
 	require.NoError(t, err)
 
 	apps, err := ddevapp.GetInactiveProjects()
