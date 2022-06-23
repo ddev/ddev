ef00de8c2d3db3fc347b04fdfe2256039e0688d0
Nico Löber <nico.loeber94@gmail.com>
2022-06-23 03:55:54 +0200

    Add `/opt/phpstorm-coverage` directory to web-container (#3924)

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -17,7 +17,7 @@ var SegmentKey = ""
 var WebImg = "drud/ddev-webserver"
 
 // WebTag defines the default web image tag
-var WebTag = "v1.19.3-1" // Note that this can be overridden by make
+var WebTag = "20220619_add_phpstorm_coverage_directory" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "drud/ddev-dbserver"
