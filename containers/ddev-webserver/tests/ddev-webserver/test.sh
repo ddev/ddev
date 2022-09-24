286a1973c09cbc01ed6ca55848b924f8149df0ea
Randy Fay <randy@randyfay.com>
2022-09-24 14:38:01 -0600

    export DOCKER_IMAGE so it makes it into test

--- containers/ddev-webserver/tests/ddev-webserver/test.sh
+++ containers/ddev-webserver/tests/ddev-webserver/test.sh
@@ -5,7 +5,7 @@ set -o pipefail
 set -o nounset
 
 if [ $# != "1" ]; then echo "docker image spec must be \$1"; exit 1; fi
-DOCKER_IMAGE=$1
+export DOCKER_IMAGE=$1
 export IS_HARDENED=false
 DOCKER_REPO=${DOCKER_IMAGE%:*}
 if [[ "${DOCKER_REPO}" == "*prod*" ]]; then
