9aee6bd677b0ead95c40586445343ec24924dc03
Randy Fay <randy@randyfay.com>
2022-10-05 19:27:45 -0600

    Bump images to v1.21.2 for upcoming release (#4261)

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -17,13 +17,13 @@ var SegmentKey = ""
 var WebImg = "drud/ddev-webserver"
 
 // WebTag defines the default web image tag
-var WebTag = "20220928_fix_yarnrc_edit_problem" // Note that this can be overridden by make
+var WebTag = "v1.21.2" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "drud/ddev-dbserver"
 
 // BaseDBTag is the main tag, DBTag is constructed from it
-var BaseDBTag = "v1.20.0"
+var BaseDBTag = "v1.21.2"
 
 // DBAImg defines the default phpmyadmin image tag used for applications.
 var DBAImg = "phpmyadmin"
@@ -35,7 +35,7 @@ var DBATag = "5" // Note that this can be overridden by make
 var RouterImage = "drud/ddev-router"
 
 // RouterTag defines the tag used for the router.
-var RouterTag = "v1.20.0" // Note that this can be overridden by make
+var RouterTag = "v1.21.2" // Note that this can be overridden by make
 
 // SSHAuthImage is image for agent
 // var SSHAuthImage = "drud/ddev-ssh-agent"
@@ -43,7 +43,7 @@ var SSHAuthImage = "drud/ddev-ssh-agent"
 
 // SSHAuthTag is ssh-agent auth tag
 // var SSHAuthTag = "v1.19.0"
-var SSHAuthTag = "v1.20.0"
+var SSHAuthTag = "v1.21.2"
 
 // Busybox is used a couple of places for a quick-pull
 var BusyboxImage = "busybox:stable"
