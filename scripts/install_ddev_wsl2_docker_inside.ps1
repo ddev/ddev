30c55b2cc05a765745b882310cef299f8ba8f7c0
Randy Fay <randy@randyfay.com>
2022-11-03 14:17:27 -0600

    Add docker-inside script for wsl2

--- scripts/install_ddev_wsl2_docker_inside.ps1
+++ scripts/install_ddev_wsl2_docker_inside.ps1
@@ -1,9 +1,7 @@
 # This PowerShell script tries to do almost all the things required to set up
-# an Ubuntu WSL2 instance for use with DDEV and Docker Desktop.
+# an Ubuntu WSL2 instance for use with DDEV and docker-ce installed inside WSL2.
 # It requires that an Ubuntu wsl2 distro be installed already, preferably with `wsl --install`, but it can also be
 # done manually.
-# It requires that Docker Desktop is installed and running, and that it has integration enabled with the Ubuntu
-# distro, which is the default behavior.
 # Run this in an administrative PowerShell window.
 # You can download, inspect, and run this, or run it directly with
 # Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
@@ -28,29 +26,35 @@ if (-not(choco 2>&1 >$null)) {
     $ErrorActionPreference = "Stop"
     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
-if (-not(Get-Command docker 2>&1 ) -Or -Not(docker ps ) ) {
-    throw "\n\ndocker does not seem to be installed yet, or Docker Desktop is not running. Please install it or start it. For example, choco install -y docker-desktop"
-}
 
-if (-not(wsl -e docker ps) ) {
-    throw "Docker Desktop integration with the default distro does not seem to be enabled yet."
+if (wsl bash "if [ -d /mnt/wsl/docker-desktop ]; then echo yup; fi" ) {
+    throw "Docker Desktop integration is enabled with the default distro and it must but turned off."
 }
 $ErrorActionPreference = "Stop"
 # Install needed choco items
-choco install -y gsudo mkcert
+choco install -y mkcert
 
 mkcert -install
 setx CAROOT "$(mkcert -CAROOT)"; If ($Env:WSLENV -notlike "*CAROOT/up:*") { setx WSLENV "CAROOT/up:$Env:WSLENV" }
 
+wsl -u root apt-get remove -y docker docker-engine docker.io containerd runc
+wsl -u root apt-get update
+wsl -u root apt-get install -y ca-certificates curl gnupg lsb-release
+wsl -u root mkdir -p /etc/apt/keyrings
+wsl -u root bash -c "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg"
+wsl -u root -e bash -c 'echo \"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null'
+
 wsl -u root -e bash -c "curl -fsSL https://apt.fury.io/drud/gpg.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/ddev.gpg > /dev/null"
 wsl -u root -e bash -c 'echo \"deb [signed-by=/etc/apt/trusted.gpg.d/ddev.gpg] https://apt.fury.io/drud/ * *\" > /etc/apt/sources.list.d/ddev.list'
-wsl -u root -e bash -c "sudo apt update && sudo apt install -y ddev"
+wsl -u root -e bash -c "sudo apt update && sudo apt install -y ddev docker-ce docker-ce-cli containerd.io"
 wsl -u root -e bash -c "apt-get upgrade -y >/dev/null"
+wsl bash -c 'sudo usermod -aG docker $USER'
 
-
-wsl bash -c 'echo $CAROOT'
+wsl bash -c 'echo CAROOT=$CAROOT'
 wsl -u root mkcert -install
+wsl -u root service docker start
 if (-not(wsl -e docker ps)) {
-    throw "docker does not seem to be working inside the WSL2 distro yet. Check Resources->WSL Integration in Docker Desktop"
+    throw "docker does not seem to be working inside the WSL2 distro yet. "
 }
+
 wsl ddev version
