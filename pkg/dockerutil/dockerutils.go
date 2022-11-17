238c01a3972e1d523efb6c19d877e0fb6a992ee9
Randy Fay <randy@randyfay.com>
2022-11-17 13:37:25 -0700

    Don't warn about Docker Desktop for Linux on WSL2

--- pkg/dockerutil/dockerutils.go
+++ pkg/dockerutil/dockerutils.go
@@ -627,7 +627,7 @@ func CheckDockerVersion(versionConstraint string) error {
 	}
 
 	// See if they're using broken docker desktop on linux
-	if runtime.GOOS == "linux" {
+	if runtime.GOOS == "linux" && !IsWSL2() {
 		client := GetDockerClient()
 		info, err := client.Info()
 		if err != nil {
