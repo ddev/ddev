1759825b8a5ae0ebd65464a5382168d9d6f2456a
Randy Fay <randy@randyfay.com>
2022-12-23 10:36:51 -0700

    Minor bump to github tests and release in support of codespaces [skip ci]

--- .github/devcontainers/src/install-ddev/install.sh
+++ .github/devcontainers/src/install-ddev/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu -o pipefail
 
-sudo apt update && sudo apt install -y curl wget xdg-utils
+sudo apt update >/dev/null && sudo apt install -y xdg-utils >/dev/null
 
 # This will eventually move to a simple apt install
 sudo apt remove ddev >/dev/null 2>&1 || true
@@ -12,4 +12,11 @@ cd /tmp && curl -s -L -O ${DDEV_URL}
 zipball=$(basename ${DDEV_URL})
 unzip ${zipball}
 chmod +x ddev && sudo mv ddev /usr/local/bin
-rm -f ${zipball}
+
+# When we have a better way to preloaod images, update this
+mkdir /tmp/tempproject && cd /tmp/tempproject
+ddev config --auto
+ddev config global --omit-containers=ddev-router
+ddev debug download-images
+
+rm -rf ${zipball} /tmp/tempproject
