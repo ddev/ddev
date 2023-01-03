60db8d014932d07e6ac540b9f829365f6d36fc7e
Randy Fay <randy@randyfay.com>
2023-01-03 13:35:53 -0700

    Try to figure out where it actually fails

--- .github/workflows/macos-colima-setup.sh
+++ .github/workflows/macos-colima-setup.sh
@@ -10,15 +10,21 @@ brew uninstall go@1.15 || true
 brew unlink go || true
 brew uninstall go@1.17 || true
 brew uninstall postgresql || true
+echo "====== Running brew install ======"
 brew install colima docker docker-compose go jq libpq mkcert mysql-client
+echo "====== Running brew link ======"
 brew link --force go libpq mysql-client
+echo "====== Completed brew link ======"
+
 
 # This command allows adding CA (in mkcert, etc) without the popup trust prompt
 # Mentioned in https://github.com/actions/virtual-environments/issues/4519#issuecomment-970202641
+echo "====== Setting trust settings ======"
 sudo security authorizationdb write com.apple.trust-settings.admin allow
 
 # Github actions macOS runners have 14BG RAM so might as well use it.
 # https://docs.github.com/en/actions/using-github-hosted-runners/about-github-hosted-runners#supported-runners-and-hardware-resources
+echo "====== Starting colima ======"
 colima start --cpu 3 --memory 6
 
 # I haven't been able to get mkcert-trusted certs in there, not sure why
