e6c16eeb8d2407d6c636f97ea1fe138e94396d9c
Randy Fay <randy@randyfay.com>
2023-01-26 15:27:49 -0700

    Less output in pre-seed status

--- containers/ddev-webserver/ddev-webserver-base-scripts/start.sh
+++ containers/ddev-webserver/ddev-webserver-base-scripts/start.sh
@@ -18,7 +18,7 @@ if [ "${DDEV_MUTAGEN_ENABLED}" = "true" ] && [ ! -f /var/www/html/.ddev/mutagen/
     RSYNC_CMD="${RSYNC_CMD} --exclude ${DDEV_FILES_DIR#/var/www/html} --exclude=.git --exclude=.tarballs --exclude=.idea"
   fi
   echo "Seeding /var/www/html with contents of project directory as quickstart for mutagen"
-  echo "Pre-seed status=$(ls -laR /var/www/html)"
+  echo "Pre-seed status=$(ls -lR /var/www/html)"
   time ${RSYNC_CMD} || true
 fi
 
