2b9dfeb4b5e0803da2d8b781b3aee694530fe1a3
Randy Fay <randy@randyfay.com>
2023-02-11 21:36:21 -0700

    Bump docker-compose to 2.16.0, which is built with go 1.20, fixing DNS issues on macOS (#4641)

--- pkg/globalconfig/global_config.go
+++ pkg/globalconfig/global_config.go
@@ -609,7 +609,7 @@ var DockerComposeVersion = ""
 
 // This is var instead of const so it can be changed in test, but should not otherwise be touched.
 // Otherwise we can't test if the version on the machine is equal to version required
-var RequiredDockerComposeVersion = "v2.15.1"
+var RequiredDockerComposeVersion = "v2.16.0"
 
 // GetRequiredDockerComposeVersion returns the version of docker-compose we need
 // based on the compiled version, or overrides in globalconfig, like
