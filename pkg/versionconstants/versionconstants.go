3e1d1a2ff83b8c6a595cf6932b76c21e2d04cdfd
Randy Fay <randy@randyfay.com>
2023-02-23 11:21:27 -0700

    Make hardened images properly handle web-entrypoint.d, fixes #4631

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -17,7 +17,7 @@ var SegmentKey = ""
 var WebImg = "drud/ddev-webserver"
 
 // WebTag defines the default web image tag
-var WebTag = "20230207_fix_nvm" // Note that this can be overridden by make
+var WebTag = "20230223_hardened_web_entrypoint.d" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "drud/ddev-dbserver"
