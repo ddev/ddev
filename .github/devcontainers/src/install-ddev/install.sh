f41d67aa5c27743735632caa511b178726be17f3
Randy Fay <randy@randyfay.com>
2023-03-03 17:57:47 -0700

    Switch devcontainer/github codespaces to use apt install (#4612) [skip ci]

--- .github/devcontainers/src/install-ddev/install.sh
+++ .github/devcontainers/src/install-ddev/install.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 set -eu -o pipefail
 
-sudo apt update >/dev/null && sudo apt install -y xdg-utils >/dev/null
+curl -fsSL https://apt.fury.io/drud/gpg.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/ddev.gpg > /dev/null
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/ddev.gpg] https://apt.fury.io/drud/ * *" | sudo tee /etc/apt/sources.list.d/ddev.list
+sudo apt update >/dev/null && sudo apt install -y ddev xdg-utils >/dev/null
 
-# This will eventually move to a simple apt install
-sudo apt remove ddev >/dev/null 2>&1 || true
-DDEV_URL=https://nightly.link/drud/ddev/workflows/master-build/master/ddev-linux-amd64.zip
-echo "Installing DDEV"
-
-cd /tmp && curl -s -L -O ${DDEV_URL}
-zipball=$(basename ${DDEV_URL})
-unzip ${zipball}
-chmod +x ddev && sudo mv ddev /usr/local/bin
-
-rm -rf ${zipball} /tmp/tempproject
