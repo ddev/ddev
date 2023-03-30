67ad766f708861635d8c4493d52dce436385eed1
Randy Fay <randy@randyfay.com>
2023-03-30 12:26:07 -0600

    Add python setup to start.sh [skip ci]

--- containers/ddev-webserver/ddev-webserver-base-scripts/functions.sh
+++ containers/ddev-webserver/ddev-webserver-base-scripts/functions.sh
@@ -11,13 +11,15 @@ function ddev_custom_init_scripts {
 
 # Set up things that gunicorn and project may need, venv
 function ddev_python_setup {
-  python -m venv /var/www/html/.ddev/.venv
-  source /var/www/html/.ddev/.venv/bin/activate
-  pip install psycopg2 gunicorn
-  if [ -f requirements.txt ]; then
-    pip install -r requirements.txt
-  fi
-  if [ -f pyproject.toml ]; then
-    pip install .
+  if [ "${DDEV_PROJECT_TYPE}" = "python" ] || [ "${DDEV_PROJECT_TYPE}" = "django4" ]; then
+    python -m venv /var/www/html/.ddev/.venv
+    source /var/www/html/.ddev/.venv/bin/activate
+    pip install psycopg2 gunicorn
+    if [ -f /var/www/html/requirements.txt ]; then
+      pip install -r /var/www/html/requirements.txt
+    fi
+    if [ -f /var/www/html/pyproject.toml ]; then
+      pip install /var/www/html
+    fi
   fi
 }
