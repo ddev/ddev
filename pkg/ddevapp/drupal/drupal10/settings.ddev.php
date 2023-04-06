a4cdf6cd402b4430f74a95bd713f995360bced8e
Rob Loach <robloach@gmail.com>
2023-04-06 13:26:38 -0400

    Drupal: Enable verbose error logging (#4689) [skip ci]

--- pkg/ddevapp/drupal/drupal10/settings.ddev.php
+++ pkg/ddevapp/drupal/drupal10/settings.ddev.php
@@ -52,3 +52,7 @@ $config['symfony_mailer.mailer_transport.sendmail']['configuration']['user']='';
 $config['symfony_mailer.mailer_transport.sendmail']['configuration']['pass']='';
 $config['symfony_mailer.mailer_transport.sendmail']['configuration']['host']='localhost';
 $config['symfony_mailer.mailer_transport.sendmail']['configuration']['port']='1025';
+
+// Enable verbose logging for errors.
+// https://www.drupal.org/forum/support/post-installation/2018-07-18/enable-drupal-8-backend-errorlogdebugging-mode
+$config['system.logging']['error_level'] = 'verbose';
