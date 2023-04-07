ee92f66caad9354686557b91dea33bba94889e51
Christian Münch <christian@muench-worms.de>
2023-04-07 11:25:46 +0200

    Make listCommandSettings private

--- cmd/ddev/cmd/list.go
+++ cmd/ddev/cmd/list.go
@@ -5,7 +5,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var ListCommandSettings = ddevapp.ListCommandSettings{}
+var listCommandSettings = ddevapp.ListCommandSettings{}
 
 // ListCmd represents the list command
 var ListCmd = &cobra.Command{
@@ -17,16 +17,16 @@ var ListCmd = &cobra.Command{
 ddev list --active-only
 ddev list -A`,
 	Run: func(cmd *cobra.Command, args []string) {
-		ddevapp.List(ListCommandSettings)
+		ddevapp.List(listCommandSettings)
 	},
 }
 
 func init() {
-	ListCmd.Flags().BoolVarP(&ListCommandSettings.ActiveOnly, "active-only", "A", false, "If set, only currently active projects will be displayed.")
-	ListCmd.Flags().BoolVarP(&ListCommandSettings.Continuous, "continuous", "", false, "If set, project information will be emitted until the command is stopped.")
-	ListCmd.Flags().BoolVarP(&ListCommandSettings.WrapTableText, "wrap-table", "W", false, "Display table with wrapped text if required.")
-	ListCmd.Flags().StringVarP(&ListCommandSettings.TypeFilter, "type", "t", "", "Show only projects of this type")
-	ListCmd.Flags().IntVarP(&ListCommandSettings.ContinuousSleepTime, "continuous-sleep-interval", "I", 1, "Time in seconds between ddev list --continuous output lists.")
+	ListCmd.Flags().BoolVarP(&listCommandSettings.ActiveOnly, "active-only", "A", false, "If set, only currently active projects will be displayed.")
+	ListCmd.Flags().BoolVarP(&listCommandSettings.Continuous, "continuous", "", false, "If set, project information will be emitted until the command is stopped.")
+	ListCmd.Flags().BoolVarP(&listCommandSettings.WrapTableText, "wrap-table", "W", false, "Display table with wrapped text if required.")
+	ListCmd.Flags().StringVarP(&listCommandSettings.TypeFilter, "type", "t", "", "Show only projects of this type")
+	ListCmd.Flags().IntVarP(&listCommandSettings.ContinuousSleepTime, "continuous-sleep-interval", "I", 1, "Time in seconds between ddev list --continuous output lists.")
 
 	RootCmd.AddCommand(ListCmd)
 }
