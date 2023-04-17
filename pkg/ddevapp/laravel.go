84d8fda7dae8a5593e065f79469dea2a8e900842
tyler36 <7234392+tyler36@users.noreply.github.com>
2023-04-17 01:10:50 +0000

    add mailhog port

--- pkg/ddevapp/laravel.go
+++ pkg/ddevapp/laravel.go
@@ -57,6 +57,7 @@ func laravelPostStartAction(app *DdevApp) error {
 		"DB_PASSWORD":   "db",
 		"DB_CONNECTION": dbConnection,
 		"MAIL_HOST":     "127.0.0.1",
+		"MAIL_PORT":     "1025",
 	}
 	err = WriteProjectEnvFile(envFilePath, envMap, envText)
 	if err != nil {
