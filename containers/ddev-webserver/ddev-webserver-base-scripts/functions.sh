2888067f232113e4be7e18e17633a204eef23bd3
Randy Fay <randy@randyfay.com>
2023-04-22 18:58:38 -0600

    Go back to simpler approach with DDEV_WEB_ENTRYPOINT [skip ci]

--- containers/ddev-webserver/ddev-webserver-base-scripts/functions.sh
+++ containers/ddev-webserver/ddev-webserver-base-scripts/functions.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 function ddev_custom_init_scripts {
-  echo "Loading custom entrypoint config from ${ENTRYPOINT}";
-  if ls ${ENTRYPOINT}/*.sh >/dev/null 2>&1; then
-    for f in ${ENTRYPOINT}/*.sh; do
-      echo "sourcing web-entrypoint.d/$f"
-      . "$f"
+  echo "Loading custom entrypoint config from ${DDEV_WEB_ENTRYPOINT}";
+  if ls ${DDEV_WEB_ENTRYPOINT}/*.sh >/dev/null 2>&1; then
+    for f in ${DDEV_WEB_ENTRYPOINT}/*.sh; do
+      echo "sourcing $f"
+      source "$f"
     done
   fi
-#      touch "${ENTRYPOINT}/.user_scripts_initialized"
+#      touch "${DDEV_WEB_ENTRYPOINT}/.user_scripts_initialized"
 }
 
 # Set up things that gunicorn and project may need, venv
