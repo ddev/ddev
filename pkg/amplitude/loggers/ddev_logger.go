5865c36b85f64ef294a13102890aaeaa6dc37b1f
Simon Gilli <25326036+gilbertsoft@users.noreply.github.com>
2023-06-14 11:01:42 +0200

    Do not show API key

--- pkg/amplitude/loggers/ddev_logger.go
+++ pkg/amplitude/loggers/ddev_logger.go
@@ -1,6 +1,9 @@
 package loggers
 
 import (
+	"fmt"
+	"regexp"
+
 	"github.com/amplitude/analytics-go/amplitude/types"
 	"github.com/ddev/ddev/pkg/util"
 )
@@ -13,17 +16,24 @@ type ddevLogger struct {
 }
 
 func (l *ddevLogger) Debugf(message string, args ...interface{}) {
-	util.Debug(message, args...)
+	util.Debug(filterMessage(message, args...))
 }
 
 func (l *ddevLogger) Infof(message string, args ...interface{}) {
-	util.Debug(message, args...)
+	util.Debug(filterMessage(message, args...))
 }
 
 func (l *ddevLogger) Warnf(message string, args ...interface{}) {
-	util.Debug(message, args...)
+	util.Debug(filterMessage(message, args...))
 }
 
 func (l *ddevLogger) Errorf(message string, args ...interface{}) {
-	util.Debug(message, args...)
+	util.Debug(filterMessage(message, args...))
+}
+
+// filterMessage removes sensitive data from the message like the API key.
+func filterMessage(message string, args ...interface{}) string {
+	re := regexp.MustCompile(`(?m)"api_key"\s*:\s*"[^"]*"`)
+
+	return re.ReplaceAllString(fmt.Sprintf(message, args...), `"api_key":"***"`)
 }
