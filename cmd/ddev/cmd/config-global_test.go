81cd3f2eeaf9672c8d0f0d16d3019e328310d6d3
Randy Fay <randy@randyfay.com>
2023-06-15 15:13:05 -0600

    Add router= testing in TestCmdGlobalConfig

--- cmd/ddev/cmd/config-global_test.go
+++ cmd/ddev/cmd/config-global_test.go
@@ -29,7 +29,7 @@ func TestCmdGlobalConfig(t *testing.T) {
 	// nolint: errcheck
 	t.Cleanup(func() {
 		// Even though the global config is going to be deleted, make sure it's sane before leaving
-		args := []string{"config", "global", "--omit-containers", "", "--nfs-mount-enabled", "--disable-http2=false", "--mutagen-enabled=false", "--simple-formatting=false", "--table-style=default", `--required-docker-compose-version=""`, `--use-docker-compose-from-path=false`, `xdebug-ide-location=""`}
+		args := []string{"config", "global", "--omit-containers", "", "--nfs-mount-enabled", "--disable-http2=false", "--mutagen-enabled=false", "--simple-formatting=false", "--table-style=default", `--required-docker-compose-version=""`, `--use-docker-compose-from-path=false`, `--xdebug-ide-location`, "", `--router=traefik`}
 		globalconfig.DdevGlobalConfig.OmitContainersGlobal = nil
 		out, err := exec.RunHostCommand(DdevBin, args...)
 		assert.NoError(err, "error running ddev config global; output=%s", out)
@@ -51,17 +51,17 @@ func TestCmdGlobalConfig(t *testing.T) {
 	out, err := exec.RunCommand(DdevBin, args)
 	assert.NoError(err, "error running ddev config global; output=%s", out)
 	assert.Contains(string(out), "Global configuration:\ninstrumentation-opt-in=false\nomit-containers=[]\nweb-environment=[]\nmutagen-enabled=false\nnfs-mount-enabled=false\nrouter-bind-all-interfaces=false\ninternet-detection-timeout-ms=3000\ndisable-http2=false\nuse-letsencrypt=false\nletsencrypt-email=\ntable-style=default\nsimple-formatting=false\nauto-restart-containers=false\nuse-hardened-images=false\nfail-on-hook-fail=false\nrequired-docker-compose-version=\nuse-docker-compose-from-path=false\nproject-tld=\nxdebug-ide-location=")
-	assert.Contains(string(out), "wsl2-no-windows-hosts-mgt=false\nrouter-http-port=80\nrouter-https-port=443")
+	assert.Contains(string(out), "router=traefik\nwsl2-no-windows-hosts-mgt=false\nrouter-http-port=80\nrouter-https-port=443")
 
 	// Update a config
 	// Don't include no-bind-mounts because global testing
 	// will turn it on and break this
-	args = []string{"config", "global", "--project-tld=ddev.test", "--instrumentation-opt-in=false", "--omit-containers=dba,ddev-ssh-agent", "--mutagen-enabled=true", "--nfs-mount-enabled=true", "--router-bind-all-interfaces=true", "--internet-detection-timeout-ms=850", "--table-style=bright", "--simple-formatting=true", "--auto-restart-containers=true", "--use-hardened-images=true", "--fail-on-hook-fail=true", `--web-environment="SOMEENV=some+val"`, `--xdebug-ide-location=container`, `--router-http-port=8081`, `--router-https-port=8882`}
+	args = []string{"config", "global", "--project-tld=ddev.test", "--instrumentation-opt-in=false", "--omit-containers=dba,ddev-ssh-agent", "--mutagen-enabled=true", "--nfs-mount-enabled=true", "--router-bind-all-interfaces=true", "--internet-detection-timeout-ms=850", "--table-style=bright", "--simple-formatting=true", "--auto-restart-containers=true", "--use-hardened-images=true", "--fail-on-hook-fail=true", `--web-environment="SOMEENV=some+val"`, `--xdebug-ide-location=container`, `--router=traditional`, `--router-http-port=8081`, `--router-https-port=8882`}
 	out, err = exec.RunCommand(DdevBin, args)
 	assert.NoError(err, "error running ddev config global; output=%s", out)
 	assert.Contains(string(out), "Global configuration:\ninstrumentation-opt-in=false\nomit-containers=[dba,ddev-ssh-agent]\nweb-environment=[\"SOMEENV=some+val\"]\nmutagen-enabled=true\nnfs-mount-enabled=true\nrouter-bind-all-interfaces=true\ninternet-detection-timeout-ms=850\ndisable-http2=false\nuse-letsencrypt=false\nletsencrypt-email=\ntable-style=bright\nsimple-formatting=true\nauto-restart-containers=true\nuse-hardened-images=true\nfail-on-hook-fail=true\nrequired-docker-compose-version=\nuse-docker-compose-from-path=false")
 	assert.Contains(string(out), "xdebug-ide-location=container")
-	assert.Contains(string(out), "router-http-port=8081\nrouter-https-port=8882")
+	assert.Contains(string(out), "router=traditional\nwsl2-no-windows-hosts-mgt=false\nrouter-http-port=8081\nrouter-https-port=8882")
 
 	err = globalconfig.ReadGlobalConfig()
 	assert.NoError(err)
@@ -77,6 +77,9 @@ func TestCmdGlobalConfig(t *testing.T) {
 	assert.True(globalconfig.DdevGlobalConfig.SimpleFormatting)
 	assert.Equal("bright", globalconfig.DdevGlobalConfig.TableStyle)
 	assert.Equal("container", globalconfig.DdevGlobalConfig.XdebugIDELocation)
+	assert.Equal("traditional", globalconfig.DdevGlobalConfig.Router)
+	assert.Equal("8081", globalconfig.DdevGlobalConfig.RouterHTTPPort)
+	assert.Equal("8882", globalconfig.DdevGlobalConfig.RouterHTTPSPort)
 
 	// Test that variables can be appended to the web environment
 	args = []string{"config", "global", `--web-environment-add="FOO=bar"`}
