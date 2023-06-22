8cc104c53baa17af657159e911939b8d637d10e3
Randy Fay <randy@randyfay.com>
2023-06-22 17:40:19 -0600

    Specify the default docker-compose version the new way

--- pkg/globalconfig/values.go
+++ pkg/globalconfig/values.go
@@ -15,6 +15,7 @@ const (
 )
 
 const DdevGithubOrg = "ddev"
+const RequiredDockerComposeVersionDefault = "v2.20.0"
 
 // ValidOmitContainers is the valid omit's that can be done in for a project
 var ValidOmitContainers = map[string]bool{
