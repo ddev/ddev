dae44ba4a0c79c0307fc683e0bf85495a684533a
Randy Fay <randy@randyfay.com>
2023-06-29 17:01:36 -0600

    build: bump mutagen to 0.17.2

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -48,7 +48,7 @@ var BUILDINFO = "BUILDINFO should have new info"
 // MutagenVersion is filled with the version we find for mutagen in use
 var MutagenVersion = ""
 
-const RequiredMutagenVersion = "0.17.1"
+const RequiredMutagenVersion = "0.17.2"
 
 // GetWebImage returns the correctly formatted web image:tag reference
 func GetWebImage() string {
