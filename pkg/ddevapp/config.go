df478a73140e876b7ceff17d532eeb3cce365b9f
Gilbertsoft <25326036+gilbertsoft@users.noreply.github.com>
2023-07-12 00:00:45 +0200

    Update pkg/ddevapp/config.go [skip ci]
    Co-authored-by: Randy Fay <randy@randyfay.com>

--- pkg/ddevapp/config.go
+++ pkg/ddevapp/config.go
@@ -141,7 +141,7 @@ func NewApp(appRoot string, includeOverrides bool) (*DdevApp, error) {
 
 	// Remove dba
 	if nodeps.ArrayContainsString(app.OmitContainers, "dba") || nodeps.ArrayContainsString(app.OmitContainersGlobal, "dba") {
-		util.Warning("PhpMyAdmin (`dba`) no longer part of DDEV core, please edit your `omit_containers` configuration to remove it")
+		util.Warning("PhpMyAdmin (`dba`) is no longer part of DDEV core, please edit your `omit_containers` configuration to remove it")
 		app.OmitContainers = nodeps.RemoveItemFromSlice(app.OmitContainers, "dba")
 		app.OmitContainersGlobal = nodeps.RemoveItemFromSlice(app.OmitContainersGlobal, "dba")
 	}
