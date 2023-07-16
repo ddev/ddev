ac244f2baa8787e91013af0b8a91d96141e5ebb1
Randy Fay <randy@randyfay.com>
2023-07-15 20:08:33 -0600

    refactor: make GetDockerPlatform() give more useful information, fixes #5142 (#5145)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -60,7 +60,22 @@ func GetDockerPlatform() (string, error) {
 	if err != nil {
 		return "", err
 	}
-	platform := info.Name
+
+	platform := info.OperatingSystem
+	switch {
+	case nodeps.IsWSL2() && info.OSType == "linux":
+		platform = "wsl2-docker-ce"
+	case strings.HasPrefix(platform, "Rancher Desktop"):
+		platform = "rancher-desktop"
+	case strings.HasPrefix(platform, "Docker Desktop"):
+		platform = "docker-desktop"
+	case info.Name == "colima":
+		platform = "colima"
+	case platform == "OrbStack":
+		platform = "orbstack"
+	default:
+		platform = info.OperatingSystem
+	}
 
 	return platform, nil
 }
