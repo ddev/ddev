729ce471025eb4338437fd7c923cd62d2b36b91d
Randy Fay <randy@randyfay.com>
2023-08-18 14:22:16 -0600

    build: use Debian 12 bookworm as base image for ddev-webserver

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -15,7 +15,7 @@ var AmplitudeAPIKey = ""
 var WebImg = "ddev/ddev-webserver"
 
 // WebTag defines the default web image tag
-var WebTag = "20230922_amazee_lagoon" // Note that this can be overridden by make
+var WebTag = "20230818_bump_debian_bookworm" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "ddev/ddev-dbserver"
