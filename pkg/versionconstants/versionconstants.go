60cf92aaf4cf8525a24ddeb50ea9b2b1ce24f941
Randy Fay <randy@randyfay.com>
2023-09-19 20:33:10 -0700

    build: bump images to v1.22.3 for release (#5357)

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -15,22 +15,22 @@ var AmplitudeAPIKey = ""
 var WebImg = "ddev/ddev-webserver"
 
 // WebTag defines the default web image tag
-var WebTag = "v1.22.2" // Note that this can be overridden by make
+var WebTag = "v1.22.3" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "ddev/ddev-dbserver"
 
 // BaseDBTag is the main tag, DBTag is constructed from it
-var BaseDBTag = "v1.22.2"
+var BaseDBTag = "v1.22.3"
 
-const TraditionalRouterImage = "ddev/ddev-nginx-proxy-router:v1.22.2"
-const TraefikRouterImage = "ddev/ddev-traefik-router:v1.22.2"
+const TraditionalRouterImage = "ddev/ddev-nginx-proxy-router:v1.22.3"
+const TraefikRouterImage = "ddev/ddev-traefik-router:v1.22.3"
 
 // SSHAuthImage is image for agent
 var SSHAuthImage = "ddev/ddev-ssh-agent"
 
 // SSHAuthTag is ssh-agent auth tag
-var SSHAuthTag = "v1.22.2"
+var SSHAuthTag = "v1.22.3"
 
 // Busybox is used a couple of places for a quick-pull
 var BusyboxImage = "busybox:stable"
