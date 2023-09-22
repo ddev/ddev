6fbfdb342684e699d2e44a0126021b8144e0ea0f
Randy Fay <randy@randyfay.com>
2023-09-22 15:13:33 -0600

    feat: implement Amazee Lagoon provider integration, fixes #3188

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -15,7 +15,7 @@ var AmplitudeAPIKey = ""
 var WebImg = "ddev/ddev-webserver"
 
 // WebTag defines the default web image tag
-var WebTag = "20230926_joelpittet_mailpit" // Note that this can be overridden by make
+var WebTag = "20230922_amazee_lagoon" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "ddev/ddev-dbserver"
