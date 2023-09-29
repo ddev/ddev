ba3cee432e34b019489a114575b937e4210fec81
Joel Pittet <pittet@cs.ubc.ca>
2023-09-28 18:43:49 -0700

    fix: remove test@example.org from BCC in mailpit sendmail_path, fixes #5381 (#5383) [skip ci]

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -15,7 +15,7 @@ var AmplitudeAPIKey = ""
 var WebImg = "ddev/ddev-webserver"
 
 // WebTag defines the default web image tag
-var WebTag = "v1.22.3" // Note that this can be overridden by make
+var WebTag = "20230926_joelpittet_mailpit" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "ddev/ddev-dbserver"
