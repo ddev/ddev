11068d8c1c472e1a020a4fa10df2189049a87f73
Randy Fay <randy@randyfay.com>
2023-10-05 18:17:48 +0200

    update webimage tag for new code

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -15,7 +15,7 @@ var AmplitudeAPIKey = ""
 var WebImg = "ddev/ddev-webserver"
 
 // WebTag defines the default web image tag
-var WebTag = "20230922_amazee_lagoon" // Note that this can be overridden by make
+var WebTag = "20231005_amayer5125_node_install" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "ddev/ddev-dbserver"
