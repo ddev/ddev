4744a60bb9689a03d1d6960ce84bcd8f72617437
Randy Fay <randy@randyfay.com>
2023-10-23 20:04:57 -0600

    ci: bump ddev-php-base and ddev-webserver for php8.3.0rc3

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -15,7 +15,7 @@ var AmplitudeAPIKey = ""
 var WebImg = "ddev/ddev-webserver"
 
 // WebTag defines the default web image tag
-var WebTag = "20231010_benwalch_bashrc_cleanup" // Note that this can be overridden by make
+var WebTag = "20231023_bump_php83" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "ddev/ddev-dbserver"
