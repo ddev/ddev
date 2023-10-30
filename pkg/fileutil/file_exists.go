b132d03d0cbc5e842baa087d53140adb690e26c3
sschoeppe <70072031+sschoeppe0@users.noreply.github.com>
2023-10-30 14:53:00 +0100

    feature: allow usage of SHELL variables on

--- pkg/fileutil/file_exists.go
+++ pkg/fileutil/file_exists.go
@@ -3,11 +3,24 @@
 
 package fileutil
 
-import "os"
+import (
+	"os"
+	"regexp"
+)
+
+// Replace SHELL variables with real value
+func replaceShellVariables(input string) string {
+	r := regexp.MustCompile(`\$(\w+)|\$\{(\w+)\}`)
+	return r.ReplaceAllStringFunc(input, func(s string) string {
+		varName := r.ReplaceAllString(s, "$1$2")
+		return os.Getenv(varName)
+	})
+}
 
 // FileExists checks a file's existence
 func FileExists(name string) bool {
-	if _, err := os.Stat(name); err != nil {
+	expandedName := replaceShellVariables(name)
+	if _, err := os.Stat(expandedName); err != nil {
 		if os.IsNotExist(err) {
 			return false
 		}
