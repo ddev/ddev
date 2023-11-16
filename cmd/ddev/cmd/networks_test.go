5b54e8e19255220e4a71f0f12cf8e2026f237fc7
Stanislav Zhuk <stasadev@gmail.com>
2023-11-16 19:40:55 +0200

    Add check for Docker 25

--- cmd/ddev/cmd/networks_test.go
+++ cmd/ddev/cmd/networks_test.go
@@ -11,7 +11,7 @@ import (
 // TestNetworkDuplicates makes sure that Docker network duplicates
 // are deleted successfully with DDEV
 // See https://github.com/ddev/ddev/pull/5508
-// Note: duplicate networks cannot be created with Docker >=25.x.x
+// Note: duplicate networks cannot be created with Docker >= 25.x.x
 // See https://github.com/moby/moby/pull/46251
 func TestNetworkDuplicates(t *testing.T) {
 	assert := asrt.New(t)
@@ -47,9 +47,16 @@ func TestNetworkDuplicates(t *testing.T) {
 	assert.NoError(err)
 
 	// Create a second network with the same name
-	_, err = client.CreateNetwork(netOptions)
-	if err != nil {
-		t.Logf("This test is run with Docker >=25.x.x, making it unrepresentative: %v", err)
+	_, errDuplicate := client.CreateNetwork(netOptions)
+
+	errVersion := dockerutil.CheckDockerVersion(">= 25.0.0-alpha1")
+
+	if errVersion == nil {
+		// Duplicate cannot be created with Docker >= 25.x.x
+		assert.Error(errDuplicate)
+	} else {
+		// Duplicate can be created with Docker < 25.x.x
+		assert.NoError(errDuplicate)
 	}
 
 	// The duplicate network is removed here
