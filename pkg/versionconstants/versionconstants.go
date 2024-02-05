c3eab51b1737cccb804d6096b4e6a605e974d452
Randy Fay <randy@randyfay.com>
2024-02-05 16:21:22 -0700

    build: Use new tag to get new deb.sury.org key (and updated php versions), fixes #5788 (#5790)

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -15,7 +15,7 @@ var AmplitudeAPIKey = ""
 var WebImg = "ddev/ddev-webserver"
 
 // WebTag defines the default web image tag
-var WebTag = "20240104_redundant_chown" // Note that this can be overridden by make
+var WebTag = "20240205_update_debsuryorg_key" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "ddev/ddev-dbserver"
