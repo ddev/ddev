aaf786bbae1af93a64527c00b92616bc49e632d2
Stanislav Zhuk <stasadev@gmail.com>
2024-02-19 12:42:44 +0200

    Move the banner below the menu

--- docs/content/assets/warning-banner-for-latest.js
+++ docs/content/assets/warning-banner-for-latest.js
@@ -5,14 +5,14 @@ document.addEventListener("DOMContentLoaded", function () {
 function load_warning_banner_for_latest() {
     let currentUrl = window.location.href;
 
-    if (currentUrl.includes('/latest/')) {
-        // Insert the warning banner as the first element in the body
-        document.body.insertAdjacentHTML('afterbegin', `
+    if (!currentUrl.includes('/stable/')) {
+        // Insert the warning banner after the nav bar
+        document.querySelector('.md-tabs[data-md-component="tabs"]').insertAdjacentHTML('afterend', `
             <div id="warning-banner-for-latest" data-md-color-scheme="default" style="display: none;">
                 <aside class="md-banner md-banner--warning">
                     <div class="md-banner__inner md-grid md-typeset">
                         You're viewing documentation for an unreleased version of DDEV.
-                        <a href="${currentUrl.replace(/\/latest\//, '/stable/')}">
+                        <a id="stable-docs-link" href="https://ddev.readthedocs.io/en/stable/">
                             <strong>Click here to see the stable documentation.</strong>
                         </a>
                     </div>
@@ -22,10 +22,8 @@ function load_warning_banner_for_latest() {
 
         // Display the warning banner after a delay to reduce flickering
         setTimeout(() => {
-            let warningBanner = document.getElementById('warning-banner-for-latest');
-            if (warningBanner) {
-                warningBanner.style.display = 'block';
-            }
+            document.getElementById('stable-docs-link').href = `https://ddev.readthedocs.io/${new URL(window.location.href).pathname.replace(/\/en\/[^/]+/, 'en/stable')}`;
+            document.getElementById('warning-banner-for-latest').style.display = 'block';
         }, 100);
     }
 }
