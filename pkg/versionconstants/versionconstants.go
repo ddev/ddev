4c57ef05e652320248a8ae03c828852ddd655575
Stanislav Zhuk <stasadev@gmail.com>
2024-02-28 21:15:36 +0200

    build: bump the webimage

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -15,7 +15,7 @@ var AmplitudeAPIKey = ""
 var WebImg = "ddev/ddev-webserver"
 
 // WebTag defines the default web image tag
-var WebTag = "20240213_php_8.2_default" // Note that this can be overridden by make
+var WebTag = "20240228_tjarrett_fixes_kill_supervisor" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "ddev/ddev-dbserver"
