1ca5643b465c79a011ca7cccdfe730a0d5b4dde2
M. Mikkel Rummelhoff <mail@mmikkel.no>
2024-03-07 23:39:00 +0100

    fix: Adds text/javascript MIME type to gzip_types and mod_deflate.conf, fixes #5937 (#5938) [skip ci]
    Co-authored-by: Randy Fay <randy@randyfay.com>
    Co-authored-by: Stanislav Zhuk <stasadev@gmail.com>

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -15,7 +15,7 @@ var AmplitudeAPIKey = ""
 var WebImg = "ddev/ddev-webserver"
 
 // WebTag defines the default web image tag
-var WebTag = "20240307_blackfire" // Note that this can be overridden by make
+var WebTag = "20240306_mmikkel_javascript_type" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "ddev/ddev-dbserver"
