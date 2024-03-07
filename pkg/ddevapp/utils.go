0583863e23c1057dca40e6c9f865282681d66245
Stanislav Zhuk <stasadev@gmail.com>
2024-03-07 16:02:28 +0200

    fix: Gitpod and Codespaces can use HTTPS

--- pkg/ddevapp/utils.go
+++ pkg/ddevapp/utils.go
@@ -499,7 +499,7 @@ func (app *DdevApp) CanUseHTTPOnly() bool {
 	switch {
 	// Gitpod and Codespaces have their own router with TLS termination
 	case nodeps.IsGitpod() || nodeps.IsCodespaces():
-		return true
+		return false
 	// If we have no router, then no https otherwise
 	case IsRouterDisabled(app):
 		return true
