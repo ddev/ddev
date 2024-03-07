b07066b9165cc4ff74dcc50baa9e2976178f34d3
Thomas Hauschild <7961978+Morgy93@users.noreply.github.com>
2024-03-07 05:24:28 +0100

    fix: isMagento2App() should detect version 2.4.5+, update test URLs (#5804) [skip ci]
    Co-authored-by: Randy Fay <randy@randyfay.com>

--- pkg/ddevapp/magento.go
+++ pkg/ddevapp/magento.go
@@ -23,7 +23,7 @@ func isMagentoApp(app *DdevApp) bool {
 
 // isMagento2App returns true if the app is of type magento2
 func isMagento2App(app *DdevApp) bool {
-	ism2, err := fileutil.FgrepStringInFile(filepath.Join(app.AppRoot, app.Docroot, "..", "SECURITY.md"), `https://hackerone.com/magento`)
+	ism2, err := fileutil.FgrepStringInFile(filepath.Join(app.AppRoot, app.Docroot, "..", "SECURITY.md"), `https://hackerone.com/`)
 	if err == nil && ism2 {
 		return true
 	}
