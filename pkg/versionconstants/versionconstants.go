55053f055955fe5e146e56954ed34ba22fc2c447
Randy Fay <randy@randyfay.com>
2024-03-20 20:58:06 -0600

    build: use new go-based platform.sh CLI, fixes #4668 (#5995)

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -15,7 +15,7 @@ var AmplitudeAPIKey = ""
 var WebImg = "ddev/ddev-webserver"
 
 // WebTag defines the default web image tag
-var WebTag = "20240305_stasadev_remove_kill_supervisor" // Note that this can be overridden by make
+var WebTag = "20240320_new_platform_cli" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "ddev/ddev-dbserver"
