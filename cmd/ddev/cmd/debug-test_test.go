b91773ffdeaccb92773a9564d50d2763d5ca182b
Randy Fay <randy@randyfay.com>
2024-03-22 09:29:43 -0600

    fix up test, simplify

--- cmd/ddev/cmd/debug-test_test.go
+++ cmd/ddev/cmd/debug-test_test.go
@@ -1,44 +1,37 @@
 package cmd
 
 import (
-	"github.com/ddev/ddev/pkg/ddevapp"
 	"os"
 	"testing"
 
 	"github.com/ddev/ddev/pkg/exec"
-	"github.com/ddev/ddev/pkg/testcommon"
 	asrt "github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-// TestDebugTestCmd ensures that `ddev debug test` has basic functionality
-func TestDebugTestCmd(t *testing.T) {
+// TestCmdDebugTest ensures that `ddev debug test` has basic functionality
+func TestCmdDebugTest(t *testing.T) {
 	assert := asrt.New(t)
 
 	origDir, _ := os.Getwd()
-
-	debugAppName := t.Name()
-
-	testDir := testcommon.CreateTmpDir(debugAppName)
-	err := os.Chdir(testDir)
+	site := TestSites[0]
+	err := os.Chdir(site.Dir)
 	require.NoError(t, err)
 
-	out, err := exec.RunHostCommand(DdevBin, "config", "--project-name", debugAppName)
-	require.NoError(t, err, "Failed to run ddev config: %s", out)
+	//app, err := ddevapp.NewApp(site.Dir, true)
 
-	app, err := ddevapp.NewApp(testDir, true)
 	require.NoError(t, err)
 	t.Cleanup(func() {
-		_ = app.Stop(true, false)
 		err = os.Chdir(origDir)
 		assert.NoError(err)
-		_ = os.RemoveAll(testDir)
 	})
 
 	_ = os.Setenv("DDEV_NONINTERACTIVE", "true")
 	_ = os.Setenv("DDEV_DEBUG", "true")
 
-	out, err = exec.RunHostCommand(DdevBin, "debug", "test")
+	out, err := exec.RunHostCommand(DdevBin, "debug", "test")
+	// This is just a casual look at the output, not intended to look for all details.
 	require.Contains(t, out, "OS Information")
+	require.Contains(t, out, "webserver_type: nginx-fpm")
 	require.Contains(t, out, "PING dkdkd.ddev.site")
 }
