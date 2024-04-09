a5ce590a024b26e6e249e62cf8c81d8ffee254db
Randy Fay <randy@randyfay.com>
2024-04-09 09:24:46 -0600

    Revert logic changes to start over again [skip ci]

--- cmd/ddev/cmd/config.go
+++ cmd/ddev/cmd/config.go
@@ -126,9 +126,6 @@ var (
 
 	// ddevVersionConstraint sets a ddev version constraint to validate the ddev against
 	ddevVersionConstraint string
-
-	// updateArg allows updating the project with auto-detection.
-	updateArg bool
 )
 
 var providerName = nodeps.ProviderDefault
@@ -312,7 +309,7 @@ func init() {
 	ConfigCommand.Flags().Bool("disable-upload-dirs-warning", true, `Disable warnings about upload-dirs not being set when using performance-mode=mutagen.`)
 	ConfigCommand.Flags().StringVar(&ddevVersionConstraint, "ddev-version-constraint", "", `Specify a ddev version constraint to validate ddev against.`)
 	ConfigCommand.Flags().Bool("corepack-enable", true, `Do 'corepack enable' to enable latest yarn/pnpm'`)
-	ConfigCommand.Flags().BoolVar(&updateArg, "update", false, `Do 'update' to auto-detect the right settings`)
+	ConfigCommand.Flags().Bool("update", false, `Update project settings based on detection and project-type overrides`)
 
 	RootCmd.AddCommand(ConfigCommand)
 
@@ -427,14 +424,11 @@ func handleMainConfigArgs(cmd *cobra.Command, _ []string, app *ddevapp.DdevApp) 
 		util.Failed("Failed to get absolute path to Docroot %s: %v", app.Docroot, pathErr)
 	}
 
-	if !updateArg && app.Type != nodeps.AppTypeNone && projectTypeArg == "" && detectedApptype != app.Type {
-		// apptype was not passed, but we found an app of a different type
+	switch {
+	case app.Type != nodeps.AppTypeNone && projectTypeArg == "" && detectedApptype != app.Type: // apptype was not passed, but we found an app of a different type
 		util.Warning("A project of type '%s' was found in %s, but the project is configured with type '%s'", detectedApptype, fullPath, app.Type)
-	}
-
-	autoArg, _ := cmd.Flags().GetBool("auto")
-
-	if updateArg || autoArg {
+		break
+	default:
 		if projectTypeArg == "" {
 			projectTypeArg = detectedApptype
 		}
@@ -445,7 +439,7 @@ func handleMainConfigArgs(cmd *cobra.Command, _ []string, app *ddevapp.DdevApp) 
 
 	// App overrides are done after app type is detected, but
 	// before user-defined flags are set.
-	err = app.ConfigFileOverrideAction(updateArg || autoArg)
+	err = app.ConfigFileOverrideAction(false)
 	if err != nil {
 		util.Failed("Failed to run ConfigFileOverrideAction: %v", err)
 	}
