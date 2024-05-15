a377e8e2d28e5b063f05993fdbcc2f85e362536d
Ofer Shaal <shaalofer@gmail.com>
2024-05-15 12:02:29 -0400

    fix: update d9simple to d10simple (#6186) [skip ci]

--- .devcontainer/setup_test_project.sh
+++ .devcontainer/setup_test_project.sh
@@ -11,7 +11,7 @@ ddev delete -Oy tmp >/dev/null || true
 ddev --version
 
 export DDEV_NONINTERACTIVE=true
-DDEV_REPO=${DDEV_REPO:-https://github.com/ddev/d9simple}
+DDEV_REPO=${DDEV_REPO:-https://github.com/ddev/d10simple}
 DDEV_ARTIFACTS=${DDEV_REPO}-artifacts
 git clone ${DDEV_ARTIFACTS} "/tmp/${DDEV_ARTIFACTS##*/}" || true
 reponame=${DDEV_REPO##*/}
