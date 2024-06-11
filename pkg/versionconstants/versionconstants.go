840f96f3a4e0869349fc147aff4ad3c63c6ef471
Randy Fay <randy@randyfay.com>
2024-06-11 17:01:33 -0600

    Update Dockerfile and versionconstants.go

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -11,7 +11,7 @@ var AmplitudeAPIKey = ""
 var WebImg = "ddev/ddev-webserver"
 
 // WebTag defines the default web image tag
-var WebTag = "20240607_rfay_mariadb_10.11_client" // Note that this can be overridden by make
+var WebTag = "20240611_juanjol_yq" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "ddev/ddev-dbserver"
