405113b392cb13041dbd8481fd232d6758e5c140
Randy Fay <randy@randyfay.com>
2024-06-10 19:30:47 -0600

    Install mysql clients if available

--- pkg/ddevapp/config.go
+++ pkg/ddevapp/config.go
@@ -7,6 +7,7 @@ import (
 	"path/filepath"
 	"regexp"
 	"runtime"
+	"slices"
 	"sort"
 	"strconv"
 	"strings"
@@ -1013,6 +1014,10 @@ redirect_stderr=true
 			return "", fmt.Errorf("failed to write .webimageBuild/webextradaemons.conf: %v", err)
 		}
 		extraWebContent = extraWebContent + "\nADD webextradaemons.conf /etc/supervisor/conf.d\nRUN chmod 644 /etc/supervisor/conf.d/webextradaemons.conf\n"
+	}
+	// For MySQL 5.7+ we'll install the matching mysql client (and mysqldump) in the ddev-webserver
+	if app.Database.Type == "mysql" && slices.Contains([]string{nodeps.MySQL57, nodeps.MySQL80, nodeps.MySQL84}, app.Database.Version) {
+		extraWebContent = extraWebContent + "\nRUN mysql-client-install.sh\n"
 	}
 
 	err = WriteBuildDockerfile(app.GetConfigPath(".webimageBuild/Dockerfile"), app.GetConfigPath("web-build"), app.WebImageExtraPackages, app.ComposerVersion, extraWebContent)
