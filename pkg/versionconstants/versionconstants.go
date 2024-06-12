18742cb24ac3dc0da6019e8486af0173f4799222
Randy Fay <randy@randyfay.com>
2024-06-12 12:59:46 -0600

    Add older mariadb-dump to container [skip ci]

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -11,7 +11,7 @@ var AmplitudeAPIKey = ""
 var WebImg = "ddev/ddev-webserver"
 
 // WebTag defines the default web image tag
-var WebTag = "20240610_mysql_clients" // Note that this can be overridden by make
+var WebTag = "20240612_fix_testAcquiaPush" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "ddev/ddev-dbserver"
