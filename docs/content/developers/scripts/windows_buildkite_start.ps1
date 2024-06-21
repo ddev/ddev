0eda7ee85bb72fe86bdd35e878ce09847404b7a0
Randy Fay <randy@randyfay.com>
2024-06-21 12:37:58 -0600

    docs: revisit WSL Docker Desktop setup instructions (#6330)

--- docs/content/developers/scripts/windows_buildkite_start.ps1
+++ docs/content/developers/scripts/windows_buildkite_start.ps1
@@ -3,7 +3,7 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Install required items using chocolatey
-choco install -y composer ddev docker-desktop git jq  mysql-cli golang GoogleChrome make mkcert netcat nodejs nssm zip
+choco install -y composer ddev git jq  mysql-cli golang GoogleChrome make mkcert netcat nodejs zip
 
 net localgroup docker-users /add
 net localgroup docker-users testbot /add
@@ -14,5 +14,3 @@ Set-Timezone -Id "Mountain Standard Time"
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock" /t REG_DWORD /f /v "AllowDevelopmentWithoutDevLicense" /d "1"
 
 cmd /c "setx /M PATH ""C:\Program Files\Git\bin;%PATH%"""
-
-
