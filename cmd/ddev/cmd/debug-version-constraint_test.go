d17eb56506d9280cafc43fb504098f6684aa1e3a
Stanislav Zhuk <stasadev@gmail.com>
2024-06-30 17:16:49 +0300

    test: fix test for versions that don't start with 'v1.'

--- cmd/ddev/cmd/debug-version-constraint_test.go
+++ cmd/ddev/cmd/debug-version-constraint_test.go
@@ -2,40 +2,43 @@ package cmd
 
 import (
 	"encoding/json"
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/ddev/ddev/pkg/exec"
 	"github.com/ddev/ddev/pkg/versionconstants"
-	asrt "github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 // TestDebugVersionConstraintCmd tests that ddev debug version-constraint works
 func TestDebugVersionConstraintCmd(t *testing.T) {
-	assert := asrt.New(t)
-
 	versionConstraint := ">= 1.twentythree"
 	out, err := exec.RunHostCommand(DdevBin, "debug", "version-constraint", versionConstraint)
-	assert.Error(err)
-	assert.Contains(out, "constraint is invalid")
+	require.Error(t, err)
+	require.Contains(t, out, "constraint is invalid")
 
 	out, err = exec.RunHostCommand(DdevBin, "debug", "version-constraint")
-	assert.Error(err)
-	assert.Contains(out, "This command only takes one optional argument")
+	require.Error(t, err)
+	require.Contains(t, out, "This command only takes one optional argument")
+
+	if !strings.HasPrefix(versionconstants.DdevVersion, "v1.") {
+		t.Skip(fmt.Sprintf("Skipping because ddev version doesn't start with 'v1.', it's '%v'", versionconstants.DdevVersion))
+	}
 
 	versionConstraint = "< " + versionconstants.DdevVersion
 	out, err = exec.RunHostCommand(DdevBin, "debug", "version-constraint", versionConstraint)
-	assert.Error(err)
-	assert.Contains(out, "doesn't meet the constraint")
+	require.Error(t, err)
+	require.Contains(t, out, "doesn't meet the constraint")
 
 	versionConstraint = ">= " + versionconstants.DdevVersion
 	out, err = exec.RunHostCommand(DdevBin, "debug", "version-constraint", versionConstraint)
-	assert.NoError(err)
-	assert.Equal(out, "true\n")
+	require.NoError(t, err)
+	require.Equal(t, out, "true\n")
 
 	versionConstraint = ">= " + versionconstants.DdevVersion
 	out, err = exec.RunHostCommand(DdevBin, "debug", "-j", "version-constraint", versionConstraint)
-	assert.NoError(err)
+	require.NoError(t, err)
 	jsonResult := make(map[string]interface{})
 	err = json.Unmarshal([]byte(out), &jsonResult)
 	require.NoError(t, err, "failed to unmarshal version-constraint '%v'", out)
