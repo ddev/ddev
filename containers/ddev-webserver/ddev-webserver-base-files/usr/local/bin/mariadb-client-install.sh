2e3b4d5dab889730f27912ea8de633d514549005
Stanislav Zhuk <stasadev@gmail.com>
2024-07-04 23:26:06 +0300

    refactor: skip maxscale and tools repos [skip ci]

--- containers/ddev-webserver/ddev-webserver-base-files/usr/local/bin/mariadb-client-install.sh
+++ containers/ddev-webserver/ddev-webserver-base-files/usr/local/bin/mariadb-client-install.sh
@@ -16,16 +16,16 @@ MARIADB_VERSION=${DDEV_DATABASE#*:}
 # Search for CHANGE_MARIADB_CLIENT to update related code.
 # Add MariaDB versions that can have their own client here:
 if [ "${MARIADB_VERSION}" = "11.4" ]; then
-  set -x
   # Configure the correct repository for mariadb
-  DEFAULT_MARIADB_VERSION="10.11"
-  sed -i "s|${DEFAULT_MARIADB_VERSION}|${MARIADB_VERSION}|g" /etc/apt/sources.list.d/mariadb.list
-  # update only mariadb.list to make it faster
-  timeout 30 apt-get update -o Dir::Etc::sourcelist="sources.list.d/mariadb.list" \
-    -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0" || exit 2
-  # Install the mariadb-client and mysql symlinks
-  # MariaDB 11.x moved MySQL symlinks into separate packages
-  apt-get install -y mariadb-client mariadb-client-compat || exit 3
+  set -x
+  timeout 30 mariadb_repo_setup --mariadb-server-version="mariadb-${MARIADB_VERSION}" --skip-maxscale --skip-tools --skip-key-import || exit 2
+  rm -f /etc/apt/sources.list.d/mariadb.list.old_*
+  # --skip-key-import flag doesn't download the existing key again and omits "apt-get -qq update",
+  # so we can run "apt-get -qq update" manually only for mariadb repo to make it faster
+  apt-get -qq update -o Dir::Etc::sourcelist="sources.list.d/mariadb.list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0" || exit 3
+  # Install the mariadb-client and MySQL symlinks
+  # MariaDB 11.x moved MySQL symlinks into a separate package
+  apt-get install -y mariadb-client mariadb-client-compat || exit 4
 else
-  echo "This script is not intended to run with mariadb:${MARIADB_VERSION}" && exit 4
+  echo "This script is not intended to run with mariadb:${MARIADB_VERSION}" && exit 5
 fi
