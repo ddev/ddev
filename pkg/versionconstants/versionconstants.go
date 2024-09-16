8f0d661d0521f6d3d336c34f3e5d0d162beb24f9
Randy Fay <randy@randyfay.com>
2024-09-16 12:02:34 -0600

    Revert "build: bump docker-compose to 2.29.3 (#6538)"
    This reverts commit b2e3801a8fed23e2cd445165a2985ebceb7a3fe8.

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -42,7 +42,7 @@ var MutagenVersion = ""
 
 const RequiredMutagenVersion = "0.17.2"
 
-const RequiredDockerComposeVersionDefault = "v2.29.3"
+const RequiredDockerComposeVersionDefault = "v2.29.1"
 
 // Drupal11RequiredSqlite3Version for ddev-webserver
 const Drupal11RequiredSqlite3Version = "3.45.1"
