9a7fe570345218fc3485828bc768beb8a47fa311
Stefan Bürk <stefan@buerk.tech>
2024-09-20 23:23:56 +0200

    feat: streamline TYPO3 non-composer mode configuration support (#6525)

--- pkg/ddevapp/typo3.go
+++ pkg/ddevapp/typo3.go
@@ -125,32 +125,68 @@ func getTypo3Hooks() []byte {
 	return []byte(Typo3Hooks)
 }
 
-// setTypo3SiteSettingsPaths sets the paths to settings files for templating
+// setTypo3SiteSettingsPaths sets the paths to settings files for templating. TYPO3 supports different setup structures,
+// composer, legacy and mono repository which differs in places and naming of these files depending on mode and version.
+// Thus different detection function are provided to determine the best suitable place and filenames.
 func setTypo3SiteSettingsPaths(app *DdevApp) {
 	var settingsFilePath, localSettingsFilePath string
 
-	if isTypo3v12OrHigher(app) {
+	if isTypo3ComposerV12OrHigher(app) {
+		// Since TYPO3 v12 the configuration files are named `settings.php` and `additional.php` and expected
+		// to be in `project-folder/config/system` for composer mode installations. Set them now to ensure ddev
+		// writes them in the correct place.
 		settingsFileBasePath := filepath.Join(app.AppRoot, app.ComposerRoot)
 		settingsFilePath = filepath.Join(settingsFileBasePath, "config", "system", "settings.php")
 		localSettingsFilePath = filepath.Join(settingsFileBasePath, "config", "system", "additional.php")
-	} else if isTypo3App(app) {
+	} else if isTypo3LegacyV12OrHigher(app) {
+		// Since TYPO3 v12 the configuration files are names `settings.php` and `additional.php` and expected to
+		// be in `docroot/typo3conf/system/` which differs from TYPO3 v12 or higher composer mode installations
+		// above. TYPO3 v12 or higher Core Development mono repository is basically a non-composer mode installation
+		// albeit having a composer.json in the project root folder. Set now the correct path and configuration file
+		// names suitable for these setups.
+		settingsFileBasePath := filepath.Join(app.AppRoot, app.Docroot)
+		settingsFilePath = filepath.Join(settingsFileBasePath, "typo3conf", "system", "settings.php")
+		localSettingsFilePath = filepath.Join(settingsFileBasePath, "typo3conf", "system", "additional.php")
+	} else if isTypo3LegacyV11OrLower(app) {
+		// Up to TYPO3 v11 configuration files had the same naming and resided in the `docroot/typo3conf/` folder
+		// which dates back to times before composer has been a gamechanger in the PHP ecosystem. TYPO3 Core Development
+		// mono repository shared the same and we do not have to differenciate between composer and legacy mode here.
 		settingsFileBasePath := filepath.Join(app.AppRoot, app.Docroot)
 		settingsFilePath = filepath.Join(settingsFileBasePath, "typo3conf", "LocalConfiguration.php")
 		localSettingsFilePath = filepath.Join(settingsFileBasePath, "typo3conf", "AdditionalConfiguration.php")
 	} else {
-		// As long as TYPO3 is not installed, the file paths are set to the
-		// AppRoot to avoid the creation of the .gitignore in the wrong location.
+		// As long as TYPO3 is not installed, the file paths are set to the AppRoot to avoid the creation of the
+		// .gitignore in the wrong location. Set the longstanding and old names here as it does not matter at all.
+		// createTypo3SettingsFile skips early if these configuration are returned to be in the AppRoot which has
+		// never been a suitable place for TYPO3 literally not writing them and flags them skipable.
 		settingsFilePath = filepath.Join(app.AppRoot, "LocalConfiguration.php")
 		localSettingsFilePath = filepath.Join(app.AppRoot, "AdditionalConfiguration.php")
 	}
 
-	// Update file paths
+	// Update file paths to the above determined paths
 	app.SiteSettingsPath = settingsFilePath
 	app.SiteDdevSettingsFile = localSettingsFilePath
 }
 
-// isTypoApp returns true if the app is of type typo3
+// isTypo3App returns true if the app is of type typo3 including composer mode, legacy mode and mono repository
 func isTypo3App(app *DdevApp) bool {
+	if isTypo3ComposerV12OrHigher(app) {
+		return true
+	}
+	if isTypo3LegacyV12OrHigher(app) {
+		return true
+	}
+	if isTypo3LegacyV11OrLower(app) {
+		return true
+	}
+	return false
+}
+
+// Up to TYPO3 v11 legacy and composer mode installation provided the system extensions and the configuration in the
+// same structure within the docroot. Only difference is that for composer mode a public subfolder is used as docroot
+// which make no difference in the way to detect both variants. TYPO3 Core Development monorepository also shared the
+// same filestem layout. Thus using only a simple docroot/typo3 folder check here and be fine.
+func isTypo3LegacyV11OrLower(app *DdevApp) bool {
 	typo3Folder := filepath.Join(app.AppRoot, app.Docroot, "typo3")
 
 	// Check if the folder exists, fails if a symlink target does not exist.
@@ -164,6 +200,100 @@ func isTypo3App(app *DdevApp) bool {
 	}
 
 	return false
+}
+
+// Since TYPO3 v12 system extensions are no longer installed into `public/typo3/sysext/` and extension no longer into
+// `public/typo3conf/ext/`. This function verifies for a composer mode installation first and extract the TYPO3 version
+// from the `Typo3Version` php class if composer dependencies are installed and looks that source file up within the
+// composer vendor folder. The TYPO3 Core Development mono repository has a root composer.json but does not have the
+// system extensions within the vendor folder like composer mode installations and thus making it safe to check for the
+// Typo3Version class within the typo3/cms-core package folder.
+func isTypo3ComposerV12OrHigher(app *DdevApp) bool {
+	composerManifest, _ := composer.NewManifest(filepath.Join(app.AppRoot, app.ComposerRoot, "composer.json"))
+	vendorDir := "vendor"
+	if composerManifest != nil {
+		vendorDir = composerManifest.GetVendorDir()
+	}
+
+	versionFilePath := filepath.Join(app.AppRoot, app.ComposerRoot, vendorDir, "typo3", "cms-core", "Classes", "Information", "Typo3Version.php")
+	versionFile, err := fileutil.ReadFileIntoString(versionFilePath)
+
+	// Typo3Version class exists since v10.3.0. Before v11.5.0 the core was always
+	// installed into the folder public/typo3 so we can early return if the file
+	// is not found in the vendor folder.
+	if err != nil {
+		util.Debug("TYPO3 version class not found in '%s' for project %s, installed version is assumed to be older than 11.5.0: %v", versionFilePath, app.Name, err)
+		return false
+	}
+
+	// We may have a TYPO3 version 11 or higher and therefor have to parse the
+	// class file to properly detect the version.
+	re := regexp.MustCompile(`const\s+VERSION\s*=\s*'([^']+)`)
+
+	matches := re.FindStringSubmatch(versionFile)
+
+	if len(matches) < 2 {
+		util.Warning("Unexpected Typo3Version found for project %s in %v.", app.Name, versionFile)
+		return false
+	}
+
+	version, err := semver.NewVersion(matches[1])
+	if err != nil {
+		// This case never should happen
+		util.Warning("Unexpected error while parsing TYPO3 version ('%s') for project %s: %v.", matches[1], app.Name, err)
+		return false
+	}
+
+	util.Debug("Found TYPO3 version %v for project %s.", version.Original(), app.Name)
+
+	return version.Major() >= 12
+}
+
+// TYPO3 v12 changed some stuff, mostly for composer mode. Place and names of main and additional configuration changed
+// for composer and legacy mode with different places and also differs in places where system extensions can be found or
+// user extensions are installed. This function verifies if the current project reflects a TYPO3 v12 or higher legacy
+// mode installation to set the correct configuration folder and names in function `setTypo3SiteSettingsPaths`.
+// The TYPO3 Core Development mono repository uses literally the same layout albeit having a composer.json in the root
+// folder. In the mono repository the TYPO3 system extensions resides not within the composer vendor folder.
+func isTypo3LegacyV12OrHigher(app *DdevApp) bool {
+	if !isTypo3LegacyV11OrLower(app) {
+		return false
+	}
+
+	typo3Folder := filepath.Join(app.AppRoot, app.Docroot, "typo3")
+
+	versionFilePath := filepath.Join(typo3Folder, "sysext", "core", "Classes", "Information", "Typo3Version.php")
+	versionFile, err := fileutil.ReadFileIntoString(versionFilePath)
+
+	// Typo3Version class exists since v10.3.0. Before v11.5.0 the core was always
+	// installed into the folder public/typo3 so we can early return if the file
+	// is not found in the vendor folder.
+	if err != nil {
+		util.Debug("TYPO3 version class not found in '%s' for project %s, installed version is assumed to be older than 11.5.0: %v", versionFilePath, app.Name, err)
+		return false
+	}
+
+	// We may have a TYPO3 version 11 or higher and therefor have to parse the
+	// class file to properly detect the version.
+	re := regexp.MustCompile(`const\s+VERSION\s*=\s*'([^']+)`)
+
+	matches := re.FindStringSubmatch(versionFile)
+
+	if len(matches) < 2 {
+		util.Warning("Unexpected Typo3Version found for project %s in %v.", app.Name, versionFile)
+		return false
+	}
+
+	version, err := semver.NewVersion(matches[1])
+	if err != nil {
+		// This case never should happen
+		util.Warning("Unexpected error while parsing TYPO3 version ('%s') for project %s: %v.", matches[1], app.Name, err)
+		return false
+	}
+
+	util.Debug("Found TYPO3 version %v for project %s.", version.Original(), app.Name)
+
+	return version.Major() >= 12
 }
 
 // typo3ImportFilesAction defines the TYPO3 workflow for importing project files.
@@ -209,46 +339,4 @@ func typo3ImportFilesAction(app *DdevApp, uploadDir, importPath, extPath string)
 	}
 
 	return nil
-}
-
-// isTypo3v12OrHigher returns true if the TYPO3 version is 12 or higher.
-func isTypo3v12OrHigher(app *DdevApp) bool {
-	composerManifest, _ := composer.NewManifest(filepath.Join(app.AppRoot, app.ComposerRoot, "composer.json"))
-	vendorDir := "vendor"
-	if composerManifest != nil {
-		vendorDir = composerManifest.GetVendorDir()
-	}
-
-	versionFilePath := filepath.Join(app.AppRoot, app.ComposerRoot, vendorDir, "typo3", "cms-core", "Classes", "Information", "Typo3Version.php")
-	versionFile, err := fileutil.ReadFileIntoString(versionFilePath)
-
-	// Typo3Version class exists since v10.3.0. Before v11.5.0 the core was always
-	// installed into the folder public/typo3 so we can early return if the file
-	// is not found in the vendor folder.
-	if err != nil {
-		util.Debug("TYPO3 version class not found in '%s' for project %s, installed version is assumed to be older than 11.5.0: %v", versionFilePath, app.Name, err)
-		return false
-	}
-
-	// We may have a TYPO3 version 11 or higher and therefor have to parse the
-	// class file to properly detect the version.
-	re := regexp.MustCompile(`const\s+VERSION\s*=\s*'([^']+)`)
-
-	matches := re.FindStringSubmatch(versionFile)
-
-	if len(matches) < 2 {
-		util.Warning("Unexpected Typo3Version found for project %s in %v.", app.Name, versionFile)
-		return false
-	}
-
-	version, err := semver.NewVersion(matches[1])
-	if err != nil {
-		// This case never should happen
-		util.Warning("Unexpected error while parsing TYPO3 version ('%s') for project %s: %v.", matches[1], app.Name, err)
-		return false
-	}
-
-	util.Debug("Found TYPO3 version %v for project %s.", version.Original(), app.Name)
-
-	return version.Major() >= 12
 }
