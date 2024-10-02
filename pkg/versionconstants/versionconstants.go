418de4c00581f3a64f7657bba2db2a23458df4fe
Randy Fay <randy@randyfay.com>
2024-10-02 15:01:44 -0600

    Add version tag

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -11,7 +11,7 @@ var AmplitudeAPIKey = ""
 var WebImg = "ddev/ddev-webserver"
 
 // WebTag defines the default web image tag
-var WebTag = "20240916_stasadev_log_stderr" // Note that this can be overridden by make
+var WebTag = "20241002_deviantintegral_vim_tiny" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "ddev/ddev-dbserver"
