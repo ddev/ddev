6e59b22a7c4fb90af3f14a87655d3fb557d8cdad
Randy Fay <randy@randyfay.com>
2024-10-13 21:28:42 -0600

    temporarily use nonexistent test

---
+++ containers/ddev-gitpod-base/test/containertest.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+if [ "${OS:-$(uname)}" = "Windows_NT" ]; then exit; fi
+
+if [ $# != "1" ]; then echo "docker image spec must be \$1"; exit 1; fi
+DOCKER_IMAGE=$1
+CONTAINER_NAME=ddev-gitpod-base-test
+
+function cleanup {
+	echo "Removing $CONTAINER_NAME"
+	docker rm -f $CONTAINER_NAME 2>/dev/null || true
+}
+trap cleanup EXIT
+
+# Wait for container to be ready.
+function containercheck {
+  for i in {15..0}; do
+    # fail if we can't find the container
+    if ! docker inspect ${CONTAINER_NAME} >/dev/null; then
+      break
+    fi
+
+    status="$(docker inspect ${CONTAINER_NAME} | jq -r '.[0].State.Status')"
+    if [ "${status}" != "running" ]; then
+      break
+    fi
+    health="$(docker inspect --format '{{json .State.Health }}' ${CONTAINER_NAME} | jq -r .Status)"
+    case ${health} in
+    healthy)
+      return 0
+      ;;
+    *)
+      sleep 1
+      ;;
+    esac
+  done
+  echo "# --- ddev-gitpod-base FAIL -----"
+  return 1
+}
+
+cleanup
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+true
+#docker run -t --rm ${DOCKER_IMAGE} ddev --version
