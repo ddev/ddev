677b446fb0f664ece8d84970943c4eb21e146ecf
Randy Fay <randy@randyfay.com>
2024-10-15 14:38:12 -0600

    keep digging myself out of multi-arch test hell

--- containers/containers_shared.mk
+++ containers/containers_shared.mk
@@ -10,8 +10,7 @@ DOTFILE_IMAGE = $(subst /,_,$(IMAGE))-$(VERSION)
 .PHONY: container push
 
 container: container-name
-	docker buildx use multi-arch-builder >/dev/null 2>&1 || docker buildx create --name multi-arch-builder --use
-	docker buildx build -o type=docker -t $(DOCKER_REPO):$(VERSION) $(DOCKER_ARGS) --platform $(BUILD_ARCHS) --label "build-info=$(DOCKER_REPO):$(VERSION) commit=$(shell git describe --tags --always)" .
+	docker build -t $(DOCKER_REPO):$(VERSION) $(DOCKER_ARGS) --label "build-info=$(DOCKER_REPO):$(VERSION) commit=$(shell git describe --tags --always)" .
 
 container-name:
 	@echo "container: $(DOCKER_REPO):$(VERSION)"
