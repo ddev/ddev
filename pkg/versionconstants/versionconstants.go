bbb7d7ad2c01f22535d495363eb30e805d86a0a8
Stanislav Zhuk <stasadev@gmail.com>
2024-10-16 17:02:51 +0300

    build: bump images to v1.23.5 and docker-compose to v2.29.7

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -11,22 +11,22 @@ var AmplitudeAPIKey = ""
 var WebImg = "ddev/ddev-webserver"
 
 // WebTag defines the default web image tag
-var WebTag = "20241009_use_cmd_composer" // Note that this can be overridden by make
+var WebTag = "v1.23.5" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "ddev/ddev-dbserver"
 
 // BaseDBTag is the main tag, DBTag is constructed from it
-var BaseDBTag = "v1.23.4"
+var BaseDBTag = "v1.23.5"
 
-const TraditionalRouterImage = "ddev/ddev-nginx-proxy-router:v1.23.4"
-const TraefikRouterImage = "ddev/ddev-traefik-router:v1.23.4"
+const TraditionalRouterImage = "ddev/ddev-nginx-proxy-router:v1.23.5"
+const TraefikRouterImage = "ddev/ddev-traefik-router:v1.23.5"
 
 // SSHAuthImage is image for agent
 var SSHAuthImage = "ddev/ddev-ssh-agent"
 
 // SSHAuthTag is ssh-agent auth tag
-var SSHAuthTag = "v1.23.4"
+var SSHAuthTag = "v1.23.5"
 
 // BusyboxImage is used a couple of places for a quick-pull
 var BusyboxImage = "busybox:stable"
@@ -42,7 +42,7 @@ var MutagenVersion = ""
 
 const RequiredMutagenVersion = "0.17.2"
 
-const RequiredDockerComposeVersionDefault = "v2.29.5"
+const RequiredDockerComposeVersionDefault = "v2.29.7"
 
 // Drupal11RequiredSqlite3Version for ddev-webserver
 const Drupal11RequiredSqlite3Version = "3.45.1"
