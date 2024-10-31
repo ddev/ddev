70f45a2aee6b9e9915301ce9f3c237394f650530
Ariel Barreiro <abarrei@gmail.com>
2024-10-31 12:45:13 -0300

    fix: test output, adding version to fail error message

--- cmd/ddev/cmd/debug-match-constraint_test.go
+++ cmd/ddev/cmd/debug-match-constraint_test.go
@@ -8,9 +8,11 @@ import (
 
 // TestDebugMatchConstraintCmd checks to see match-constraint behaves as expected
 func TestDebugMatchConstraintCmd(t *testing.T) {
+	version, err := exec.RunHostCommand(DdevBin, "--version")
+
 	out, err := exec.RunHostCommand(DdevBin, "debug", "match-constraint", "> 1.0")
-	require.NoError(t, err, "Match constraint should not have errored for > 1.0", out)
+	require.NoError(t, err, "Match constraint should not have errored for > 1.0, version='%s', out='%s'", version, out)
 
 	out, err = exec.RunHostCommand(DdevBin, "debug", "match-constraint", "< 1.0")
-	require.Error(t, err, "Match constraint should have errored for < 1.0", out)
+	require.Error(t, err, "Match constraint should have errored for < 1.0, version='%s', out='%s'", version, out)
 }
