a4bbccedda4fb8933f0a03b2462068e794555748
Ariel Barreiro <abarrei@gmail.com>
2024-10-31 15:28:43 -0300

    tests: fix for any version, just checking on = and !=

--- cmd/ddev/cmd/debug-match-constraint_test.go
+++ cmd/ddev/cmd/debug-match-constraint_test.go
@@ -2,17 +2,20 @@ package cmd
 
 import (
 	"github.com/ddev/ddev/pkg/exec"
+	"github.com/ddev/ddev/pkg/versionconstants"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
 
 // TestDebugMatchConstraintCmd checks to see match-constraint behaves as expected
+// @see https://github.com/Masterminds/semver#checking-version-constraints
 func TestDebugMatchConstraintCmd(t *testing.T) {
-	version, _ := exec.RunHostCommand(DdevBin, "--version")
 
-	out, err := exec.RunHostCommand(DdevBin, "debug", "match-constraint", "> 1.0")
-	require.NoError(t, err, "Match constraint should not have errored for > 1.0, version='%s', out='%s'", version, out)
+	constraint := "= " + versionconstants.DdevVersion
+	out, err := exec.RunHostCommand(DdevBin, "debug", "match-constraint", constraint)
+	require.NoError(t, err, "Match constraint should not have errored for %s, out='%s'", constraint, out)
 
-	out, err = exec.RunHostCommand(DdevBin, "debug", "match-constraint", "< 1.0")
-	require.Error(t, err, "Match constraint should have errored for < 1.0, version='%s', out='%s'", version, out)
+	constraint = "!= " + versionconstants.DdevVersion
+	out, err = exec.RunHostCommand(DdevBin, "debug", "match-constraint", constraint)
+	require.Error(t, err, "Match constraint should have errored for %s, out='%s'", constraint, out)
 }
