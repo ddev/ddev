1679424df3cfe265cdb5e98349948facd05ae956
Ariel Barreiro <abarrei@gmail.com>
2024-11-01 09:07:19 -0300

    tests: add skip and cleanup test

--- cmd/ddev/cmd/debug-match-constraint_test.go
+++ cmd/ddev/cmd/debug-match-constraint_test.go
@@ -1,10 +1,11 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/ddev/ddev/pkg/exec"
 	"github.com/ddev/ddev/pkg/versionconstants"
 	"github.com/stretchr/testify/require"
-	"strings"
+	"regexp"
 	"testing"
 )
 
@@ -12,15 +13,16 @@ import (
 // @see https://github.com/Masterminds/semver#checking-version-constraints
 func TestDebugMatchConstraintCmd(t *testing.T) {
 
-	constraint := ">= 1.0"
-	v, _ := exec.RunHostCommand(DdevBin, "--version")
-	v = strings.Trim(v, "\r\n ")
-	t.Logf("DdevBin='%s', version from binary='%s', version from versionconstants='%s'", DdevBin, v, constraint)
+	versionRegex := regexp.MustCompile(`^v[0-9]+\.`)
+	if !versionRegex.MatchString(versionconstants.DdevVersion) {
+		t.Skip(fmt.Sprintf("Skipping because ddev version doesn't start with any valid version tag, it's '%v'", versionconstants.DdevVersion))
+	}
 
+	constraint := ">= 1.0"
 	out, err := exec.RunHostCommand(DdevBin, "debug", "match-constraint", constraint)
 	require.NoError(t, err, "Match constraint should not have errored for %s, out='%s'", constraint, out)
 
-	constraint := "< 1.0"
+	constraint = "< 1.0"
 	out, err = exec.RunHostCommand(DdevBin, "debug", "match-constraint", constraint)
 	require.Error(t, err, "Match constraint should have errored for %s, out='%s'", constraint, out)
 }
