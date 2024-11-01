8ca4f70a5b623e1a927151815cdb2ffa5defe81f
Randy Fay <randy@randyfay.com>
2024-10-31 21:15:01 -0600

    try t.Log() instead of util.Debug()

--- cmd/ddev/cmd/debug-match-constraint_test.go
+++ cmd/ddev/cmd/debug-match-constraint_test.go
@@ -2,9 +2,9 @@ package cmd
 
 import (
 	"github.com/ddev/ddev/pkg/exec"
-	"github.com/ddev/ddev/pkg/util"
 	"github.com/ddev/ddev/pkg/versionconstants"
 	"github.com/stretchr/testify/require"
+	"strings"
 	"testing"
 )
 
@@ -13,8 +13,9 @@ import (
 func TestDebugMatchConstraintCmd(t *testing.T) {
 
 	constraint := "= " + versionconstants.DdevVersion
-	out, _ := exec.RunHostCommand(DdevBin, "--version")
-	util.Debug("DdevBin=%s, version=%s", DdevBin, out)
+	v, _ := exec.RunHostCommand(DdevBin, "--version")
+	v = strings.Trim(v, "\r\n ")
+	t.Logf("DdevBin='%s', version from binary='%s', version from versionconstants='%s'", DdevBin, v, constraint)
 
 	out, err := exec.RunHostCommand(DdevBin, "debug", "match-constraint", constraint)
 	require.NoError(t, err, "Match constraint should not have errored for %s, out='%s'", constraint, out)
