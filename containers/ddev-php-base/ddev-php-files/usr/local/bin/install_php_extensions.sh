c70bddc88f1a73aad8c9ca229ea4eb9512dd7389
Randy Fay <randy@randyfay.com>
2024-11-01 15:36:08 -0600

    Move logic to a script so it can be used by ddev adding layer

---
+++ containers/ddev-php-base/ddev-php-files/usr/local/bin/install_php_extensions.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+if [ -z "${1:-}" ] || [ -z "${2:-}" ]; then
+    echo "Usage: $0 <PHP_VERSION> <ARCH>"
+    exit 1
+fi
+
+if ! command -v yq >/dev/null ; then
+  echo "yq is required, please install it" && exit 2
+fi
+
+# Assign PHP version and architecture
+PHP_VERSION=$1
+ARCH=$2
+
+# Retrieve and format the list of packages for the specified PHP version and architecture
+pkgs=$(yq ".${PHP_VERSION//.}.${ARCH} | join(\" \")" /php-packages.yaml | awk -v v="$PHP_VERSION" 'BEGIN {RS=" ";} {printf "%s-%s ", v, $0}')
+
+# Echo the packages to be installed for logging
+echo "Installing packages for PHP ${PHP_VERSION} on ${ARCH}: $pkgs"
+
+apt-get -qq install --no-install-recommends --no-install-suggests -y $pkgs || exit $?
