6eacea9a9e61b691557468a622b451b391103725
Stanislav Zhuk <stasadev@gmail.com>
2024-11-07 09:25:41 +0200

    Return original exit code for mariadb [skip ci]

--- containers/ddev-webserver/ddev-webserver-base-files/usr/local/bin/mariadb-client-install.sh
+++ containers/ddev-webserver/ddev-webserver-base-files/usr/local/bin/mariadb-client-install.sh
@@ -18,14 +18,14 @@ MARIADB_VERSION=${DDEV_DATABASE#*:}
 if [ "${MARIADB_VERSION}" = "11.4" ]; then
   # Configure the correct repository for mariadb
   set -x
-  log-stderr.sh --timeout "${START_SCRIPT_TIMEOUT:-30}" mariadb_repo_setup --mariadb-server-version="mariadb-${MARIADB_VERSION}" --skip-maxscale --skip-tools --skip-key-import || exit 2
+  log-stderr.sh --timeout "${START_SCRIPT_TIMEOUT:-30}" mariadb_repo_setup --mariadb-server-version="mariadb-${MARIADB_VERSION}" --skip-maxscale --skip-tools --skip-key-import || exit $?
   rm -f /etc/apt/sources.list.d/mariadb.list.old_*
   # --skip-key-import flag doesn't download the existing key again and omits "apt-get -qq update",
   # so we can run "apt-get -qq update" manually only for mariadb repo to make it faster
-  apt-get -qq update -o Dir::Etc::sourcelist="sources.list.d/mariadb.list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0" || exit 3
+  apt-get -qq update -o Dir::Etc::sourcelist="sources.list.d/mariadb.list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0" || exit $?
   # Install the mariadb-client and MySQL symlinks
   # MariaDB 11.x moved MySQL symlinks into a separate package
-  apt-get install -y mariadb-client mariadb-client-compat || exit 4
+  DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends --no-install-suggests -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y mariadb-client mariadb-client-compat || exit $?
 else
-  echo "This script is not intended to run with mariadb:${MARIADB_VERSION}" && exit 5
+  echo "This script is not intended to run with mariadb:${MARIADB_VERSION}" && exit 1
 fi
