857a06d1d32af255c079b79eb6d7dd22603f9720
Stanislav Zhuk <stasadev@gmail.com>
2024-11-11 14:06:21 +0200

    Remove go library call because it needs complex logic for stdin

--- cmd/ddev/cmd/auth-ssh.go
+++ cmd/ddev/cmd/auth-ssh.go
@@ -2,96 +2,70 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/ddev/ddev/pkg/ddevapp"
 	"github.com/ddev/ddev/pkg/docker"
-	"github.com/ddev/ddev/pkg/dockerutil"
 	"github.com/ddev/ddev/pkg/fileutil"
-	"github.com/ddev/ddev/pkg/globalconfig"
-	"github.com/ddev/ddev/pkg/nodeps"
-	"github.com/ddev/ddev/pkg/output"
-	"github.com/ddev/ddev/pkg/util"
-	dockerContainer "github.com/docker/docker/api/types/container"
-	"github.com/docker/docker/api/types/mount"
-	"github.com/spf13/cobra"
+	"github.com/ddev/ddev/pkg/heredoc"
 	"os"
 	"path/filepath"
+	"slices"
 	"strings"
+
+	"github.com/ddev/ddev/pkg/ddevapp"
+	"github.com/ddev/ddev/pkg/exec"
+	"github.com/ddev/ddev/pkg/globalconfig"
+	"github.com/ddev/ddev/pkg/nodeps"
+	"github.com/ddev/ddev/pkg/util"
+	"github.com/spf13/cobra"
 )
 
-// sshKeyPath is the full path to the path containing SSH keys.
-var sshKeyPath string
+var sshKeyFiles, sshKeyDirs []string
 
 // AuthSSHCommand implements the "ddev auth ssh" command
 var AuthSSHCommand = &cobra.Command{
 	Use:   "ssh",
 	Short: "Add SSH key authentication to the ddev-ssh-agent container",
-	Long:  `Use this command to provide the password to your SSH key to the ddev-ssh-agent container, where it can be used by other containers. Normal usage is "ddev auth ssh", or if your key is not in ~/.ssh, ddev auth ssh --ssh-key-path=/some/path/.ssh"`,
-	Example: `ddev auth ssh
-ddev auth ssh -d ~/.ssh
-ddev auth ssh -f ~/.ssh/id_ed25519`,
+	Long:  `Use this command to provide the password to your SSH key to the ddev-ssh-agent container, where it can be used by other containers.`,
+	Example: heredoc.DocI2S(`
+		ddev auth ssh
+		ddev auth ssh -d ~/.ssh
+		ddev auth ssh -f ~/.ssh/id_ed25519 -f ~/.ssh/id_rsa
+		ddev auth ssh -d ~/.ssh -f /path/to/id_ed25519
+	`),
 	Run: func(cmd *cobra.Command, args []string) {
 		var err error
 		if len(args) > 0 {
 			util.Failed("This command takes no arguments.")
 		}
 
-		if cmd.Flags().Changed("ssh-key-path") {
-			sshKeyPath = cmd.Flag("ssh-key-path").Value.String()
-		} else if cmd.Flags().Changed("ssh-key-file") {
-			sshKeyPath = cmd.Flag("ssh-key-file").Value.String()
-		}
-
 		uidStr, _, _ := util.GetContainerUIDGid()
 
-		if sshKeyPath == "" {
+		// Use ~/.ssh if nothing is provided
+		if sshKeyFiles == nil && sshKeyDirs == nil {
 			homeDir, err := os.UserHomeDir()
 			if err != nil {
 				util.Failed("Unable to determine home directory: %v", err)
 			}
-			sshKeyPath = filepath.Join(homeDir, ".ssh")
-		}
-		if !filepath.IsAbs(sshKeyPath) {
-			sshKeyPath, err = filepath.Abs(sshKeyPath)
-			if err != nil {
-				util.Failed("Failed to derive absolute path for SSH key path %s: %v", sshKeyPath, err)
-			}
-		}
-		fi, err := os.Stat(sshKeyPath)
-		if os.IsNotExist(err) {
-			util.Failed("The SSH key path %s was not found", sshKeyPath)
-		}
-		if err != nil {
-			util.Failed("Failed to check status of SSH key path %s: %v", sshKeyPath, err)
+			sshKeyDirs = append(sshKeyDirs, filepath.Join(homeDir, ".ssh"))
 		}
 
-		var paths []string
-		var files []string
-		if !fi.IsDir() {
-			if cmd.Flags().Changed("ssh-key-path") {
-				util.Failed("SSH key path %s is not a directory", sshKeyPath)
-			}
-			files = append(files, sshKeyPath)
-		} else {
-			if cmd.Flags().Changed("ssh-key-file") {
-				util.Failed("SSH key path %s is not a file", sshKeyPath)
-			}
-			files, err = fileutil.ListFilesInDirFullPath(sshKeyPath, true)
-			if err != nil {
-				util.Failed("Failed to list files in %s: %v", sshKeyPath, err)
-			}
-		}
+		files := getSSHKeyPaths(sshKeyDirs, true, false)
+		files = append(files, getSSHKeyPaths(sshKeyFiles, false, true)...)
+
+		var keys []string
 		// Get real paths to key files in case they are symlinks
 		for _, file := range files {
-			realPath, err := filepath.EvalSymlinks(file)
+			key, err := filepath.EvalSymlinks(file)
 			if err != nil {
 				util.Failed("Error resolving symlinks for %s: %v", file, err)
 			}
-			if fileutil.FileIsReadable(realPath) {
-				paths = append(paths, realPath)
+			if !slices.Contains(keys, key) {
+				if isPrivateKey, _ := fileIsPrivateKey(key); isPrivateKey {
+					keys = append(keys, key)
+				}
 			}
 		}
-		if len(paths) == 0 {
-			util.Failed("No SSH keys found in %s", sshKeyPath)
+		if len(keys) == 0 {
+			util.Failed("No SSH keys found in %s", strings.Join(files, ", "))
 		}
 
 		app, err := ddevapp.GetActiveApp("")
@@ -109,51 +83,107 @@ ddev auth ssh -f ~/.ssh/id_ed25519`,
 			util.Failed("Failed to start ddev-ssh-agent container: %v", err)
 		}
 
+		var mounts []string
 		// Map to track already added keys
 		addedKeys := make(map[string]struct{})
-		var mounts []mount.Mount
-		for i, keyPath := range paths {
+		for i, keyPath := range keys {
 			filename := filepath.Base(keyPath)
 			// If it has the same name, change it to avoid conflicts
+			// This can happen if you have symlinks to the same key
 			if _, exists := addedKeys[filename]; exists {
 				filename = fmt.Sprintf("%s_%d", filename, i)
 			}
 			addedKeys[filename] = struct{}{}
-			mount := mount.Mount{
-				Type:     mount.TypeBind,
-				Source:   keyPath,
-				Target:   "/tmp/sshtmp/" + filename,
-				ReadOnly: true,
-			}
-			mounts = append(mounts, mount)
+			keyPath = util.WindowsPathToCygwinPath(keyPath)
+			mounts = append(mounts, "--mount=type=bind,src="+keyPath+",dst=/tmp/sshtmp/"+filename)
 		}
-		sshAddCmd := []string{"bash", "-c", fmt.Sprintf(`cp -r /tmp/sshtmp ~/.ssh && chmod -R go-rwx ~/.ssh && cd ~/.ssh && grep -l '^-----BEGIN .* PRIVATE KEY-----' * | xargs -d '\n' ssh-add; exit_code=$?; if [ $exit_code -eq 123 ]; then echo >&2 "No SSH keys found in %s"; fi; exit $exit_code`, sshKeyPath)}
-		config := &dockerContainer.Config{
-			Entrypoint: []string{},
-		}
-		hostConfig := &dockerContainer.HostConfig{
-			Mounts:      mounts,
-			VolumesFrom: []string{ddevapp.SSHAuthName},
-		}
-		_, out, err := dockerutil.RunSimpleContainerExtended(docker.GetSSHAuthImage()+"-built", "auth-ssh-"+util.RandString(6), sshAddCmd, uidStr, true, false, config, hostConfig)
-		out = strings.TrimSpace(out)
+
+		dockerCmd := []string{"run", "-it", "--rm", "--volumes-from=" + ddevapp.SSHAuthName, "--user=" + uidStr, "--entrypoint="}
+		dockerCmd = append(dockerCmd, mounts...)
+		dockerCmd = append(dockerCmd, docker.GetSSHAuthImage()+"-built", "bash", "-c", `cp -r /tmp/sshtmp ~/.ssh && chmod -R go-rwx ~/.ssh && cd ~/.ssh && grep -l '^-----BEGIN .* PRIVATE KEY-----' * | xargs -d '\n' ssh-add`)
+
+		err = exec.RunInteractiveCommand("docker", dockerCmd)
+
 		if err != nil {
 			helpMessage := ""
-			// Add a more helpful message to the obscure error from Docker
-			// Can be triggered if key is in /tmp on macOS
+			// Add more helpful message to the obscure error from Docker
+			// Can be triggered if the key is in /tmp on macOS
 			if strings.Contains(err.Error(), "bind source path does not exist") {
 				helpMessage = "\n\nThe specified SSH key path is not shared with your Docker provider."
 			}
-			util.Failed("Unable to run auth ssh: %v; output='%s'%s", err, out, helpMessage)
+			util.Failed("Docker command 'docker %v' failed: %v %v", echoDockerCmd(dockerCmd), err, helpMessage)
 		}
-		output.UserOut.Println(out)
 	},
 }
 
+// getSSHKeyPaths returns an array of full paths to SSH keys
+// with checks to ensure they are valid.
+func getSSHKeyPaths(sshKeyPathArray []string, acceptsDirsOnly bool, acceptsFilesOnly bool) []string {
+	var files []string
+	for _, sshKeyPath := range sshKeyPathArray {
+		if !filepath.IsAbs(sshKeyPath) {
+			sshKeyPath, err := filepath.Abs(sshKeyPath)
+			if err != nil {
+				util.Failed("Failed to derive absolute path for SSH key path %s: %v", sshKeyPath, err)
+			}
+		}
+		fi, err := os.Stat(sshKeyPath)
+		if os.IsNotExist(err) {
+			util.Failed("The SSH key path %s was not found", sshKeyPath)
+		}
+		if err != nil {
+			util.Failed("Failed to check status of SSH key path %s: %v", sshKeyPath, err)
+		}
+		if !fi.IsDir() {
+			if acceptsDirsOnly {
+				util.Failed("SSH key path %s is not a directory", sshKeyPath)
+			}
+			files = append(files, sshKeyPath)
+		} else {
+			if acceptsFilesOnly {
+				util.Failed("SSH key path %s is not a file", sshKeyPath)
+			}
+			files, err = fileutil.ListFilesInDirFullPath(sshKeyPath, true)
+			if err != nil {
+				util.Failed("Failed to list files in %s: %v", sshKeyPath, err)
+			}
+		}
+	}
+	return files
+}
+
+// fileIsPrivateKey checks if a file is readable and that it is a private key.
+// Regex isn't used here because files can be huge.
+// The full check if it's really a private key is done with grep -l '^-----BEGIN .* PRIVATE KEY-----'.
+func fileIsPrivateKey(filePath string) (bool, error) {
+	file, err := os.OpenFile(filePath, os.O_RDONLY, 0666)
+	if err != nil {
+		return false, err
+	}
+	// nolint: errcheck
+	defer file.Close()
+	prefix := []byte("-----BEGIN")
+	buffer := make([]byte, len(prefix))
+	_, err = file.Read(buffer)
+	if err != nil {
+		return false, err
+	}
+	return string(buffer) == string(prefix), nil
+}
+
+// echoDockerCmd formats the Docker command to be more readable.
+func echoDockerCmd(dockerCmd []string) string {
+	for i, arg := range dockerCmd {
+		if strings.Contains(arg, " ") {
+			dockerCmd[i] = `"` + arg + `"`
+		}
+	}
+	return strings.Join(dockerCmd, " ")
+}
+
 func init() {
-	AuthSSHCommand.Flags().StringP("ssh-key-file", "f", "", "full path to SSH key file")
-	AuthSSHCommand.Flags().StringP("ssh-key-path", "d", "", "full path to SSH key directory")
-	AuthSSHCommand.MarkFlagsMutuallyExclusive("ssh-key-file", "ssh-key-path")
+	AuthSSHCommand.Flags().StringArrayVarP(&sshKeyFiles, "ssh-key-file", "f", nil, "full path to SSH key file")
+	AuthSSHCommand.Flags().StringArrayVarP(&sshKeyDirs, "ssh-key-path", "d", nil, "full path to SSH key directory")
 
 	AuthCmd.AddCommand(AuthSSHCommand)
 }
