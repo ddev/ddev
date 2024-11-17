a8e522ef034e70d2907c1f61a47d2f8f29f9b3df
Randy Fay <randy@randyfay.com>
2024-11-17 10:38:27 -0700

    Bump ddev-webserver

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -11,7 +11,7 @@ var AmplitudeAPIKey = ""
 var WebImg = "ddev/ddev-webserver"
 
 // WebTag defines the default web image tag
-var WebTag = "20241118_rfay_d11_requirements_global" // Note that this can be overridden by make
+var WebTag = "20241117_remove_python_django" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "ddev/ddev-dbserver"
