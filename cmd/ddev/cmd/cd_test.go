74930e4a42714c136f6f6ef63e45966b899a31f5
Stanislav Zhuk <stasadev@gmail.com>
2024-11-20 00:21:21 +0200

    fix cd test

--- cmd/ddev/cmd/cd_test.go
+++ cmd/ddev/cmd/cd_test.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"github.com/ddev/ddev/pkg/ddevapp"
 	"github.com/ddev/ddev/pkg/exec"
 	"github.com/ddev/ddev/pkg/globalconfig"
 	"github.com/ddev/ddev/pkg/util"
@@ -13,21 +14,33 @@ import (
 // TestCdCmd runs `ddev cd` to see if it works.
 func TestCdCmd(t *testing.T) {
 	assert := asrt.New(t)
+	bashScript := filepath.Join(globalconfig.GetGlobalDdevDir(), "commands/host/shells/ddev.sh")
+	fishScript := filepath.Join(globalconfig.GetGlobalDdevDir(), "commands/host/shells/ddev.fish")
 	// Shows help
 	out, err := exec.RunHostCommand(DdevBin, "cd", TestSites[0].Name)
 	assert.NoError(err)
-	assert.Contains(out, filepath.Join(globalconfig.GetGlobalDdevDir(), "commands/host/shells/ddev.sh"))
-	assert.Contains(out, filepath.Join(globalconfig.GetGlobalDdevDir(), "commands/host/shells/ddev.fish"))
+	assert.Contains(out, bashScript)
+	assert.Contains(out, fishScript)
+	// Shows help
+	out, err = exec.RunHostCommand(DdevBin, "cd", "does-not-exist-"+util.RandString(4))
+	assert.NoError(err)
+	assert.Contains(out, bashScript)
+	assert.Contains(out, fishScript)
+	// Shows help
+	out, err = exec.RunHostCommand(DdevBin, "cd")
+	assert.NoError(err)
+	assert.Contains(out, bashScript)
+	assert.Contains(out, fishScript)
 	// Returns the path to the project
 	out, err = exec.RunHostCommand(DdevBin, "cd", TestSites[0].Name, "--get-approot")
 	assert.NoError(err)
 	assert.Equal(strings.TrimRight(out, "\n"), TestSites[0].Dir)
-	// Shows error
-	out, err = exec.RunHostCommand(DdevBin, "cd", "does-not-exist-"+util.RandString(4))
-	assert.Error(err)
-	assert.Contains(out, "Failed to find path for project")
-	// Shows error
-	out, err = exec.RunHostCommand(DdevBin, "cd")
-	assert.Error(err)
-	assert.Contains(out, "This command only takes one argument: project-name")
+	// Returns list of projects for autocompletion
+	out, err = exec.RunHostCommand(DdevBin, "cd", "--list")
+	assert.NoError(err)
+	projects, err := ddevapp.GetProjects(false)
+	assert.NoError(err)
+	for _, project := range projects {
+		assert.Contains(out, project.Name)
+	}
 }
