3b74cd44eb7d2300a9d03dbf473a7071619f1672
Randy Fay <randy@randyfay.com>
2024-11-27 11:25:08 -0700

    corepack_enable should be true for apptype drupal

--- pkg/ddevapp/drupal.go
+++ pkg/ddevapp/drupal.go
@@ -429,6 +429,8 @@ func drupalConfigOverrideAction(app *DdevApp) error {
 		drupalVersion = 9
 	case nodeps.AppTypeDrupal10:
 		drupalVersion = 10
+	case nodeps.AppTypeDrupal:
+		fallthrough
 	case nodeps.AppTypeDrupal11:
 		app.CorepackEnable = true
 		drupalVersion = 11
