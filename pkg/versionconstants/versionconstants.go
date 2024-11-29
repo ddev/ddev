772b398d92e4d01181b3bd6873fa3b8191356220
Stanislav Zhuk <stasadev@gmail.com>
2024-11-29 15:14:47 +0200

    build: bump Xdebug to 3.4.0 and docker-compose to v2.31.0 (#6786)

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -11,7 +11,7 @@ var AmplitudeAPIKey = ""
 var WebImg = "ddev/ddev-webserver"
 
 // WebTag defines the default web image tag
-var WebTag = "20241122_stasadev_php_pecl" // Note that this can be overridden by make
+var WebTag = "20241128_stasadev_xdebug" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "ddev/ddev-dbserver"
@@ -41,4 +41,4 @@ var MutagenVersion = ""
 
 const RequiredMutagenVersion = "0.18.0"
 
-const RequiredDockerComposeVersionDefault = "v2.30.3"
+const RequiredDockerComposeVersionDefault = "v2.31.0"
