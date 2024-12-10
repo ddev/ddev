585439bcfbce3b62d9e1189ff8e1ff8e03d1c2cf
Randy Fay <randy@randyfay.com>
2024-12-10 10:41:53 -0700

    Bump versionconstants [skip ci]

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -17,7 +17,7 @@ var WebTag = "v1.24.1" // Note that this can be overridden by make
 var DBImg = "ddev/ddev-dbserver"
 
 // BaseDBTag is the main tag, DBTag is constructed from it
-var BaseDBTag = "v1.24.1"
+var BaseDBTag = "20241210_rfay_mysql_tests"
 
 const TraefikRouterImage = "ddev/ddev-traefik-router:v1.24.1"
 
