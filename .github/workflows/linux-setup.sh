b6b68f0d94cf46b87ea833651b214027e59538c1
Randy Fay <randy@randyfay.com>
2024-12-30 08:26:24 -0700

    @stasadev suggestion, thanks [skip ci]
    Co-authored-by: Stanislav Zhuk <stasadev@gmail.com>

--- .github/workflows/linux-setup.sh
+++ .github/workflows/linux-setup.sh
@@ -19,8 +19,8 @@ curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
   | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
   && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
   | sudo tee /etc/apt/sources.list.d/ngrok.list \
-  && sudo apt update \
-  && sudo apt install ngrok
+  && sudo apt-get update -qq >/dev/null \
+  && sudo apt-get install -y -qq ngrok
 
 # Without this .curlrc CircleCI linux image doesn't respect mkcert certs
 echo "capath=/etc/ssl/certs/" >>~/.curlrc
