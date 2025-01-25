1652aaf3caaa20b23a0b80b155aca256d8296159
Randy Fay <randy@randyfay.com>
2025-01-25 14:38:36 -0700

    Forgot we have to background orb stop

--- .buildkite/test.sh
+++ .buildkite/test.sh
@@ -46,7 +46,8 @@ if [ "${OSTYPE%%[0-9]*}" = "darwin" ]; then
 
   # For Lima and Colima, as of Lima 1.0.4, having orbstack running
   # makes lima fail, see https://github.com/lima-vm/lima/issues/3145#issuecomment-2613728408
-  orb stop || true
+  command -v orb 2>/dev/null && echo "Stopping orbstack" && (nohup orb stop &)
+  sleep 3 # Since we backgrounded orb stop, make sure it completes
 
   # Now start the docker provider we want
   case ${DOCKER_TYPE:=none} in
