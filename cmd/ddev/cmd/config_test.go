3e363e91a3182afbb8be1541016604a80de0f63d
Stanislav Zhuk <stasadev@gmail.com>
2025-03-10 15:30:05 +0200

    fix: make `ddev config` respect project name from `config.*.yaml`

--- cmd/ddev/cmd/config_test.go
+++ cmd/ddev/cmd/config_test.go
@@ -888,3 +888,53 @@ func TestDrupalAppTypeUsage(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, nodeps.AppTypeDrupalLatestStable, app.Type)
 }
+
+// TestConfigRespectNameOverride tests that `ddev config` respects name override from `.ddev/config.*.yaml` files
+func TestConfigRespectNameOverride(t *testing.T) {
+	assert := asrt.New(t)
+
+	projectName := strings.ToLower(t.Name())
+	origDir, _ := os.Getwd()
+	_, _ = exec.RunHostCommand(DdevBin, "stop", "--unlist", projectName)
+
+	// Create a temporary directory and switch to it.
+	tmpDir := testcommon.CreateTmpDir(t.Name())
+	_ = os.Chdir(tmpDir)
+
+	var err error
+
+	t.Cleanup(func() {
+		err = os.Chdir(origDir)
+		assert.NoError(err)
+		out, err := exec.RunHostCommand(DdevBin, "delete", "-Oy", projectName)
+		assert.NoError(err, "output=%s", out)
+		_ = os.RemoveAll(tmpDir)
+	})
+
+	_ = os.Chdir(tmpDir)
+
+	// Create a dummy config file to make it look like it's an existing project
+	// Otherwise DDEV will not read the override
+	err = os.MkdirAll(filepath.Join(tmpDir, ".ddev"), 0777)
+	require.NoError(t, err)
+	configFile := filepath.Join(tmpDir, ".ddev/config.yaml")
+	_, err = os.Create(configFile)
+	require.NoError(t, err)
+
+	// Create an override for the project name
+	projectNameOverride := projectName + "-override"
+	err = os.WriteFile(filepath.Join(tmpDir, ".ddev/config.local.yaml"), []byte(`name: `+projectNameOverride), 0644)
+	require.NoError(t, err)
+
+	out, err := exec.RunHostCommand(DdevBin, "config", "--auto")
+	require.NoError(t, err, "error running ddev config --auto: '%s'", out)
+
+	configContents, err := os.ReadFile(configFile)
+	require.NoError(t, err, "Unable to read '%s'", configFile)
+
+	app := &ddevapp.DdevApp{}
+	err = yaml.Unmarshal(configContents, app)
+	require.NoError(t, err, "Could not unmarshal '%s'", configFile)
+
+	require.Equal(t, projectNameOverride, app.Name)
+}
