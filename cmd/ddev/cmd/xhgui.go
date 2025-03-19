594f140daa83b15892a113ea66376bfee49472ab
Stanislav Zhuk <stasadev@gmail.com>
2025-03-19 15:00:26 +0200

    reformat example with heredoc

--- cmd/ddev/cmd/xhgui.go
+++ cmd/ddev/cmd/xhgui.go
@@ -5,6 +5,7 @@ import (
 	"github.com/ddev/ddev/pkg/config/types"
 	"github.com/ddev/ddev/pkg/ddevapp"
 	"github.com/ddev/ddev/pkg/exec"
+	"github.com/ddev/ddev/pkg/heredoc"
 	"github.com/ddev/ddev/pkg/output"
 	"github.com/ddev/ddev/pkg/util"
 	"github.com/spf13/cobra"
@@ -15,12 +16,13 @@ var DdevXHGuiCmd = &cobra.Command{
 	ValidArgsFunction: ddevapp.GetProjectNamesFunc("active", 1),
 	Use:               "xhgui [on|off|launch|status]",
 	Short:             "Starts or checks status of XHGui performance monitoring and UI",
-	Example: `ddev xhgui
-ddev xhgui launch
-ddev xhgui on
-ddev xhgui off
-ddev xhgui status
-`,
+	Example: heredoc.DocI2S(`
+		ddev xhgui
+		ddev xhgui launch
+		ddev xhgui on
+		ddev xhgui off
+		ddev xhgui status
+	`),
 	Args: cobra.MaximumNArgs(1),
 	RunE: func(_ *cobra.Command, args []string) error {
 		app, err := ddevapp.GetActiveApp("")
