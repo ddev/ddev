50c59d8a762ee671d3c577f76cf7a3fdb0636aea
Stanislav Zhuk <stasadev@gmail.com>
2025-03-31 11:03:58 +0300

    feat: make generic webserver work with ephemeral ports

--- pkg/ddevapp/ddevapp.go
+++ pkg/ddevapp/ddevapp.go
@@ -1244,8 +1244,13 @@ func (app *DdevApp) Start() error {
 	app.RouterHTTPSPort = app.GetPrimaryRouterHTTPSPort()
 	app.MailpitHTTPPort = app.GetMailpitHTTPPort()
 	app.MailpitHTTPSPort = app.GetMailpitHTTPSPort()
+
+	AssignRouterPortsToGenericWebserverPorts(app)
+
 	portsToCheck := []*string{&app.RouterHTTPPort, &app.RouterHTTPSPort, &app.MailpitHTTPPort, &app.MailpitHTTPSPort}
 	GetEphemeralPortsIfNeeded(portsToCheck, true)
+
+	SyncGenericWebserverPortsWithRouterPorts(app)
 
 	app.DockerEnv()
 	dockerutil.EnsureDdevNetwork()
