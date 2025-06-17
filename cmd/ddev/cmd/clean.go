2c0c89cc94372f850db6635ca65320e417ffc6f9
Stanislav Zhuk <stasadev@gmail.com>
2025-06-17 17:35:33 +0300

    sort imports

--- cmd/ddev/cmd/clean.go
+++ cmd/ddev/cmd/clean.go
@@ -6,9 +6,9 @@ import (
 	"strings"
 
 	"github.com/ddev/ddev/pkg/amplitude"
-	"github.com/ddev/ddev/pkg/heredoc"
 	"github.com/ddev/ddev/pkg/ddevapp"
 	"github.com/ddev/ddev/pkg/globalconfig"
+	"github.com/ddev/ddev/pkg/heredoc"
 	"github.com/ddev/ddev/pkg/output"
 	"github.com/ddev/ddev/pkg/util"
 	"github.com/spf13/cobra"
