01d2012342f799a61e5f073ffa7b353a7ae10061
Randy Fay <randy@randyfay.com>
2025-06-20 12:19:00 -0600

    yay, working on traditional windows with UAC prompt [skip ci]

--- cmd/ddev_hostname/elevate_windows.go
+++ cmd/ddev_hostname/elevate_windows.go
@@ -24,16 +24,21 @@ const (
 )
 
 type shellExecuteInfo struct {
-	cbSize       uint32
-	fMask        uintptr
-	hwnd         uintptr
-	lpVerb       uintptr
-	lpFile       uintptr
-	lpParameters uintptr
-	lpDirectory  uintptr
-	nShow        int32
-	hInstApp     uintptr
-	hProcess     uintptr
+	cbSize         uint32
+	fMask          uint32
+	hwnd           uintptr
+	lpVerb         *uint16
+	lpFile         *uint16
+	lpParameters   *uint16
+	lpDirectory    *uint16
+	nShow          int32
+	hInstApp       uintptr
+	lpIDList       uintptr
+	lpClass        *uint16
+	hkeyClass      uintptr
+	dwHotKey       uint32
+	hIconOrMonitor uintptr
+	hProcess       windows.Handle
 }
 
 func elevateIfNeeded() {
@@ -71,7 +76,6 @@ func isElevated() bool {
 
 func elevate() {
 	util.Debug("elevate() called")
-	var err error
 	// Prepare UTF-16 pointers
 	verbPtr, _ := syscall.UTF16PtrFromString("runas")
 	exePath, _ := os.Executable()
@@ -79,32 +83,37 @@ func elevate() {
 	paramStr := strings.Join(os.Args[1:], " ")
 	paramsPtr, _ := syscall.UTF16PtrFromString(paramStr)
 
-	// Fill out the native SHELLEXECUTEINFO struct
 	sei := shellExecuteInfo{
 		cbSize:       uint32(unsafe.Sizeof(shellExecuteInfo{})),
 		fMask:        SEE_MASK_NOCLOSEPROCESS,
 		hwnd:         0,
-		lpVerb:       uintptr(unsafe.Pointer(verbPtr)),
-		lpFile:       uintptr(unsafe.Pointer(exePtr)),
-		lpParameters: uintptr(unsafe.Pointer(paramsPtr)),
-		lpDirectory:  0,
+		lpVerb:       verbPtr,
+		lpFile:       exePtr,
+		lpParameters: paramsPtr,
+		lpDirectory:  nil,
 		nShow:        windows.SW_NORMAL,
+		// hInstApp, lpIDList, lpClass, hkeyClass, dwHotKey, hIconOrMonitor, hProcess are zeroed
 	}
 
-	// Call ShellExecuteExW → UAC prompt
 	ret, _, lastErr := procShellExecuteExW.Call(uintptr(unsafe.Pointer(&sei)))
 	if ret == 0 {
 		fmt.Fprintln(os.Stderr, "Elevation failed:", lastErr)
 		os.Exit(1)
 	}
 
-	// Wait for the elevated process to finish
-	_, err = windows.WaitForSingleObject(windows.Handle(sei.hProcess), windows.INFINITE)
-	if err != nil {
-		util.Failed("Failed to wait for elevated process: %v", err)
+	if sei.hProcess != 0 {
+		_, err := windows.WaitForSingleObject(sei.hProcess, windows.INFINITE)
+		if err != nil {
+			util.Failed("Failed to wait for elevated process: %v", err)
+		}
+		var exitCode uint32
+		err = windows.GetExitCodeProcess(sei.hProcess, &exitCode)
+		if err != nil {
+			util.Failed("Failed to get exit code: %v", err)
+		}
+		os.Exit(int(exitCode))
+	} else {
+		// If hProcess is not set, just exit
+		os.Exit(0)
 	}
-	var exitCode uint32
-	err = windows.GetExitCodeProcess(windows.Handle(sei.hProcess), &exitCode)
-	util.Failed("Elevation failed; exitCode=%v: %v", exitCode, err)
-	os.Exit(int(exitCode))
 }
