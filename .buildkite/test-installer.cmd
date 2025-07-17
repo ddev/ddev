c4fceb27218531591fb7c37491cd8289822d429d
Randy Fay <randy@randyfay.com>
2025-07-17 16:40:46 -0600

    Try to make concurrency groups work in buildkite

--- .buildkite/test-installer.cmd
+++ .buildkite/test-installer.cmd
@@ -1,5 +1,10 @@
 @echo "Running Windows installer tests..."
 
+@echo "DEBUG: Concurrency group debug value: %CONCURRENCY_GROUP_DEBUG%"
+@echo "DEBUG: Agent ID: %BUILDKITE_AGENT_ID%"
+@echo "DEBUG: Agent Name: %BUILDKITE_AGENT_NAME%"
+@echo "DEBUG: Hostname: %HOSTNAME%"
+
 TASKKILL /T /F /IM mutagen.exe 2>NUL
 
 "C:\Program Files\git\bin\bash" .buildkite/test-installer.sh
--- .buildkite/test.cmd
+++ .buildkite/test.cmd
@@ -1,5 +1,10 @@
 @echo "Building using bash and build.sh"
 
+@echo "DEBUG: Concurrency group debug value: %CONCURRENCY_GROUP_DEBUG%"
+@echo "DEBUG: Agent ID: %BUILDKITE_AGENT_ID%"
+@echo "DEBUG: Agent Name: %BUILDKITE_AGENT_NAME%"
+@echo "DEBUG: Hostname: %HOSTNAME%"
+
 TASKKILL /T /F /IM mutagen.exe
 
 "C:\Program Files\git\bin\bash" .buildkite/test.sh
