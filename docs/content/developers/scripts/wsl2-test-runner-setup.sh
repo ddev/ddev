9943816b237e62db5092a3456be6f7c2f7f1d3a2
Randy Fay <randy@randyfay.com>
2025-08-01 11:37:58 -0600

    test: ngrok broke their installation moving to bookworm, fix it (#7501)

--- docs/content/developers/scripts/wsl2-test-runner-setup.sh
+++ docs/content/developers/scripts/wsl2-test-runner-setup.sh
@@ -40,7 +40,7 @@ sudo snap install --classic go
 # ngrok
 curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
   | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
-  && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
+  && echo "deb https://ngrok-agent.s3.amazonaws.com bookworm main" \
   | sudo tee /etc/apt/sources.list.d/ngrok.list \
   && sudo apt-get update >/dev/null \
   && sudo apt-get install -y ngrok >/dev/null
