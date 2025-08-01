3dc30296f2e2dfd0b2327c89fc9798b1ec6fcada
Randy Fay <randy@randyfay.com>
2025-08-01 11:26:06 -0600

    build: ngrok broke their installation moving to bookworm, fix it

--- .github/workflows/linux-setup.sh
+++ .github/workflows/linux-setup.sh
@@ -17,7 +17,7 @@ sudo apt-get install -y -qq build-essential expect libnss3-tools libcurl4-gnutls
 
 curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
   | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
-  && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
+  && echo "deb https://ngrok-agent.s3.amazonaws.com bookworm main" \
   | sudo tee /etc/apt/sources.list.d/ngrok.list \
   && sudo apt-get update -qq >/dev/null \
   && sudo apt-get install -y -qq ngrok
