9247506685a5815d65e58f94be99be0fb4096831
Stanislav Zhuk <stasadev@gmail.com>
2025-08-29 21:58:25 +0300

    Use GetMinimalContainerTimeout instead of GetStartScriptTimeout [skip ci]

--- containers/ddev-webserver/ddev-webserver-base-files/usr/local/bin/n-install.sh
+++ containers/ddev-webserver/ddev-webserver-base-files/usr/local/bin/n-install.sh
@@ -33,9 +33,7 @@ ln -sf "/mnt/ddev-global-cache/n_prefix/${HOSTNAME}" "${N_PREFIX}"
 
 # try online install that also uses cache
 n_install_result=true
-timeout "${START_SCRIPT_TIMEOUT:-30}" n install "${N_INSTALL_VERSION}" \
-  || log-stderr.sh --timeout "${START_SCRIPT_TIMEOUT:-30}" n install "${N_INSTALL_VERSION}" \
-  || n_install_result=false
+log-stderr.sh --timeout "${START_SCRIPT_TIMEOUT:-30}" n install "${N_INSTALL_VERSION}" || n_install_result=false
 
 # try offline install on fail
 if [ "${n_install_result}" = "false" ] && timeout "${START_SCRIPT_TIMEOUT:-30}" n install "${N_INSTALL_VERSION}" --offline; then
