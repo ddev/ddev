dc8688ade2ee37295636375a24d235d0551ac080
Randy Fay <randy@randyfay.com>
2025-09-13 15:12:06 -0600

    fix(ddev-php-base): Remove php8.4-obsolete config, fixes #7616 (#7617)

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -20,7 +20,7 @@ var AmplitudeAPIKey = ""
 var WebImg = "ddev/ddev-webserver"
 
 // WebTag defines the default web image tag
-var WebTag = "20250820_rfay_debug_test" // Note that this can be overridden by make
+var WebTag = "20250912_rfay_php84_settings" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "ddev/ddev-dbserver"
