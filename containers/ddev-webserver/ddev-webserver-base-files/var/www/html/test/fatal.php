065c2dd54951b688ea84b28a28e57d7f9294b947
Randy Fay <randy@randyfay.com>
2025-10-06 12:56:12 -0600

    Fix fatal.php for deprecated USER_ERRROR

--- containers/ddev-webserver/ddev-webserver-base-files/var/www/html/test/fatal.php
+++ containers/ddev-webserver/ddev-webserver-base-files/var/www/html/test/fatal.php
@@ -1,3 +1,8 @@
 <?php
 
-trigger_error("Fatal error", E_USER_ERROR);
+// PHP 8.4+ deprecated E_USER_ERROR with trigger_error()
+if (PHP_VERSION_ID >= 80400) {
+    exit("Fatal error");
+} else {
+    trigger_error("Fatal error", E_USER_ERROR);
+}
