471c273184632cd23b36a94e65da93a537f98db3
Stanislav Zhuk <stasadev@gmail.com>
2025-10-17 18:10:24 +0300

    Fix typo

--- pkg/ddevapp/addons.go
+++ pkg/ddevapp/addons.go
@@ -706,7 +706,7 @@ func ListAvailableAddons() ([]*github.Repository, error) {
 			}
 		}
 		if err != nil {
-			msg := fmt.Sprintf("Unable to get list of available services: %v", err)
+			msg := fmt.Sprintf("Unable to get list of available add-ons: %v", err)
 			if resp != nil {
 				rate := resp.Rate
 				if rate.Limit != 0 {
