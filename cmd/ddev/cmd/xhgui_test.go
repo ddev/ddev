6ab13aaee6558852cdf5e22160a95869c59e0765
Stanislav Zhuk <stasadev@gmail.com>
2025-10-31 21:28:47 +0200

    Revert "test: Improve TestCmdXHGui diagnostics and retry logic for intermittent 403 errors"
    This reverts commit 64e0be7010d87c2a201f35d4d901ebbf610f15e9.

--- cmd/ddev/cmd/xhgui_test.go
+++ cmd/ddev/cmd/xhgui_test.go
@@ -2,65 +2,18 @@ package cmd
 
 import (
 	"fmt"
-	"net/http"
 	"os"
 	"strings"
 	"testing"
 	"time"
 
 	"github.com/ddev/ddev/pkg/ddevapp"
-	"github.com/ddev/ddev/pkg/dockerutil"
 	"github.com/ddev/ddev/pkg/exec"
 	"github.com/ddev/ddev/pkg/globalconfig"
 	"github.com/ddev/ddev/pkg/nodeps"
 	"github.com/ddev/ddev/pkg/testcommon"
 	"github.com/stretchr/testify/require"
 )
-
-// logHTTPFailureDiagnostics logs detailed diagnostic information when an HTTP request fails
-// This helps understand whether failures are due to routing, container readiness, or application issues
-func logHTTPFailureDiagnostics(t *testing.T, url string, resp *http.Response, body string, err error, app *ddevapp.DdevApp, serviceName string) {
-	t.Logf("Failed to access %s at %s: %v", serviceName, url, err)
-
-	if resp != nil {
-		t.Logf("Response status: %d", resp.StatusCode)
-		t.Logf("Response headers:")
-		for k, v := range resp.Header {
-			t.Logf("  %s: %v", k, v)
-		}
-		// Key headers to check:
-		// - "Server" will show "nginx" if from service container, or identify the router
-		// - "X-Powered-By" will show "PHP/x.x.x" if PHP-FPM processed the request
-	}
-
-	if len(body) > 0 {
-		maxLen := 500
-		if len(body) < maxLen {
-			maxLen = len(body)
-		}
-		t.Logf("Response body (first %d chars): %s", maxLen, body[:maxLen])
-	}
-
-	// Try accessing the service container internally to see if the issue is routing or the container itself
-	internalOut, _, internalErr := app.Exec(&ddevapp.ExecOpts{
-		Service: serviceName,
-		Cmd:     "curl -I 127.0.0.1 2>&1",
-	})
-	t.Logf("Internal container curl to 127.0.0.1 in %s (err=%v):\n%s", serviceName, internalErr, internalOut)
-
-	// Check container health
-	containerName := ddevapp.GetContainerName(app, serviceName)
-	container, findErr := dockerutil.FindContainerByName(containerName)
-	if findErr == nil && container != nil {
-		health, healthLog := dockerutil.GetContainerHealth(container)
-		t.Logf("%s container health: %s", serviceName, health)
-		if healthLog != "" {
-			t.Logf("Health log: %s", healthLog)
-		}
-	} else {
-		t.Logf("Could not find %s container: %v", serviceName, findErr)
-	}
-}
 
 // TestCmdXHGui tests the `ddev xhgui` command
 func TestCmdXHGui(t *testing.T) {
@@ -127,12 +80,7 @@ func TestCmdXHGui(t *testing.T) {
 	require.NotNil(t, desc["xhgui_https_url"])
 	xhguiURL := desc["xhgui_https_url"].(string)
 
-	// Increase retries to handle transient 403/timing issues
-	// Using longer backoff: 1s, 2s, 4s, 8s, 16s, 32s, 64s = ~127s total
-	out, resp, err := testcommon.GetLocalHTTPResponseWithBackoff(t, xhguiURL, 10, 1*time.Second, opts)
-	if err != nil {
-		logHTTPFailureDiagnostics(t, xhguiURL, resp, out, err, app, "xhgui")
-	}
+	out, _, err = testcommon.GetLocalHTTPResponseWithBackoff(t, xhguiURL, 5, 500*time.Millisecond, opts)
 	require.NoError(t, err)
 	// Output should contain at least one run
 	require.Contains(t, out, strings.ToLower(app.GetHostname()))
