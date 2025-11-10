6c703ab5c65d40294ad3f0acc0b0a79b613db501
Randy Fay <randy@randyfay.com>
2025-11-10 13:57:02 +0000

    use new pushed image [skip ci]

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -20,7 +20,7 @@ var AmplitudeAPIKey = ""
 var WebImg = "ddev/ddev-webserver"
 
 // WebTag defines the default web image tag
-var WebTag = "20251127_stasadev_php85" // Note that this can be overridden by make
+var WebTag = "20251110_mariadb_11_4" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "ddev/ddev-dbserver"
