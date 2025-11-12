737060c8ba01d37b665115e8f13d58b4d26f96fb
Stanislav Zhuk <stasadev@gmail.com>
2025-11-12 21:12:41 +0200

    disable snapshots for mariadb:5.5

--- cmd/ddev/cmd/snapshot.go
+++ cmd/ddev/cmd/snapshot.go
@@ -54,6 +54,9 @@ ddev snapshot --all`,
 			if app.Database.Type == nodeps.Postgres && app.Database.Version == nodeps.Postgres9 {
 				util.Failed("Snapshots are not supported for postgres:9")
 			}
+			if app.Database.Type == nodeps.MariaDB && app.Database.Version == nodeps.MariaDB55 {
+				util.Failed("Snapshots are not supported for mariadb:5.5")
+			}
 			switch {
 			case snapshotCleanup:
 				deleteAppSnapshot(app)
