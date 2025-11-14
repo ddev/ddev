fa9f32ee46a6c651a4fab244a37c1b34e9ad4d13
Randy Fay <randy@randyfay.com>
2025-11-14 12:44:38 +0100

    test(pantheon): fix TestPantheonPush, which was broken by composer 2.9 (#7854) [skip ci]

--- pkg/ddevapp/providerPantheon_test.go
+++ pkg/ddevapp/providerPantheon_test.go
@@ -206,7 +206,7 @@ func TestPantheonPush(t *testing.T) {
 
 	// Make sure we have Drush
 	stdout, stderr, err := app.Exec(&ddevapp.ExecOpts{
-		Cmd: "composer require --no-interaction drush/drush >/dev/null 2>/dev/null",
+		Cmd: "composer require --no-interaction drush/drush",
 	})
 	require.NoError(t, err, "failed to composer require drush err=%v stdout='%s', stderr='%s'", err, stdout, stderr)
 	err = app.MutagenSyncFlush()
