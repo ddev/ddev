e00770c09e6355c0ddf30e4ac29573e3ae0e004a
Francesco Ciannavei <fracianna5@gmail.com>
2025-11-19 23:12:31 +0100

    Added short descriptions to CodeIgniter 4's TestDetectAppTypes Files [skip buildkite]

---
+++ pkg/ddevapp/testdata/TestDetectAppType/codeigniter/app/Config/App.php
@@ -0,0 +1,3 @@
+<?php
+
+// App.php is CodeIgniter 4's main configuration file that defines core application settings like base URL, timezone, character encoding, session handling, security options, and other fundamental behaviors of your application.
