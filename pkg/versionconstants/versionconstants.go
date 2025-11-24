b38d2b13ab5fa02ecfcb511544d0dffa18520dac
Stanislav Zhuk <stasadev@gmail.com>
2025-11-24 17:56:15 +0200

    build(curl): use trixie-backports to install newer curl [skip buildkite]

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -20,7 +20,7 @@ var AmplitudeAPIKey = ""
 var WebImg = "ddev/ddev-webserver"
 
 // WebTag defines the default web image tag
-var WebTag = "20251117_apt_sources" // Note that this can be overridden by make
+var WebTag = "20251124_stasadev_curl" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "ddev/ddev-dbserver"
