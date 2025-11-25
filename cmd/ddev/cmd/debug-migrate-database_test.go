ed09ce362d8aa1243748ca2f1138c9728dddb77e
Randy Fay <randy@randyfay.com>
2025-11-25 10:25:31 -0700

    TestDebugMigrateDatabase can test this

--- cmd/ddev/cmd/debug-migrate-database_test.go
+++ cmd/ddev/cmd/debug-migrate-database_test.go
@@ -10,36 +10,38 @@ import (
 	"github.com/ddev/ddev/pkg/ddevapp"
 	"github.com/ddev/ddev/pkg/exec"
 	"github.com/ddev/ddev/pkg/nodeps"
-	asrt "github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 // TestDebugMigrateDatabase checks to see if we can migrate database
 func TestDebugMigrateDatabase(t *testing.T) {
-	assert := asrt.New(t)
-
 	origDir, _ := os.Getwd()
 
 	site := TestSites[0]
 	_ = os.Chdir(site.Dir)
 
 	app, err := ddevapp.NewApp(site.Dir, false)
-	assert.NoError(err)
+	require.NoError(t, err)
 
+	// Remove existing
+	err = app.Stop(true, false)
+	require.NoError(t, err)
+
+	// Use notorious mariadb:11.8 as source and notorious mysql:8.4 as target
 	app.Database.Type = nodeps.MariaDB
-	app.Database.Version = nodeps.MariaDBDefaultVersion
+	app.Database.Version = nodeps.MariaDB118
 
 	t.Cleanup(func() {
 		out, err := exec.RunHostCommand(DdevBin, "debug", "migrate-database", fmt.Sprintf("%s:%s", nodeps.MariaDB, nodeps.MariaDBDefaultVersion))
-		assert.NoError(err, "failed to migrate database; out='%s'", out)
+		require.NoError(t, err, "failed to migrate database; out='%s'", out)
 
-		assert.Contains(out, fmt.Sprintf("Database was converted to %s:%s", nodeps.MariaDB, nodeps.MariaDBDefaultVersion))
+		require.Contains(t, out, fmt.Sprintf("Database was converted to %s:%s", nodeps.MariaDB, nodeps.MariaDBDefaultVersion))
 
 		out, stderr, err := app.Exec(&ddevapp.ExecOpts{
 			Service: "db",
 			Cmd:     fmt.Sprintf(`%s -e 'DROP TABLE IF EXISTS %s;'`, app.GetDBClientCommand(), t.Name()),
 		})
-		assert.NoError(err, "DROP table didn't work, out='%s', stderr='%s'", out, stderr)
+		require.NoError(t, err, "DROP table didn't work, out='%s', stderr='%s'", out, stderr)
 		_ = os.Chdir(origDir)
 	})
 
@@ -52,7 +54,7 @@ func TestDebugMigrateDatabase(t *testing.T) {
 	})
 	require.NoError(t, err)
 	// It should have our default version
-	require.True(t, strings.HasPrefix(out, nodeps.MariaDBDefaultVersion))
+	require.True(t, strings.HasPrefix(out, nodeps.MariaDB118))
 
 	// Import a database so we have something to work with
 	err = app.ImportDB(filepath.Join(origDir, "testdata", t.Name(), "users.sql"), "", false, false, "")
@@ -65,9 +67,9 @@ func TestDebugMigrateDatabase(t *testing.T) {
 	require.NoError(t, err)
 
 	// Try a migration
-	out, err = exec.RunHostCommand(DdevBin, "utility", "migrate-database", fmt.Sprintf("%s:%s", nodeps.MySQL, nodeps.MySQL80))
+	out, err = exec.RunHostCommand(DdevBin, "utility", "migrate-database", fmt.Sprintf("%s:%s", nodeps.MySQL, nodeps.MySQL84))
 	require.NoError(t, err, "failed to migrate database; out='%s'", out)
-	require.Contains(t, out, fmt.Sprintf("Database was converted to %s:%s", nodeps.MySQL, nodeps.MySQL80))
+	require.Contains(t, out, fmt.Sprintf("Database was converted to %s:%s", nodeps.MySQL, nodeps.MySQL84))
 
 	_, err = app.ReadConfig(true)
 	require.NoError(t, err)
@@ -86,5 +88,5 @@ func TestDebugMigrateDatabase(t *testing.T) {
 		Cmd:     fmt.Sprintf(`%s -N  -e 'SELECT VERSION();'`, app.GetDBClientCommand()),
 	})
 	require.NoError(t, err)
-	require.True(t, strings.HasPrefix(out, nodeps.MySQL80))
+	require.True(t, strings.HasPrefix(out, nodeps.MySQL84))
 }
