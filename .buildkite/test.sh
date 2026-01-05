18f33fe6b56cf7553041a4e8ca2c8c2b476dfea3
Randy Fay <randy@randyfay.com>
2026-01-05 12:52:00 -0700

    Improve script

--- .buildkite/test.sh
+++ .buildkite/test.sh
@@ -61,22 +61,26 @@ if [ "${os:-}" = "darwin" ]; then
   # Now start the docker provider we want
   case ${DOCKER_TYPE:=none} in
     "colima_vz")
-      COLIMA_INSTANCE=vz
+      export COLIMA_INSTANCE=vz
+      set -x
       colima start ${COLIMA_INSTANCE}
       colima ssh -p ${COLIMA_INSTANCE} -- bash -c 'docker rm -f $(docker ps -aq) || true'
       colima ssh -p ${COLIMA_INSTANCE} -- bash -c 'sudo rm -rf /var/lib/docker/containers/*'
       colima ssh -p ${COLIMA_INSTANCE} -- sudo systemctl restart docker
       docker context use colima-${COLIMA_INSTANCE}
+      set +x
       ;;
 
     "lima")
-      LIMA_INSTANCE=lima-vz
-      HOMEDIR=/home/testbot.linux
+      export LIMA_INSTANCE=lima-vz
+      export HOMEDIR=/home/testbot.linux
       limactl start ${LIMA_INSTANCE}
+      set -x
       limactl shell ${LIMA_INSTANCE} bash -c 'docker rm -f $(docker ps -aq) || true'
-      limactl shell lima-vz bash -c 'rm -rf /home/testbot.linux/.local/share/docker/containers/*'
+      limactl shell lima-vz bash -c 'rm -rf ${HOMEDIR}/.local/share/docker/containers/*'
       limactl shell ${LIMA_INSTANCE} systemctl --user restart docker
       docker context use lima-${LIMA_INSTANCE}
+      set +x
       ;;
 
     "docker-desktop")
