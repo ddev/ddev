2d43b16d80a85d88e2089e67ccef48026afcf302
Randy Fay <randy@randyfay.com>
2026-01-07 18:06:05 -0700

    fix(router): healthcheck after new config must happen as normal user (#8010) [skip ci]

--- pkg/ddevapp/router.go
+++ pkg/ddevapp/router.go
@@ -152,7 +152,8 @@ func StartDdevRouter() error {
 			return fmt.Errorf("failed to find router for healthcheck: %v", err)
 		}
 		util.Debug("Forcing router healthcheck to verify new config is loaded")
-		_, _, err = dockerutil.Exec(router.ID, "rm -f /tmp/healthy && /healthcheck.sh", "0")
+		uid, _, _ := dockerutil.GetContainerUser()
+		_, _, err = dockerutil.Exec(router.ID, "rm -f /tmp/healthy && /healthcheck.sh", uid)
 		if err != nil {
 			return fmt.Errorf("router healthcheck failed: %v", err)
 		}
