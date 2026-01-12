e5820eaf59e41df0dfa98379954263b3eda4cd64
Randy Fay <randy@randyfay.com>
2026-01-12 15:42:53 -0700

    push image after upstream commit [skip ci]

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -20,7 +20,7 @@ var AmplitudeAPIKey = ""
 var WebImg = "ddev/ddev-webserver"
 
 // WebTag defines the default web image tag
-var WebTag = "20261203_xdebug_diagnose" // Note that this can be overridden by make
+var WebTag = "20260112_copilot_xdebug_diagnose" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "ddev/ddev-dbserver"
