adfd7a5c565dc9c01ee1aca7fe142a3d577b6930
Randy Fay <randy@randyfay.com>
2026-01-12 18:49:22 -0700

    get the upstream repo in git

--- .devcontainer/setup_test_project.sh
+++ .devcontainer/setup_test_project.sh
@@ -6,6 +6,7 @@ echo "You don't need to wait for the test project to be set up."
 sudo chown ${USER} /workspaces || true
 mkdir -p /workspaces/.config
 set -x
+git remote add upstream https://github.com/ddev/ddev || true
 git fetch upstream
 make
 sudo ln -sf "${PWD}/.gotmp/bin/linux_$(dpkg --print-architecture)/ddev" /usr/local/bin/ddev
