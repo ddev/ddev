1af51d766b89638d5480f582a2ca3ce7a8f60ee3
Randy Fay <randy@randyfay.com>
2026-01-12 17:42:04 -0700

    chore(codespaces): Use newer test project for codespaces [skip ci]

--- .devcontainer/setup_test_project.sh
+++ .devcontainer/setup_test_project.sh
@@ -11,7 +11,7 @@ ddev delete -Oy tmp >/dev/null || true
 ddev --version
 
 export DDEV_NONINTERACTIVE=true CI=true
-DDEV_REPO=${DDEV_REPO:-https://github.com/ddev/d10simple}
+DDEV_REPO=${DDEV_REPO:-https://github.com/ddev/d11simple}
 DDEV_ARTIFACTS=${DDEV_REPO}-artifacts
 git clone ${DDEV_ARTIFACTS} "/tmp/${DDEV_ARTIFACTS##*/}" || true
 reponame=${DDEV_REPO##*/}
