8ff1ac2055996f47f13b3e4e589c44afb91f0bc3
Randy Fay <randy@randyfay.com>
2026-01-19 17:41:18 -0700

    Add more mkcert support [skip ci]

--- containers/devcontainers/install-ddev/install.sh
+++ containers/devcontainers/install-ddev/install.sh
@@ -4,4 +4,5 @@ set -eu -o pipefail
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://pkg.ddev.com/apt/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/ddev.gpg > /dev/null
 echo "deb [signed-by=/etc/apt/keyrings/ddev.gpg] https://pkg.ddev.com/apt/ * *" | sudo tee /etc/apt/sources.list.d/ddev.list
-sudo apt-get update >/dev/null && sudo apt-get install -y ddev xdg-utils >/dev/null
+sudo apt-get update >/dev/null && sudo apt-get install -y ddev mkcert xdg-utils >/dev/null
+sudo mkcert -install
