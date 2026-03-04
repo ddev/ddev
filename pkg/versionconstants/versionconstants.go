5ec42f79c8eb2d5bceeac94762051ba8503acd91
Stanislav Zhuk <stasadev@gmail.com>
2026-03-04 22:19:18 +0200

    push the web image

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -20,7 +20,7 @@ var AmplitudeAPIKey = ""
 var WebImg = "ddev/ddev-webserver"
 
 // WebTag defines the default web image tag
-var WebTag = "v1.25.1" // Note that this can be overridden by make
+var WebTag = "20260303_akibaat_reduce_world_writeables" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "ddev/ddev-dbserver"
