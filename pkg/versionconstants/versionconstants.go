8d8482804b0add238e93df654875b582690afa39
Stanislav Zhuk <stasadev@gmail.com>
2026-06-01 13:23:14 +0300

    build: pin Node.js to 24.15.0, fixes #8436 (#8438)

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -20,7 +20,7 @@ var AmplitudeAPIKey = ""
 var WebImg = "ddev/ddev-webserver"
 
 // WebTag defines the default web image tag
-var WebTag = "20260524_stasadev_docker_rootless" // Note that this can be overridden by make
+var WebTag = "20260531_stasadev_node_pin" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "ddev/ddev-dbserver"
