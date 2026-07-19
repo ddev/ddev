3a2d3c95bb9348e4021c27bc8ffa82b98de46520
rfay <randy@randyfay.com>
2026-07-19 21:05:53 +0000

    build(docker): use the branch tag for the ddev-php-base pin and WebTag [skip ci]
    - For #8600
    
    Uses an arbitrary, made-up tag for the `ddev-php-base` pin in
    `containers/ddev-webserver/Dockerfile` and for `WebTag` in
    `pkg/versionconstants/versionconstants.go`, instead of following this
    repo's standard convention of using the current branch name so that
    `push-tagged-image.yml` can build and publish matching test images for
    this branch.
    
    - `containers/ddev-webserver/Dockerfile`: pin `ddev-php-base` to
      `20260719_build_perf_script` (this branch's name).
    - `pkg/versionconstants/versionconstants.go`: set `WebTag` to
      `20260719_build_perf_script` to match.
    
    Same as the previous commit's manual testing; re-verified after the tag
    rename that `ddev utility rebuild` still resolves and builds correctly
    with the renamed local image tags.
    
    None; tag rename only, no behavior change.
    
    A maintainer still needs to run `push-tagged-image.yml` for both
    `ddev-php-base` and `ddev-webserver` with tag `20260719_build_perf_script`
    before this branch's CI can pull the real images.

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -20,7 +20,7 @@ var AmplitudeAPIKey = ""
 var WebImg = "ddev/ddev-webserver"
 
 // WebTag defines the default web image tag
-var WebTag = "20260719_rfay_n_prefix_group0" // Note that this can be overridden by make
+var WebTag = "20260719_build_perf_script" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "ddev/ddev-dbserver"
