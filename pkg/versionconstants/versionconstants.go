17d199e8dbbe14fdd8dffeab775eefd11b9ed3d7
Randy Fay <randy@randyfay.com>
2026-07-29 16:22:41 -0600

    update versionconstants after ddev-php-base consolidation

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -20,7 +20,7 @@ var AmplitudeAPIKey = ""
 var WebImg = "ddev/ddev-webserver"
 
 // WebTag defines the default web image tag
-var WebTag = "20260721_rfay_content_addressed_image_tags-71f9d46e8b" // Note that this can be overridden by make
+var WebTag = "20260721_rfay_content_addressed_image_tags-d15999faa9" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "ddev/ddev-dbserver"
