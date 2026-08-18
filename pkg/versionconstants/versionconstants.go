deccef7672b85381c702d7b40731f32e0052dabb
Randy Fay <randy@randyfay.com>
2026-08-17 22:42:35 -0600

    Update versionconstants after rebase

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -37,11 +37,10 @@ var WebTagBranch = "20260814_rfay_docker_update_phase_2"
 var DBImg = "ddev/ddev-dbserver"
 
 // BaseDBTag is the main tag, DBTag is constructed from it
-var BaseDBTag = "20260814_rfay_uncompressed_snapshot-2bfcd888e6"
+var BaseDBTag = "7b74988035" // 20260814_rfay_uncompressed_snapshot-7b74988035
 
 // BaseDBTagBranch is the branch BaseDBTag's content was built from.
-var BaseDBTagBranch = "20260814_rfay_docker_update_phase_2"
-
+var BaseDBTagBranch = "20260814_rfay_uncompressed_snapshot"
 
 // TraefikRouterImage is image for router
 var TraefikRouterImage = "ddev/ddev-traefik-router"
