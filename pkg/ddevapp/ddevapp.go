08b1691e3ff84598296a7c9b380c8a25cd3f4073
Stanislav Zhuk <stasadev@gmail.com>
2026-08-28 13:11:23 +0300

    chore(compose): remove unused COMPOSE_REMOVE_ORPHANS, for #8759 (#8761) [skip ci]

--- pkg/ddevapp/ddevapp.go
+++ pkg/ddevapp/ddevapp.go
@@ -2984,7 +2984,6 @@ func (app *DdevApp) DockerEnv() map[string]string {
 	envVars := map[string]string{
 		// The compose project name can no longer contain dots; must be lower-case
 		"COMPOSE_PROJECT_NAME":           app.GetComposeProjectName(),
-		"COMPOSE_REMOVE_ORPHANS":         "true",
 		"COMPOSER_EXIT_ON_PATCH_FAILURE": "1",
 		"DDEV_SITENAME":                  app.Name,
 		"DDEV_TLD":                       app.ProjectTLD,
