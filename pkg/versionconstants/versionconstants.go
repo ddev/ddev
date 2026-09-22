2225d643e6e1e0efd3de82ec16127f9d399eabc2
Stanislav Zhuk <stasadev@gmail.com>
2026-09-22 20:34:17 +0300

    Revert "fix(pr-to-pr): Minor nginx fixup suggestion as proposed in review of #8821 (#8847) [skip buildkite]"
    This reverts commit a7c6a7692e27cefa030921565409581bb062b6cb.

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -49,7 +49,7 @@ func IsUnreleasedDdevVersion(version string) bool {
 var WebImg = "ddev/ddev-webserver"
 
 // WebTag defines the default web image tag
-var WebTag = "19b2502be3" // 20260913_stasadev_webserver_error_pages-19b2502be3
+var WebTag = "71799c0f31" // 20260913_stasadev_webserver_error_pages-71799c0f31
 
 // WebTagBranch is the branch WebTag's content was built from.
 var WebTagBranch = "20260913_stasadev_webserver_error_pages"
