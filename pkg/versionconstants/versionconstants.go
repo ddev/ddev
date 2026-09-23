41f79966f91ba90925fe8f743fcbded0156048eb
Randy Fay <randy@randyfay.com>
2026-09-22 20:15:42 -0600

    update WebTag after rebase [skip buildkite]

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -49,7 +49,7 @@ func IsUnreleasedDdevVersion(version string) bool {
 var WebImg = "ddev/ddev-webserver"
 
 // WebTag defines the default web image tag
-var WebTag = "3e905aa7bd" // 20260914_rfay_utilities_extra_layer-3e905aa7bd
+var WebTag = "3b665ca1d0" // 20260914_rfay_utilities_extra_layer-3b665ca1d0
 
 // WebTagBranch is the branch WebTag's content was built from.
 var WebTagBranch = "20260914_rfay_utilities_extra_layer"
